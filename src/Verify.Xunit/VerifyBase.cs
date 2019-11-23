4d847536a341acf50d889c52f045df7349e71601
Simon Cropp <simon.cropp@gmail.com>
2019-11-23 11:37:18 +1100

    .

--- src/Verify.Xunit/VerifyBase.cs
+++ src/Verify.Xunit/VerifyBase.cs
@@ -1,51 +1,13 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
-using Xunit.Sdk;
 
 namespace VerifyXunit
 {
     public partial class VerifyBase :
         XunitContextBase
     {
-        public async Task Verify(string target)
-        {
-            Guard.AgainstNull(target, nameof(target));
-            target = ApplyScrubbers(target);
-            var (receivedPath, verifiedPath) = GetFileNames();
-            if (!File.Exists(verifiedPath))
-            {
-                await FileHelpers.WriteText(receivedPath, target);
-                ClipboardCapture.Append(receivedPath, verifiedPath);
-                if (DiffRunner.FoundDiff)
-                {
-                    await FileHelpers.WriteText(verifiedPath, "");
-                    DiffRunner.Launch(receivedPath, verifiedPath);
-                }
-
-                throw new Exception($"First verification. {Context.UniqueTestName}.verified{extension} not found. Verification command has been copied to the clipboard.");
-            }
-
-            var verifiedText = await FileHelpers.ReadText(verifiedPath);
-
-            try
-            {
-                Assert.Equal(verifiedText, target);
-            }
-            catch (EqualException exception)
-            {
-                await FileHelpers.WriteText(receivedPath, target);
-                ClipboardCapture.Append(receivedPath, verifiedPath);
-                exception.PrefixWithCopyCommand();
-                DiffRunner.Launch(receivedPath, verifiedPath);
-
-                throw;
-            }
-        }
-
         (string receivedPath, string verifiedPath) GetFileNames()
         {
             var filePrefix = Path.Combine(SourceDirectory, Context.UniqueTestName);
---
+++ src/Verify.Xunit/VerifyBase_String.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace VerifyXunit
+{
+    public partial class VerifyBase :
+        XunitContextBase
+    {
+        public async Task Verify(string target)
+        {
+            Guard.AgainstNull(target, nameof(target));
+            target = ApplyScrubbers(target);
+            var (receivedPath, verifiedPath) = GetFileNames();
+            if (!File.Exists(verifiedPath))
+            {
+                await FileHelpers.WriteText(receivedPath, target);
+                ClipboardCapture.Append(receivedPath, verifiedPath);
+                if (DiffRunner.FoundDiff)
+                {
+                    await FileHelpers.WriteText(verifiedPath, "");
+                    DiffRunner.Launch(receivedPath, verifiedPath);
+                }
+
+                throw new Exception($"First verification. {Context.UniqueTestName}.verified{extension} not found. Verification command has been copied to the clipboard.");
+            }
+
+            var verifiedText = await FileHelpers.ReadText(verifiedPath);
+
+            try
+            {
+                Assert.Equal(verifiedText, target);
+            }
+            catch (EqualException exception)
+            {
+                await FileHelpers.WriteText(receivedPath, target);
+                ClipboardCapture.Append(receivedPath, verifiedPath);
+                exception.PrefixWithCopyCommand();
+                DiffRunner.Launch(receivedPath, verifiedPath);
+
+                throw;
+            }
+        }
+    }
+}

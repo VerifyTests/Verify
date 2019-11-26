c5f1712b100ad527cef3265c477294df6ba41208
SimonCropp <simon.cropp@gmail.com>
2019-11-26 19:05:16 +1100

    Create DiffRunnerTests.cs

---
+++ src/Verify.Xunit.Tests/DiffRunnerTests.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+public class DiffRunnerTests :
+    VerifyBase
+{
+    [Fact(Skip = "Explicit")]
+    public void Launch()
+    {
+        DiffRunner.Launch(Path.Combine(SourceDirectory, "DiffRunnerFile1.txt"), Path.Combine(SourceDirectory, "DiffRunnerFile2.txt"));
+    }
+
+    public DiffRunnerTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}

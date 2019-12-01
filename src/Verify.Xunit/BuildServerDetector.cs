1ad00fcd91927ffd80ed05a0a89dd1ff8626f2dd
SimonCropp <simon.cropp@gmail.com>
2019-12-02 08:54:24 +1100

    Update BuildServerDetector.cs

--- src/Verify.Xunit/BuildServerDetector.cs
+++ src/Verify.Xunit/BuildServerDetector.cs
@@ -4,20 +4,34 @@ static class BuildServerDetector
 {
     static BuildServerDetector()
     {
-        // Appveyor: https://www.appveyor.com/docs/environment-variables/
-        // Travis: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+        // Appveyor
+        // https://www.appveyor.com/docs/environment-variables/
+        // Travis
+        // https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
         if (string.Equals(Environment.GetEnvironmentVariable("CI"), "true", StringComparison.OrdinalIgnoreCase))
         {
             Detected = true;
             return;
         }
-        // Jenkins: https://wiki.jenkins.io/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-belowJenkinsSetEnvironmentVariables
+
+        // Jenkins
+        // https://wiki.jenkins.io/display/JENKINS/Building+a+software+project#Buildingasoftwareproject-belowJenkinsSetEnvironmentVariables
         if (Environment.GetEnvironmentVariable("JENKINS_URL") != null)
         {
             Detected = true;
             return;
         }
-        // AzureDevops: https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#agent-variables
+
+        // GitHub Action
+        // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/using-environment-variables#default-environment-variables
+        if (Environment.GetEnvironmentVariable("GITHUB_ACTION") != null)
+        {
+            Detected = true;
+            return;
+        }
+
+        // AzureDevops
+        // https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml#agent-variables
         // Variable name is 'Agent.Id' but must be referenced with an underscore
         // https://docs.microsoft.com/en-us/azure/devops/pipelines/release/variables?view=azure-devops&tabs=powershell#using-default-variables
         if (Environment.GetEnvironmentVariable("Agent_Id") != null)

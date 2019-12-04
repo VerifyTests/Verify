134330648a9ce6f17fbbcfeb77512d1b72a36948
SimonCropp <simon.cropp@gmail.com>
2019-12-04 21:08:45 +1100

    .

---
+++ src/Verify.Xunit/FileNameBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using VerifyXunit;
+
+static class FileNameBuilder
+{
+    public static (string received, string verified) GetFileNames(string extension, string? suffix, Namer namer, Type testType, string directory, string testName)
+    {
+        var filePrefix = GetFilePrefix(namer, testType, directory, testName);
+        if (suffix == null)
+        {
+            var received = $"{filePrefix}.received.{extension}";
+            var verified = $"{filePrefix}.verified.{extension}";
+            return (received, verified);
+        }
+        else
+        {
+            var received = $"{filePrefix}.{suffix}.received.{extension}";
+            var verified = $"{filePrefix}.{suffix}.verified.{extension}";
+            return (received, verified);
+        }
+    }
+
+    static string GetFilePrefix(Namer namer, Type testType, string directory, string testName)
+    {
+        var builder = new StringBuilder(Path.Combine(directory, testName));
+
+        if (namer.UniqueForRuntimeAndVersion || Global.Namer.UniqueForRuntimeAndVersion)
+        {
+            builder.Append($".{Namer.runtimeAndVersion}");
+        }
+        else if (namer.UniqueForRuntime || Global.Namer.UniqueForRuntime)
+        {
+            builder.Append($".{Namer.runtime}");
+        }
+
+        if (namer.UniqueForAssemblyConfiguration || Global.Namer.UniqueForAssemblyConfiguration)
+        {
+            builder.Append($".{testType.Assembly.GetAttributeConfiguration()}");
+        }
+
+        return builder.ToString();
+    }
+}
--- src/Verify.Xunit/VerifyBase_Namer.cs
+++ src/Verify.Xunit/VerifyBase_Namer.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using System.Text;
-
-namespace VerifyXunit
+﻿namespace VerifyXunit
 {
     public partial class VerifyBase
     {
@@ -24,40 +21,7 @@ namespace VerifyXunit
 
         (string receivedPath, string verifiedPath) GetFileNames(string extension, string? suffix = null)
         {
-            var filePrefix = GetFilePrefix();
-            if (suffix == null)
-            {
-                var receivedPath = $"{filePrefix}.received.{extension}";
-                var verifiedPath = $"{filePrefix}.verified.{extension}";
-                return (receivedPath, verifiedPath);
-            }
-            else
-            {
-                var receivedPath = $"{filePrefix}.{suffix}.received.{extension}";
-                var verifiedPath = $"{filePrefix}.{suffix}.verified.{extension}";
-                return (receivedPath, verifiedPath);
-            }
-        }
-
-        string GetFilePrefix()
-        {
-            var builder = new StringBuilder(Path.Combine(SourceDirectory, Context.UniqueTestName));
-
-            if (Namer.UniqueForRuntimeAndVersion || Global.Namer.UniqueForRuntimeAndVersion)
-            {
-                builder.Append($".{Namer.runtimeAndVersion}");
-            }
-            else if (Namer.UniqueForRuntime || Global.Namer.UniqueForRuntime)
-            {
-                builder.Append($".{Namer.runtime}");
-            }
-
-            if (Namer.UniqueForAssemblyConfiguration || Global.Namer.UniqueForAssemblyConfiguration)
-            {
-                builder.Append($".{Context.TestType.Assembly.GetAttributeConfiguration()}");
-            }
-
-            return builder.ToString();
+            return FileNameBuilder.GetFileNames(extension, suffix, Namer, Context.TestType, SourceDirectory, Context.UniqueTestName);
         }
     }
 }

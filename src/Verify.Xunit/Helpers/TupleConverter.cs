4f2f9f101721a31056391cc2eea8a60983a62cf1
SimonCropp <simon.cropp@gmail.com>
2019-12-04 21:24:40 +1100

    .

---
+++ src/Verify.Xunit/Helpers/TupleConverter.cs
@@ -0,0 +1,39 @@
+﻿#if !NETSTANDARD2_0
+using System.Reflection;
+using System;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
+
+static class TupleConverter
+{
+    public static Dictionary<string, object> ExpressionToDictionary(Expression<Func<ITuple>> expression)
+    {
+        var unaryExpression = (UnaryExpression) expression.Body;
+        var methodCallExpression = (MethodCallExpression) unaryExpression.Operand;
+        var method = methodCallExpression.Method;
+        var attribute = ReadTupleElementNamesAttribute(method);
+        var dictionary = new Dictionary<string, object>();
+        var result = expression.Compile().Invoke();
+        for (var index = 0; index < attribute.TransformNames.Count; index++)
+        {
+            var transformName = attribute.TransformNames[index];
+            dictionary.Add(transformName, result[index]);
+        }
+
+        return dictionary;
+    }
+
+    static TupleElementNamesAttribute ReadTupleElementNamesAttribute(MethodInfo method)
+    {
+        var attribute = (TupleElementNamesAttribute) method.ReturnTypeCustomAttributes.GetCustomAttributes(typeof(TupleElementNamesAttribute), false).SingleOrDefault();
+        if (attribute != null)
+        {
+            return attribute;
+        }
+
+        throw new Exception("Verify is only to be used on methods that return a tuple.");
+    }
+}
+#endif
--- src/Verify.Xunit/VerifyBase_Tuple.cs
+++ src/Verify.Xunit/VerifyBase_Tuple.cs
@@ -1,10 +1,7 @@
 ﻿#if !NETSTANDARD2_0
-using System.Reflection;
 using System;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace VerifyXunit
@@ -13,35 +10,8 @@ namespace VerifyXunit
     {
         public Task Verify(Expression<Func<ITuple>> expression)
         {
-            var dictionary = ExpressionToDictionary(expression);
+            var dictionary = TupleConverter.ExpressionToDictionary(expression);
             return Verify(dictionary);
-        }
-
-        static Dictionary<string, object> ExpressionToDictionary(Expression<Func<ITuple>> expression)
-        {
-            var unaryExpression = (UnaryExpression) expression.Body;
-            var methodCallExpression = (MethodCallExpression) unaryExpression.Operand;
-            var method = methodCallExpression.Method;
-            var attribute = ReadTupleElementNamesAttribute(method);
-            var dictionary = new Dictionary<string, object>();
-            var result = expression.Compile().Invoke();
-            for (var index = 0; index < attribute.TransformNames.Count; index++)
-            {
-                var transformName = attribute.TransformNames[index];
-                dictionary.Add(transformName, result[index]);
-            }
-
-            return dictionary;
-        }
-
-        static TupleElementNamesAttribute ReadTupleElementNamesAttribute(MethodInfo method)
-        {
-            var attribute = (TupleElementNamesAttribute) method.ReturnTypeCustomAttributes.GetCustomAttributes(typeof(TupleElementNamesAttribute), false).SingleOrDefault();
-            if (attribute != null)
-            {
-                return attribute;
-            }
-            throw new Exception($"{nameof(Verify)} is only to be used on methods that return a tuple.");
         }
     }
 }

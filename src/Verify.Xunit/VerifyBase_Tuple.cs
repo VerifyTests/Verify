ca1a9ed01a04f41ad4276adca8c9a702f04c567f
SimonCropp <simon.cropp@gmail.com>
2019-12-15 09:04:24 +1100

    Update VerifyBase_Tuple.cs

--- src/Verify.Xunit/VerifyBase_Tuple.cs
+++ src/Verify.Xunit/VerifyBase_Tuple.cs
@@ -3,14 +3,17 @@ using System;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Verify;
 
 namespace VerifyXunit
 {
     public partial class VerifyBase
     {
-        public Task Verify(Expression<Func<ITuple>> expression)
+        public Task Verify(
+            Expression<Func<ITuple>> expression,
+            VerifySettings? settings = null)
         {
-            return verifier.Verify(expression);
+            return verifier.Verify(expression, settings);
         }
     }
 }

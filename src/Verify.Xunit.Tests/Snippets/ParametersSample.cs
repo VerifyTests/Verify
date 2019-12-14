0b40c93587e731e46f94f1506181c07063c67842
SimonCropp <simon.cropp@gmail.com>
2019-12-14 20:21:40 +1100

    .

---
+++ src/Verify.Xunit.Tests/Snippets/ParametersSample.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+public class ParametersSample :
+    VerifyBase
+{
+    [Theory]
+    [InlineData("Value1")]
+    [InlineData("Value2")]
+    public Task Usage(string arg)
+    {
+        return Verify(arg);
+    }
+
+    public ParametersSample(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}

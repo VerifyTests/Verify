048b64da4bbaa5801bd904a37a83414f44c26d5e
SimonCropp <simon.cropp@gmail.com>
2019-12-14 16:28:41 +1100

    .

--- src/Verify.NUnit/Verifier_Text.cs
+++ src/Verify.NUnit/Verifier_Text.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using NUnit.Framework;
 using Verify;
 
 namespace VerifyNUnit

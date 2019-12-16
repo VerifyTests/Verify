cfbbf1c5fd0852d4165b496aec83cf755195e5ac
Simon Cropp <simon.cropp@gmail.com>
2019-12-16 15:20:14 +1100

    add verify file

---
+++ src/Verify.Xunit/VerifyBase_File.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Verify;
+
+namespace VerifyXunit
+{
+    public partial class VerifyBase
+    {
+        public Task VerifyFile(
+            string path,
+            VerifySettings? settings = null)
+        {
+            return verifier.VerifyFile(path, settings);
+        }
+
+        public Task VerifyFile(
+            FileInfo file,
+            VerifySettings? settings = null)
+        {
+            return verifier.VerifyFile(file, settings);
+        }
+    }
+}
---
+++ src/Verify/Verifier/Verifier_File.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Verify;
+
+partial class Verifier
+{
+    public Task VerifyFile(
+        string path,
+        VerifySettings? settings = null)
+    {
+        Guard.FileExists(path, nameof(path));
+        settings = settings.OrDefault();
+        if (!settings.HasExtension())
+        {
+            settings = new VerifySettings(settings);
+            settings.UseExtension(Path.GetExtension(path).Substring(1));
+        }
+
+        return Verify(File.OpenRead(path), settings);
+    }
+
+    public Task VerifyFile(
+        FileInfo file,
+        VerifySettings? settings = null)
+    {
+        Guard.AgainstNull(file, nameof(file));
+        return VerifyFile(file.FullName, settings);
+    }
+}

44cb8922fd6a867cf882dce04339a40cc72a346c
Simon Cropp <simon.cropp@gmail.com>
2019-12-21 23:14:06 +1100

    Update ClipboardCapture.cs

--- src/Verify/ClipboardCapture.cs
+++ src/Verify/ClipboardCapture.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TextCopy;
@@ -8,16 +10,43 @@ static class ClipboardCapture
     static StringBuilder builder = new StringBuilder();
     static SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
 
+    static string moveCommand;
+    static string deleteCommand;
+    
+    static ClipboardCapture()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            moveCommand = "move /Y \"{0}\" \"{1}\""; 
+            deleteCommand = "del \"{0}\"";
+            return;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            moveCommand = "mv -f \"{0}\" \"{1}\"";
+            deleteCommand = "rm -f \"{0}\"";
+            return;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            moveCommand = "mv -f \"{0}\" \"{1}\"";
+            deleteCommand = "rm -f \"{0}\"";
+            return;
+        }
+
+        throw new Exception($"OS not supported: {RuntimeInformation.OSDescription}");
+    }
+    
     public static Task AppendMove(string received, string verified)
     {
-        var command = $"move /Y \"{received}\" \"{verified}\"";
-        return Append(command);
+        return Append(string.Format(moveCommand, received, verified));
     }
 
     public static Task AppendDelete(string verified)
     {
-        var command = $"del \"{verified}\"";
-        return Append(command);
+        return Append(string.Format(deleteCommand, verified));
     }
 
     static async Task Append(string command)

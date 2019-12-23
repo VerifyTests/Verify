aa564b73553a3ffb466b44f4baecab819161f013
SimonCropp <simon.cropp@gmail.com>
2019-12-23 16:05:18 +1100

    cleanup

---
+++ src/Verify/Splitters/ObjectConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class ObjectConverter
+{
+    public string ToExtension { get; }
+    public Func<object, IEnumerable<Stream>> Func { get; }
+
+    public ObjectConverter(string toExtension, Func<object, IEnumerable<Stream>> func)
+    {
+        ToExtension = toExtension;
+        Func = func;
+    }
+}
---
+++ src/Verify/Splitters/StreamConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class StreamConverter
+{
+    public string ToExtension { get; }
+    public Func<Stream, IEnumerable<Stream>> Func { get; }
+
+    public StreamConverter(string toExtension, Func<Stream, IEnumerable<Stream>> func)
+    {
+        ToExtension = toExtension;
+        Func = func;
+    }
+}

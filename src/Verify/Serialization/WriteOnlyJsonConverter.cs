e52e03a10ffc8411e14960febf3a8ea6a7996288
SimonCropp <simon.cropp@gmail.com>
2020-01-05 12:56:20 +1100

    add generic WriteOnlyJsonConverter

--- src/Verify/Serialization/WriteOnlyJsonConverter.cs
+++ src/Verify/Serialization/WriteOnlyJsonConverter.cs
@@ -11,4 +11,21 @@ namespace Verify
             throw new NotImplementedException();
         }
     }
+
+    public abstract class WriteOnlyJsonConverter<T> :
+        WriteOnlyJsonConverter
+        where T: class
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            WriteJson(writer, (T?) value, serializer);
+        }
+
+        public abstract void WriteJson(JsonWriter writer, T? value, JsonSerializer serializer);
+
+        public override bool CanConvert(Type type)
+        {
+            return typeof(T).IsAssignableFrom(type);
+        }
+    }
 }

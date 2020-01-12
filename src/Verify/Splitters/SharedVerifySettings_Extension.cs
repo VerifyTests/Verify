ca6aa33987e6bc04fb78aed88ad9cf77b68d179b
SimonCropp <simon.cropp@gmail.com>
2020-01-12 15:37:01 +1100

    split converter settings

---
+++ src/Verify/Splitters/SharedVerifySettings_Extension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Verify
+{
+    public static partial class SharedVerifySettings
+    {
+        static Dictionary<string, StreamConverter> extensionConverters = new Dictionary<string, StreamConverter>();
+
+        internal static bool TryGetConverter(string extension, out StreamConverter converter)
+        {
+            return extensionConverters.TryGetValue(extension, out converter);
+        }
+
+        public static void RegisterFileConverter(
+            string fromExtension,
+            string toExtension,
+            Func<Stream, IEnumerable<Stream>> func)
+        {
+            Guard.AgainstNull(func, nameof(func));
+            RegisterFileConverter(fromExtension, toExtension, (stream, settings) => func(stream));
+        }
+
+        public static void RegisterFileConverter(
+            string fromExtension,
+            string toExtension,
+            Func<Stream, VerifySettings, IEnumerable<Stream>> func)
+        {
+            Guard.AgainstNull(func, nameof(func));
+            Guard.AgainstBadExtension(fromExtension, nameof(fromExtension));
+            Guard.AgainstBadExtension(toExtension, nameof(toExtension));
+            var converter = new StreamConverter(
+                toExtension,
+                func);
+            extensionConverters[fromExtension] = converter;
+        }
+    }
+}
--- src/Verify/Splitters/SharedVerifySettings_Typed.cs
+++ src/Verify/Splitters/SharedVerifySettings_Typed.cs
@@ -8,13 +8,7 @@ namespace Verify
 {
     public static partial class SharedVerifySettings
     {
-        static Dictionary<string, StreamConverter> extensionConverters = new Dictionary<string, StreamConverter>();
         static List<TypeConverter> typedConverters = new List<TypeConverter>();
-
-        internal static bool TryGetConverter(string extension, out StreamConverter converter)
-        {
-            return extensionConverters.TryGetValue(extension, out converter);
-        }
 
         internal static bool TryGetConverter<T>([NotNullWhen(true)] out TypeConverter? converter)
         {
@@ -27,29 +21,6 @@ namespace Verify
 
             converter = null;
             return false;
-        }
-
-        public static void RegisterFileConverter(
-            string fromExtension,
-            string toExtension,
-            Func<Stream, IEnumerable<Stream>> func)
-        {
-            Guard.AgainstNull(func, nameof(func));
-            RegisterFileConverter(fromExtension, toExtension, (stream, settings) => func(stream));
-        }
-
-        public static void RegisterFileConverter(
-            string fromExtension,
-            string toExtension,
-            Func<Stream, VerifySettings, IEnumerable<Stream>> func)
-        {
-            Guard.AgainstNull(func, nameof(func));
-            Guard.AgainstBadExtension(fromExtension, nameof(fromExtension));
-            Guard.AgainstBadExtension(toExtension, nameof(toExtension));
-            var converter = new StreamConverter(
-                toExtension,
-                func);
-            extensionConverters[fromExtension] = converter;
         }
 
         public static void RegisterFileConverter<T>(

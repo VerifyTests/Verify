0e29f75e99c45de146d8178ea96024e45f42ac34
SimonCropp <simon.cropp@gmail.com>
2020-01-18 14:28:57 +1100

    .

---
+++ src/Verify.Tests/DiffTool/ProcessFinderTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+public class ProcessFinderTest :
+    VerifyBase
+{
+    [Fact]
+    public void Find()
+    {
+        var startNew = Stopwatch.StartNew();
+        var enumerable = ProcessFinder.Find().ToList();
+        WriteLine(startNew.ElapsedMilliseconds);
+        foreach (var x in enumerable)
+        {
+            Debug.WriteLine(x);
+        }
+    }
+
+    public ProcessFinderTest(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
---
+++ src/Verify/DiffTool/ProcessFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+
+static class ProcessFinder
+{
+    public static IEnumerable<string> Find()
+    {
+        var wmiQuery = @"
+select CommandLine
+from Win32_Process
+where (NOT (CommandLine Is Null)) and
+      CommandLine like '%"" ""%'";
+        var searcher = new ManagementObjectSearcher(wmiQuery);
+        var retObjectCollection = searcher.Get();
+        foreach (var retObject in retObjectCollection)
+        {
+            var arg0 = (string) retObject["CommandLine"];
+            yield return arg0;
+        }
+    }
+}

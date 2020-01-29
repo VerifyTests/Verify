642572e724b6be05fed9a2f75dbf3dea311bb88e
SimonCropp <simon.cropp@gmail.com>
2020-01-29 21:04:01 +1100

    .

--- src/DiffEngine/DiffRunner.cs
+++ src/DiffEngine/DiffRunner.cs
@@ -52,7 +52,7 @@ namespace DiffEngine
             Launch(diffTool, path1, path2);
         }
 
-        public static void Launch(ResolvedDiffTool tool, string path1, string path2)
+        internal static void Launch(ResolvedDiffTool tool, string path1, string path2)
         {
             Guard.AgainstNull(tool, nameof(tool));
             var command = tool.BuildCommand(path1, path2);
--- src/DiffEngine/DiffTools.cs
+++ src/DiffEngine/DiffTools.cs
@@ -1,57 +1,55 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using DiffEngine;
 
-namespace DiffEngine
+static class DiffTools
 {
-    public static class DiffTools
+    public static Dictionary<string, ResolvedDiffTool> ExtensionLookup = new Dictionary<string, ResolvedDiffTool>();
+    public static List<ResolvedDiffTool> ResolvedDiffTools = new List<ResolvedDiffTool>();
+
+    internal static List<DiffTool> Tools()
     {
-        public static Dictionary<string, ResolvedDiffTool> ExtensionLookup = new Dictionary<string, ResolvedDiffTool>();
-        public static List<ResolvedDiffTool> ResolvedDiffTools = new List<ResolvedDiffTool>();
-
-        internal static List<DiffTool> Tools()
+        return new List<DiffTool>
         {
-            return new List<DiffTool>
-            {
-               Implementation.VisualStudio(),
-               Implementation.VsCode(),
-               Implementation.WinMerge(),
-               Implementation.CodeCompare(),
-               Implementation.SublimeMerge(),
-               Implementation.Meld(),
-               Implementation.AraxisMerge(),
-               Implementation.P4Merge(),
-               Implementation.BeyondCompare()
-            };
-        }
+            Implementation.VisualStudio(),
+            Implementation.VsCode(),
+            Implementation.WinMerge(),
+            Implementation.CodeCompare(),
+            Implementation.SublimeMerge(),
+            Implementation.Meld(),
+            Implementation.AraxisMerge(),
+            Implementation.P4Merge(),
+            Implementation.BeyondCompare()
+        };
+    }
 
-        static DiffTools()
+    static DiffTools()
+    {
+        foreach (var tool in Tools().Where(x => x.Exists))
         {
-            foreach (var tool in Tools().Where(x => x.Exists))
+            var diffTool = new ResolvedDiffTool(
+                tool.Name,
+                tool.ExePath!,
+                tool.BuildArguments,
+                tool.IsMdi,
+                tool.SupportsAutoRefresh);
+            ResolvedDiffTools.Add(diffTool);
+            foreach (var ext in tool.BinaryExtensions)
             {
-                var diffTool = new ResolvedDiffTool(
-                    tool.Name,
-                    tool.ExePath!,
-                    tool.BuildArguments,
-                    tool.IsMdi,
-                    tool.SupportsAutoRefresh);
-                ResolvedDiffTools.Add(diffTool);
-                foreach (var ext in tool.BinaryExtensions)
-                {
-                    ExtensionLookup[ext] = diffTool;
-                }
+                ExtensionLookup[ext] = diffTool;
             }
         }
+    }
 
-        public static bool TryFind(string extension, [NotNullWhen(true)] out ResolvedDiffTool? tool)
+    public static bool TryFind(string extension, [NotNullWhen(true)] out ResolvedDiffTool? tool)
+    {
+        if (Extensions.IsTextExtension(extension))
         {
-            if (Extensions.IsTextExtension(extension))
-            {
-                tool = ResolvedDiffTools.LastOrDefault();
-                return tool != null;
-            }
-
-            return ExtensionLookup.TryGetValue(extension, out tool);
+            tool = ResolvedDiffTools.LastOrDefault();
+            return tool != null;
         }
+
+        return ExtensionLookup.TryGetValue(extension, out tool);
     }
 }

2f6488392cc253ec8b715326c0ddab053983cb83
SimonCropp <simon.cropp@gmail.com>
2020-03-28 06:54:23 +1100

    add namespace test

---
+++ src/Verify.Tests/NamerInNamespaceTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Verify;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TheNamesapce
+{
+    public class NamerInNamespaceTests :
+        VerifyBase
+    {
+        [Fact]
+        public Task Runtime()
+        {
+            var settings = new VerifySettings();
+            settings.UniqueForRuntime();
+            return Verify(Namer.Runtime, settings);
+        }
+
+        [Fact]
+        public Task RuntimeAndVersion()
+        {
+            var settings = new VerifySettings();
+            settings.UniqueForRuntimeAndVersion();
+            return Verify(Namer.RuntimeAndVersion, settings);
+        }
+
+        [Fact]
+        public void AccessNamerRuntimeAndVersion()
+        {
+            Debug.WriteLine(Namer.Runtime);
+            Debug.WriteLine(Namer.RuntimeAndVersion);
+        }
+
+        [Fact]
+        public Task AssemblyConfiguration()
+        {
+            var settings = new VerifySettings();
+            settings.UniqueForAssemblyConfiguration();
+            return Verify("Foo", settings);
+        }
+
+        public NamerInNamespaceTests(ITestOutputHelper output) :
+            base(output)
+        {
+        }
+    }
+}

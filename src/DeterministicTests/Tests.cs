6aa9671e83726b9e6d5ace728393e5326037919c
Simon Cropp <simon.cropp@gmail.com>
2020-03-31 06:58:18 +1100

    Suppress Deterministic (#70)

---
+++ src/DeterministicTests/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiffEngine;
+using EmptyFiles;
+using Verify;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+public class Tests :
+    VerifyBase
+{
+    [Fact]
+    public Task Simple()
+    {
+        return Verify("Foo");
+    }
+
+    public Tests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}

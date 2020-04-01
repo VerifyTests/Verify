e3f0ccec484286e9671341e6ecfb53790c03cd35
SimonCropp <simon.cropp@gmail.com>
2020-04-01 13:03:39 +1100

    split converter

--- src/Verify/Serialization/WriteOnlyJsonConverter.cs
+++ src/Verify/Serialization/WriteOnlyJsonConverter.cs
@@ -11,21 +11,4 @@ namespace Verify
             throw new NotImplementedException();
         }
     }
-
-    public abstract class WriteOnlyJsonConverter<T> :
-        WriteOnlyJsonConverter
-        where T: class
-    {
-        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
-        {
-            WriteJson(writer, (T?) value, serializer);
-        }
-
-        public abstract void WriteJson(JsonWriter writer, T? fragment, JsonSerializer serializer);
-
-        public override bool CanConvert(Type type)
-        {
-            return typeof(T).IsAssignableFrom(type);
-        }
-    }
 }
---
+++ src/Verify/Serialization/WriteOnlyJsonConverterT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Verify
+{
+    public abstract class WriteOnlyJsonConverter<T> :
+        WriteOnlyJsonConverter
+        where T: class
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            WriteJson(writer, (T?) value, serializer);
+        }
+
+        public abstract void WriteJson(JsonWriter writer, T? fragment, JsonSerializer serializer);
+
+        public override bool CanConvert(Type type)
+        {
+            return typeof(T).IsAssignableFrom(type);
+        }
+    }
+}

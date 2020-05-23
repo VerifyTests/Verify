c7bf9e9bf4ada53e9e11797da6665546002ba485
SimonCropp <simon.cropp@gmail.com>
2020-05-24 09:41:28 +1000

    Update Verifier_Json.cs

--- src/Verify.NUnit/Verifier_Json.cs
+++ src/Verify.NUnit/Verifier_Json.cs
@@ -8,21 +8,21 @@ namespace VerifyNUnit
     public static partial class Verifier
     {
         public static async Task Verify<T>(
-            Task<T> task,
+            Task<T> target,
             VerifySettings? settings = null,
             [CallerFilePath] string sourceFile = "")
         {
             using var verifier = BuildVerifier(sourceFile);
-            await verifier.Verify(task, settings);
+            await verifier.Verify(target, settings);
         }
 
         public static Task Verify<T>(
-            IAsyncEnumerable<T> enumerable,
+            IAsyncEnumerable<T> target,
             VerifySettings? settings = null,
             [CallerFilePath] string sourceFile = "")
         {
             var verifier = BuildVerifier(sourceFile);
-            return verifier.Verify(enumerable, settings);
+            return verifier.Verify(target, settings);
         }
 
         public static async Task Verify<T>(

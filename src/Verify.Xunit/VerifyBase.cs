af777f4991e86fa39a7c139876f5f0f773d9b298
SimonCropp <simon.cropp@gmail.com>
2020-06-09 23:42:01 +1000

    Create VerifyBase.cs

---
+++ src/Verify.Xunit/VerifyBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VerifyXunit
+{
+    [Obsolete(
+        "VerifyBase is no longer required. Instead use the static Verifier.Verify().",
+        error: true)]
+    public class VerifyBase
+    {
+
+    }
+}

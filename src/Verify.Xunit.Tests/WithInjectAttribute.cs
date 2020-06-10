2666d4fa7d6b0b3fed5cbc078fa11476789da849
SimonCropp <simon.cropp@gmail.com>
2020-06-10 19:37:59 +1000

    more tests

---
+++ src/Verify.Xunit.Tests/WithInjectAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+[InjectInfo]
+public class WithInjectAttribute
+{
+    [Fact]
+    public Task Test()
+    {
+        return Verifier.Verify("Result");
+    }
+}

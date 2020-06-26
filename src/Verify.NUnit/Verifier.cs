c2720de19ab6dbd9e5c703703aac68094118df85
SimonCropp <simon.cropp@gmail.com>
2020-06-26 13:25:28 +1000

    Update Verifier.cs

--- src/Verify.NUnit/Verifier.cs
+++ src/Verify.NUnit/Verifier.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -8,15 +9,25 @@ namespace VerifyNUnit
 {
     public static partial class Verifier
     {
-        static FieldInfo field = typeof(TestContext.TestAdapter)
-            .GetField("_test", BindingFlags.Instance | BindingFlags.NonPublic);
+        static FieldInfo field;
+
+        static Verifier()
+        {
+            var temp = typeof(TestContext.TestAdapter)
+                .GetField("_test", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (temp == null)
+            {
+                throw new Exception("Could not find field `_test` on TestContext.TestAdapter.");
+            }
+            field = temp;
+        }
 
         static InnerVerifier BuildVerifier(string sourceFile)
         {
             Guard.AgainstNullOrEmpty(sourceFile, nameof(sourceFile));
             var context = TestContext.CurrentContext;
             var adapter = context.Test;
-            var test = (Test) field.GetValue(adapter);
+            var test = (Test) field.GetValue(adapter)!;
 
             var method = test.Method.MethodInfo;
             var name = TestNameBuilder.GetUniqueTestName(Path.GetFileNameWithoutExtension(sourceFile), method, adapter.Arguments);

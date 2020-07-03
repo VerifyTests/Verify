3660c4b362d172ba14032eee198e927cab0ff5d5
SimonCropp <simon.cropp@gmail.com>
2020-07-03 18:41:52 +1000

    Update VerifySettings.cs

--- src/Verify/VerifySettings.cs
+++ src/Verify/VerifySettings.cs
@@ -5,10 +5,6 @@ using System.Text;
 
 namespace VerifyTests
 {
-}
-
-namespace VerifyTests
-{
     public partial class VerifySettings
     {
         internal static VerifySettings Default = new VerifySettings();

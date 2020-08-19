328d9eb23b98f540c203676bdeb486f6f45ec4a7
Simon Cropp <simon.cropp@gmail.com>
2020-08-19 15:51:08 +1000

    add quote toggling

---
+++ src/Verify/Serialization/JsonTextWriterEx.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+class JsonTextWriterEx :
+    JsonTextWriter
+{
+    public JsonTextWriterEx(TextWriter writer) :
+        base(writer)
+    {
+    }
+
+    public override void WriteValue(string? value)
+    {
+        if (value != null)
+        {
+            if (value.Contains("'") && !value.Contains("\""))
+            {
+                QuoteChar = '"';
+                base.WriteValue(value);
+                QuoteChar = JsonFormatter.QuoteChar;
+                return;
+            }
+        }
+
+        base.WriteValue(value);
+    }
+}

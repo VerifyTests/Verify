0af51082f276d1a1a22c81757c72865c327a70cb
Simon Cropp <simon.cropp@gmail.com>
2020-09-09 09:50:59 +1000

    .

---
+++ src/Verify.Tests/Converters/JsonAppenderTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public class JsonAppenderTests
+{
+    static JsonAppenderTests()
+    {
+        VerifierSettings.RegisterJsonAppender(
+            settings =>
+            {
+                if (!settings.SourceFile.Contains("JsonAppenderTests"))
+                {
+                    return null;
+                }
+
+                return new ToAppend("theData", "theValue");
+            });
+    }
+
+    [Fact]
+    public Task Text()
+    {
+        return Verifier.Verify("Foo");
+    }
+
+    [Fact]
+    public Task Anon()
+    {
+        return Verifier.Verify(new {foo = "bar"});
+    }
+
+    [Fact]
+    public Task Stream()
+    {
+        return Verifier.Verify(FileHelpers.OpenRead("sample.txt"));
+    }
+
+    [Fact]
+    public Task File()
+    {
+        return Verifier.VerifyFile("sample.txt");
+    }
+}
---
+++ src/Verify/Splitters/Settings_JsonAppender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace VerifyTests
+{
+    public static partial class VerifierSettings
+    {
+        static ConcurrentBag<JsonAppender> jsonAppenders = new ConcurrentBag<JsonAppender>();
+
+        internal static IEnumerable<ToAppend> GetJsonAppenders(VerifySettings settings)
+        {
+            foreach (var appender in jsonAppenders)
+            {
+                var data = appender(settings);
+                if (data != null)
+                {
+                    yield return data.Value;
+                }
+            }
+        }
+
+        public static void RegisterJsonAppender(JsonAppender appender)
+        {
+            Guard.AgainstNull(appender, nameof(appender));
+            jsonAppenders.Add(appender);
+        }
+    }
+}

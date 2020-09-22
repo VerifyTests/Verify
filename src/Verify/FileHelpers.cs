577f62ac079d57aa3d3707f1f47e0054ae180fcf
Simon Cropp <simon.cropp@gmail.com>
2020-09-22 20:46:53 +1000

    .

--- src/Verify/FileHelpers.cs
+++ src/Verify/FileHelpers.cs
@@ -2,7 +2,7 @@
 using System.Text;
 using System.Threading.Tasks;
 
-static partial class FileHelpers
+static class FileHelpers
 {
     public static readonly Encoding Utf8NoBOM = new UTF8Encoding(false, true);
 
@@ -40,4 +40,43 @@ static partial class FileHelpers
     {
         return stream.ReadAsString();
     }
+
+#if NETSTANDARD2_1 || NET5_0
+    public static Task WriteText(string filePath, string text)
+    {
+        return File.WriteAllTextAsync(filePath, text);
+    }
+
+    public static async Task<StringBuilder> ReadText(string filePath)
+    {
+        await using var stream = OpenRead(filePath);
+        return await ReadText(stream);
+    }
+
+    public static async Task WriteStream(string filePath, Stream stream)
+    {
+        await using var fileStream = OpenWrite(filePath);
+        await stream.CopyToAsync(fileStream);
+    }
+#else
+    public static async Task WriteText(string filePath, string text)
+    {
+        var encodedText = Utf8NoBOM.GetBytes(text);
+
+        using var fileStream = OpenWrite(filePath);
+        await fileStream.WriteAsync(encodedText, 0, encodedText.Length);
+    }
+
+    public static async Task WriteStream(string filePath, Stream stream)
+    {
+        using var fileStream = OpenWrite(filePath);
+        await stream.CopyToAsync(fileStream);
+    }
+
+    public static async Task<StringBuilder> ReadText(string filePath)
+    {
+        using var stream = OpenRead(filePath);
+        return await ReadText(stream);
+    }
+#endif
 }

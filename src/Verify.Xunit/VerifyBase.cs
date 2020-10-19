101cfed712b467b857b3d29dcbe6817b05600853
Daniel Weber <dw@exram.de>
2020-10-19 15:54:54 +0200

    [WIP] Throw early when VerifyBase-derived classes fail to call the base constructor explicitly.

--- src/Verify.Xunit/VerifyBase.cs
+++ src/Verify.Xunit/VerifyBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using VerifyTests;
 
 namespace VerifyXunit
@@ -11,6 +12,9 @@ namespace VerifyXunit
 
         public VerifyBase(VerifySettings? settings = null, [CallerFilePath] string sourceFile = "")
         {
+            if ((settings == null) && (sourceFile == ""))
+                throw new InvalidOperationException($"{nameof(VerifyBase)}.ctor must be called explicitly.");
+
             this.settings = settings;
             this.sourceFile = sourceFile;
         }

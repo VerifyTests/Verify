57d2531bcd7519b755c623de41f35e549547eb92
Simon Cropp <simon.cropp@gmail.com>
2020-10-30 22:27:59 +1100

    Update ReflectionExtensions.cs

--- src/Verify/ReflectionExtensions.cs
+++ src/Verify/ReflectionExtensions.cs
@@ -1,31 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using VerifyTests;
 
 static class ReflectionExtensions
 {
-    public static List<Type> InstanceTypes(this Assembly assembly)
-    {
-        return assembly.GetExportedTypes()
-            .Where(x => !x.IsAbstract && !x.IsNested)
-            .ToList();
-    }
-
-    const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy;
-
-    public static MethodInfo GetPublicMethod(this Type type, string method)
-    {
-        var methodInfo = type.GetMethod(method, flags);
-        if (methodInfo != null)
-        {
-            return methodInfo;
-        }
-
-        throw InnerVerifier.exceptionBuilder($"Method `{method}` not found on type `{type.Name}`.");
-    }
-
     public static bool IsException(this Type type)
     {
         return typeof(Exception).IsAssignableFrom(type);

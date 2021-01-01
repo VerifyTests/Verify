74d5d55fe7070ae8a8b5b82bbaf4ebf9147cd4f2
Simon Cropp <simon.cropp@gmail.com>
2021-01-02 09:47:59 +1100

    .

---
+++ src/Verify.Xunit.Tests/Inheritance/Base.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public abstract class Base
+{
+    [Fact]
+    public Task TestInBase()
+    {
+        return Verifier.Verify("Foo");
+    }
+    [Fact]
+    public virtual Task TestToOverride()
+    {
+        return Verifier.Verify("Foo");
+    }
+}
---
+++ src/Verify.Xunit.Tests/Inheritance/Inherited.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using VerifyXunit;
+
+[UsesVerify]
+public class Inherited : Base
+{
+    public override Task TestToOverride()
+    {
+        Trace.WriteLine("");
+        return base.TestToOverride();
+    }
+}

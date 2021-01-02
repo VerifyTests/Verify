c1b7524f61c05fc36b927022faa1935fdf863ae1
Simon Cropp <simon.cropp@gmail.com>
2021-01-02 12:48:19 +1100

    add integration test for DeriveTestDirectory

---
+++ src/Verify.DerivePaths.Tests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public class Tests
+{
+    static Tests()
+    {
+        VerifierSettings.DeriveTestDirectory((sourceFile, directory) =>
+        {
+            Assert.True(File.Exists(sourceFile));
+            Assert.True(Directory.Exists(directory));
+            Assert.EndsWith("Verify.DerivePaths.Tests/Tests.cs", sourceFile.Replace(@"\","/"));
+            Assert.EndsWith("Verify.DerivePaths.Tests/", directory.Replace(@"\","/"));
+            return Path.Combine(directory, "Custom");
+        });
+    }
+
+    [Fact]
+    public async Task Test()
+    {
+        await Verifier.Verify("Value");
+    }
+}

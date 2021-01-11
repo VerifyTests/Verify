f6e5dd853bb02a1628d106478b990fd43aea5062
Simon Cropp <simon.cropp@gmail.com>
2021-01-12 09:56:43 +1100

    remove Compare(this StringBuilder

--- src/Verify/StringBuilderExtensions.cs
+++ src/Verify/StringBuilderExtensions.cs
@@ -8,26 +8,6 @@ static class StringBuilderExtensions
         builder.Replace("\r", "\n");
     }
 
-    public static bool Compare(this StringBuilder builder1, StringBuilder builder2)
-    {
-        if (builder1.Length != builder2.Length)
-        {
-            return false;
-        }
-
-        for (var index = 0; index < builder1.Length; index++)
-        {
-            var char1 = builder1[index];
-            var char2 = builder2[index];
-            if (char1 != char2)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public static void TrimEnd(this StringBuilder builder)
     {
         if (builder.Length == 0)

402d5cf448be36a135d44e0a2df34b93e5b57f53
Simon Cropp <simon.cropp@gmail.com>
2021-01-13 19:15:45 +1100

    Update InnerVerifier_Text.cs

--- src/Verify/Verifier/InnerVerifier_Text.cs
+++ src/Verify/Verifier/InnerVerifier_Text.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 
 namespace VerifyTests
@@ -10,32 +8,6 @@ namespace VerifyTests
         Task VerifyString(string target)
         {
             return VerifyBinary(Enumerable.Empty<ConversionStream>(),target, null);
-        }
-
-        async Task VerifyStringBuilder(StringBuilder target, string extension)
-        {
-            ApplyScrubbers.Apply(target, settings.instanceScrubbers);
-
-            VerifyEngine engine = new(settings, fileNameBuilder);
-
-            var received = target.ToString();
-            List<ResultBuilder> builders = new()
-            {
-                new(extension, file => Comparer.Text(file, received, settings))
-            };
-
-            builders.AddRange(
-                VerifierSettings.GetFileAppenders(settings)
-                    .Select(appender =>
-                    {
-                        return new ResultBuilder(
-                            appender.Extension,
-                            file => GetResult(settings, file, appender));
-                    }));
-
-            await engine.HandleResults(builders);
-
-            await engine.ThrowIfRequired();
         }
     }
 }

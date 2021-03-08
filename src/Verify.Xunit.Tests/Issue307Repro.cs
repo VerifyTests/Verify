18458b8bae3dcc74099ad21c14742acfb9f03b1c
Daniel Weber <dw@exram.de>
2021-03-08 11:07:33 +0100

    Repro for #307. Despite settings the extension to json in the settings, the *.received.* file will have the extension txt.

---
+++ src/Verify.Xunit.Tests/Issue307Repro.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+namespace Verify.Xunit.Tests
+{
+    [UsesVerify]
+    public class Issue307Repro
+    {
+        public class Element
+        {
+            public string? Id { get; set; }
+        }
+
+        [Fact]
+        public async Task Repro()
+        {
+            var settings = new VerifySettings();
+            settings.UseExtension("json");
+
+            await Verifier
+                .Verify(new Element(), settings);
+        }
+    }
+}

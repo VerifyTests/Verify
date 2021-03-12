fb81597d3be2e8742db3b337c666acaab7b9d30b
Simon Cropp <simon.cropp@gmail.com>
2021-03-12 12:56:52 +1100

    fix clipboard disable

---
+++ src/DisableClipboard/DisableClipboardTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using DiffEngine;
+using TextCopy;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+[UsesVerify]
+public class DisableClipboardTests
+{
+    static DisableClipboardTests()
+    {
+        VerifierSettings.DisableClipboard();
+        DiffRunner.Disabled = true;
+    }
+
+    [Fact]
+    public async Task String()
+    {
+        try
+        {
+            await Verifier.Verify("Foo");
+        }
+        catch
+        {
+        }
+
+        await AssertClipboardNotEffected();
+    }
+
+    static async Task AssertClipboardNotEffected()
+    {
+        var text = await ClipboardService.GetTextAsync();
+        if (text == null)
+        {
+            return;
+        }
+
+        Assert.DoesNotContain("DisableClipboardTests", text);
+    }
+}
--- src/Verify/Clipboard/VerifierSettings.cs
+++ src/Verify/Clipboard/VerifierSettings.cs
@@ -6,7 +6,7 @@
 
         public static void DisableClipboard()
         {
-            clipboardDisabled = false;
+            clipboardDisabled = true;
         }
     }
 }

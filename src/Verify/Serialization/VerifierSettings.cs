e8cc6141bebafaca736c198c33ca4c1d25c325b9
Simon Cropp <simon.cropp@gmail.com>
2021-05-20 14:42:21 +1000

    .

--- src/Verify/Serialization/VerifierSettings.cs
+++ src/Verify/Serialization/VerifierSettings.cs
@@ -73,7 +73,7 @@ namespace VerifyTests
             {typeof(ulong), (target, _) => ((ulong) target).ToString()},
             {typeof(decimal), (target, _) => ((decimal) target).ToString(CultureInfo.InvariantCulture)},
 #if NET5_0_OR_GREATER
-            //   {typeof(Half), (target, settings) => ((Half) target).ToString(CultureInfo.InvariantCulture)},
+            {typeof(Half), (target, settings) => ((Half) target).ToString(CultureInfo.InvariantCulture)},
 #endif
             {typeof(float), (target, _) => ((float) target).ToString(CultureInfo.InvariantCulture)},
             {typeof(double), (target, _) => ((double) target).ToString(CultureInfo.InvariantCulture)},

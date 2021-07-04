eaf498da942468366a4524d4b58d59c3e2fed130
Simon Cropp <simon.cropp@gmail.com>
2021-07-04 17:44:53 +1000

    expose ignore members in settings

--- src/Verify/Serialization/VerifySettings.cs
+++ src/Verify/Serialization/VerifySettings.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace VerifyTests
@@ -7,6 +9,32 @@ namespace VerifyTests
     {
         internal SerializationSettings serialization = VerifierSettings.serialization;
         bool isCloned;
+
+        public IReadOnlyList<string> IgnoredByNameMembers
+        {
+            get
+            {
+                return serialization.ignoredByNameMembers;
+            }
+        }
+
+        public IReadOnlyDictionary<Type, IReadOnlyList<string>> IgnoredMembers
+        {
+            get
+            {
+                return serialization.ignoredMembers
+                    .ToDictionary(x => x.Key, x => (IReadOnlyList<string>)x.Value);
+            }
+        }
+
+        public IReadOnlyDictionary<Type, IReadOnlyList<Func<object, bool>>> IgnoredInstances
+        {
+            get
+            {
+                return serialization.ignoredInstances
+                    .ToDictionary(x => x.Key, x => (IReadOnlyList<Func<object, bool>>)x.Value);
+            }
+        }
 
         public void ModifySerialization(Action<SerializationSettings> action)
         {

b445bd005067d162800636e46c39ba21ddbbe5b9
Simon Cropp <simon.cropp@gmail.com>
2021-09-21 12:50:59 +1000

    .

--- src/Verify/Naming/ReflectionFileNameBuilder.cs
+++ src/Verify/Naming/ReflectionFileNameBuilder.cs
@@ -1,81 +1,77 @@
 ﻿using System.Linq;
+using VerifyTests;
 
-namespace VerifyTests
+class ReflectionFileNameBuilder
 {
-    /// <summary>
-    /// Not for public use.
-    /// </summary>
-    public class ReflectionFileNameBuilder
+    public static (string fileNamePrefix, string? directory) FileNamePrefix(
+        MethodInfo method, 
+        Type type, 
+        string sourceFile, 
+        VerifySettings settings,
+        string uniqueness)
     {
-        public static (string fileNamePrefix, string? directory) FileNamePrefix(
-            MethodInfo method, 
-            Type type, 
-            string sourceFile, 
-            VerifySettings settings,
-            string uniqueness)
-        {
-            var pathInfo = VerifierSettings.GetPathInfo(sourceFile, type, method);
+        var pathInfo = VerifierSettings.GetPathInfo(sourceFile, type, method);
 
-            var fileNamePrefix = GetFileNamePrefix(method, type, settings, pathInfo, uniqueness);
-            var directory = settings.Directory ?? pathInfo.Directory;
-            return (fileNamePrefix, directory);
+        var fileNamePrefix = GetFileNamePrefix(method, type, settings, pathInfo, uniqueness);
+        var directory = settings.Directory ?? pathInfo.Directory;
+        return (fileNamePrefix, directory);
+    }
+
+    static string GetFileNamePrefix(MethodInfo method, Type type, VerifySettings settings, PathInfo pathInfo, string uniqueness)
+    {
+        if (settings.fileName is not null)
+        {
+            return settings.fileName + uniqueness;
         }
 
-        static string GetFileNamePrefix(MethodInfo method, Type type, VerifySettings settings, PathInfo pathInfo, string uniqueness)
+        var typeName = settings.typeName ?? pathInfo.TypeName ?? GetTypeName(type);
+        var methodName = settings.methodName ?? pathInfo.MethodName ?? method.Name;
+
+        var parameterText = GetParameterText(method, settings);
+
+        return $"{typeName}.{methodName}{parameterText}{uniqueness}";
+    }
+
+    static string GetParameterText(MethodInfo method, VerifySettings settings)
+    {
+        if (settings.parametersText is not null)
         {
-            if (settings.fileName is not null)
-            {
-                return settings.fileName + uniqueness;
-            }
-
-            var typeName = settings.typeName ?? pathInfo.TypeName ?? GetTypeName(type);
-            var methodName = settings.methodName ?? pathInfo.MethodName ?? method.Name;
-
-            var parameterText = GetParameterText(method, settings);
-
-            return $"{typeName}.{methodName}{parameterText}{uniqueness}";
+            return $"_{settings.parametersText}";
         }
 
-        static string GetParameterText(MethodInfo method, VerifySettings settings)
+        var methodParameters = method.GetParameters();
+        if (methodParameters.IsEmpty())
         {
-            if (settings.parametersText is not null)
-            {
-                return $"_{settings.parametersText}";
-            }
-
-            var methodParameters = method.GetParameters();
-            if (methodParameters.IsEmpty())
-            {
-                return "";
-            }
-
-            var settingsParameters = settings.parameters;
-            if (settingsParameters is null)
-            {
-                throw BuildMissingParametersException(method, methodParameters);
-            }
-
-            if (methodParameters.Length != settingsParameters.Length)
-            {
-                throw new($"The number of passed in parameters ({settingsParameters.Length}) must match the number of parameters for the method ({methodParameters.Length}).");
-            }
-
-            var dictionary = new Dictionary<string, object?>();
-            for (var index = 0; index < methodParameters.Length; index++)
-            {
-                var parameter = methodParameters[index];
-                var value = ((IReadOnlyList<object?>)settingsParameters)[index];
-                dictionary[parameter.Name!] = value;
-            }
-
-            var concat = ParameterBuilder.Concat(dictionary);
-            return $"_{concat}";
+            return "";
         }
 
-        static Exception BuildMissingParametersException(MethodInfo method, ParameterInfo[] parameters)
+        var settingsParameters = settings.parameters;
+        if (settingsParameters is null)
         {
-            var names = string.Join(", ", parameters.Select(x => x.Name));
-            return new($@"Method `{method.DeclaringType!.Name}.{method.Name}` requires parameters, but none have been defined. Add UseParameters. For example:
+            throw BuildMissingParametersException(method, methodParameters);
+        }
+
+        if (methodParameters.Length != settingsParameters.Length)
+        {
+            throw new($"The number of passed in parameters ({settingsParameters.Length}) must match the number of parameters for the method ({methodParameters.Length}).");
+        }
+
+        var dictionary = new Dictionary<string, object?>();
+        for (var index = 0; index < methodParameters.Length; index++)
+        {
+            var parameter = methodParameters[index];
+            var value = ((IReadOnlyList<object?>)settingsParameters)[index];
+            dictionary[parameter.Name!] = value;
+        }
+
+        var concat = ParameterBuilder.Concat(dictionary);
+        return $"_{concat}";
+    }
+
+    static Exception BuildMissingParametersException(MethodInfo method, ParameterInfo[] parameters)
+    {
+        var names = string.Join(", ", parameters.Select(x => x.Name));
+        return new($@"Method `{method.DeclaringType!.Name}.{method.Name}` requires parameters, but none have been defined. Add UseParameters. For example:
 
 var settings = new VerifySettings();
 settings.UseParameters({names});
@@ -85,16 +81,15 @@ or
 
 await Verifier.Verify(target).UseParameters({names});
 ");
-        }
+    }
 
-        static string GetTypeName(Type type)
+    static string GetTypeName(Type type)
+    {
+        if (type.IsNested)
         {
-            if (type.IsNested)
-            {
-                return $"{type.ReflectedType!.Name}.{type.Name}";
-            }
-
-            return type.Name;
+            return $"{type.ReflectedType!.Name}.{type.Name}";
         }
+
+        return type.Name;
     }
 }
--- src/Verify/Verifier/GetFileConvention.cs
+++ src/Verify/Verifier/GetFileConvention.cs
@@ -1,7 +1,1 @@
-﻿namespace VerifyTests
-{
-    /// <summary>
-    /// Not for public use.
-    /// </summary>
-    public  delegate (string fileNamePrefix, string? directory) GetFileConvention(string uniqueness);
-}
+﻿internal delegate (string fileNamePrefix, string? directory) GetFileConvention(string uniqueness);

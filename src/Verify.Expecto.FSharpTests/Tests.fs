d2e51e6e9707e99161d15209447c990c7c8398bd
Simon Cropp <simon.cropp@gmail.com>
2021-09-21 10:59:52 +1000

    Update Tests.fs

--- src/Verify.Expecto.FSharpTests/Tests.fs
+++ src/Verify.Expecto.FSharpTests/Tests.fs
@@ -13,20 +13,6 @@ let tests =
       let subject = false
       Expect.isTrue subject "I should fail because the subject is false"
 
-    testCase "I'm skipped (should skip)" <| fun _ ->
-      Tests.skiptest "Yup, waiting for a sunny day..."
-
-    testCase "I'm always fail (should fail)" <| fun _ ->
-      Tests.failtest "This was expected..."
-
-    testCase "contains things" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4 |]
-                         "This is the case; {2,3,4} contains {2,4}"
-
-    testCase "contains things (should fail)" <| fun _ ->
-      Expect.containsAll [| 2; 3; 4 |] [| 2; 4; 1 |]
-                         "Expecting we have one (1) in there"
-
     testCase "Sometimes I want to ༼ノಠل͟ಠ༽ノ ︵ ┻━┻" <| fun _ ->
       Expect.equal "abcdëf" "abcdef" "These should equal"
 

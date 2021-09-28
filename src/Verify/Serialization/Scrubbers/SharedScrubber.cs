ce0b4bc8550d6d237ff60050a8d2a564cd90a2de
Simon Cropp <simon.cropp@gmail.com>
2021-09-29 08:49:34 +1000

    .

--- src/Verify/Serialization/Scrubbers/SharedScrubber.cs
+++ src/Verify/Serialization/Scrubbers/SharedScrubber.cs
@@ -1,14 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using Newtonsoft.Json;
 
-class SharedScrubber
+partial class SharedScrubber
 {
-    internal static List<string> dateFormats = new(){"d"};
-    internal static List<string> datetimeFormats = new();
-    internal static List<string> datetimeOffsetFormats = new();
-    bool scrubGuids;
-    bool scrubDateTimes;
     JsonSerializerSettings settings;
 
     public SharedScrubber(bool scrubGuids, bool scrubDateTimes, JsonSerializerSettings settings)
@@ -17,106 +11,7 @@ class SharedScrubber
         this.scrubDateTimes = scrubDateTimes;
         this.settings = settings;
     }
-
-    public bool TryConvert(DateTime value, [NotNullWhen(true)] out string? result)
-    {
-        if (!scrubDateTimes)
-        {
-            result = null;
-            return false;
-        }
-
-        result = Convert(value);
-        return true;
-    }
-
-#if NET6_0_OR_GREATER
-
-    public bool TryParseConvertDate(string value, [NotNullWhen(true)] out string? result)
-    {
-        if (scrubDateTimes)
-        {
-            foreach (var format in dateFormats)
-            {
-                if (DateOnly.TryParseExact(value, format, null, DateTimeStyles.None, out var date))
-                {
-                    result = Convert(date);
-                    return true;
-                }
-            }
-        }
-
-        result = null;
-        return false;
-    }
-
-    public bool TryConvert(DateOnly value, [NotNullWhen(true)] out string? result)
-    {
-        if (!scrubDateTimes)
-        {
-            result = null;
-            return false;
-        }
-
-        result = Convert(value);
-        return true;
-    }
-
-    static string Convert(DateOnly date)
-    {
-        var next = CounterContext.Current.Next(date);
-        return $"Date_{next}";
-    }
-
-#endif
-
-    public bool TryConvert(DateTimeOffset value, [NotNullWhen(true)] out string? result)
-    {
-        if (!scrubDateTimes)
-        {
-            result = null;
-            return false;
-        }
-
-        result = Convert(value);
-        return true;
-    }
     
-    public bool TryConvert(Guid value, [NotNullWhen(true)] out string? result)
-    {
-        if (!scrubGuids)
-        {
-            result = null;
-            return false;
-        }
-
-        result = Convert(value);
-        return true;
-    }
-
-    public static string Convert(Guid guid)
-    {
-        if (guid == Guid.Empty)
-        {
-            return $"Guid_Empty";
-        }
-
-        var next = CounterContext.Current.Next(guid);
-        return $"Guid_{next}";
-    }
-
-    static string Convert(DateTime dateTime)
-    {
-        var next = CounterContext.Current.Next(dateTime);
-        return $"DateTime_{next}";
-    }
-
-    static string Convert(DateTimeOffset dateTime)
-    {
-        var next = CounterContext.Current.Next(dateTime);
-        return $"DateTimeOffset_{next}";
-    }
-
     public bool TryConvertString(string value, [NotNullWhen(true)] out string? result)
     {
         if (TryParseConvertGuid(value, out result))
@@ -143,67 +38,5 @@ class SharedScrubber
         result = null;
         return false;
     }
-
-    public bool TryParseConvertDateTime(string value, [NotNullWhen(true)] out string? result)
-    {
-        if (scrubDateTimes)
-        {
-            if (DateTime.TryParseExact(value, settings.DateFormatString, null, DateTimeStyles.None, out var dateTime))
-            {
-                result = Convert(dateTime);
-                return true;
-            }
-
-            foreach (var format in datetimeFormats)
-            {
-                if (DateTime.TryParseExact(value, format, null, DateTimeStyles.None, out dateTime))
-                {
-                    result = Convert(dateTime);
-                    return true;
-                }
-            }
-        }
-
-        result = null;
-        return false;
-    }
-
-    public bool TryParseConvertDateTimeOffset(string value, [NotNullWhen(true)] out string? result)
-    {
-        if (scrubDateTimes)
-        {
-            if (DateTimeOffset.TryParseExact(value, settings.DateFormatString, null, DateTimeStyles.None, out var dateTimeOffset))
-            {
-                result = Convert(dateTimeOffset);
-                return true;
-            }
-
-            foreach (var format in datetimeOffsetFormats)
-            {
-                if (DateTimeOffset.TryParseExact(value, format, null, DateTimeStyles.None, out dateTimeOffset))
-                {
-                    result = Convert(dateTimeOffset);
-                    return true;
-                }
-            }
-        }
-        result = null;
-
-        return false;
-    }
-
-    public bool TryParseConvertGuid(string value, [NotNullWhen(true)] out string? result)
-    {
-        if (scrubGuids)
-        {
-            if (Guid.TryParse(value, out var guid))
-            {
-                result = Convert(guid);
-                return true;
-            }
-        }
-
-        result = null;
-        return false;
-    }
+    
 }
---
+++ src/Verify/Serialization/Scrubbers/SharedScrubber_Guids.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+partial class SharedScrubber
+{
+    bool scrubGuids;
+    
+    public bool TryConvert(Guid value, [NotNullWhen(true)] out string? result)
+    {
+        if (!scrubGuids)
+        {
+            result = null;
+            return false;
+        }
+
+        result = Convert(value);
+        return true;
+    }
+
+    public static string Convert(Guid guid)
+    {
+        if (guid == Guid.Empty)
+        {
+            return "Guid_Empty";
+        }
+
+        var next = CounterContext.Current.Next(guid);
+        return $"Guid_{next}";
+    }
+
+    public bool TryParseConvertGuid(string value, [NotNullWhen(true)] out string? result)
+    {
+        if (scrubGuids)
+        {
+            if (Guid.TryParse(value, out var guid))
+            {
+                result = Convert(guid);
+                return true;
+            }
+        }
+
+        result = null;
+        return false;
+    }
+}

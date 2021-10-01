bf769bce61d0fba9f314285e8253390a1fc396c1
Simon Cropp <simon.cropp@gmail.com>
2021-10-01 19:31:39 +1000

    split out logger recording

--- src/Verify.Tests/Tests.cs
+++ src/Verify.Tests/Tests.cs
@@ -1,6 +1,4 @@
 ﻿using System.Globalization;
-using System.Net.Http;
-using Microsoft.Extensions.Logging;
 using VerifyTests;
 using VerifyXunit;
 using Xunit;
@@ -59,103 +57,11 @@ public class Tests
         }
     }
 
-
     [Fact]
     public Task WithNewline()
     {
         return Verifier.Verify(new { Property = "F\roo" });
     }
-
-    #region LoggerRecordingTyped
-
-    [Fact]
-    public Task LoggingTyped()
-    {
-        var provider = LoggerRecording.Start();
-        var logger = provider.CreateLogger<ClassThatUsesTypedLogging>();
-        ClassThatUsesTypedLogging target = new(logger);
-
-        var result = target.Method();
-
-        return Verifier.Verify(result);
-    }
-
-    class ClassThatUsesTypedLogging
-    {
-        ILogger<ClassThatUsesTypedLogging> logger;
-
-        public ClassThatUsesTypedLogging(ILogger<ClassThatUsesTypedLogging> logger)
-        {
-            this.logger = logger;
-        }
-
-        public string Method()
-        {
-            logger.LogWarning("The log entry");
-            return "result";
-        }
-    }
-
-    #endregion
-
-    [Fact]
-    public Task LoggingComplexState()
-    {
-        var provider = LoggerRecording.Start();
-        provider.Log(LogLevel.Warning, default, new StateObject("Value1"), null, (_, _) => "The Message");
-        using (provider.BeginScope(new StateObject("Value2")))
-        {
-            provider.Log(LogLevel.Warning, default, new StateObject("Value3"), null, (_, _) => "Entry in scope");
-        }
-
-        return Verifier.Verify("Foo");
-    }
-
-    class StateObject
-    {
-        public string Property { get; }
-
-        public StateObject(string property)
-        {
-            Property = property;
-        }
-    }
-
-    #region LoggerRecording
-
-    [Fact]
-    public Task Logging()
-    {
-        var provider = LoggerRecording.Start();
-        ClassThatUsesLogging target = new(provider);
-
-        var result = target.Method();
-
-        return Verifier.Verify(result);
-    }
-
-    class ClassThatUsesLogging
-    {
-        ILogger logger;
-
-        public ClassThatUsesLogging(ILogger logger)
-        {
-            this.logger = logger;
-        }
-
-        public string Method()
-        {
-            logger.LogWarning("The log entry");
-            using (logger.BeginScope("The scope"))
-            {
-                logger.LogWarning("Entry in scope");
-            }
-
-            return "result";
-        }
-    }
-
-    #endregion
 
     [ModuleInitializer]
     public static void TreatAsStringInit()

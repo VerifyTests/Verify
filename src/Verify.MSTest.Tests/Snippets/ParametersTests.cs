f25b19c9b84c51b8710d8f5ef38329980e4c9f17
Simon Cropp <simon.cropp@gmail.com>
2021-10-15 11:01:26 +1100

    Update ParametersTests.cs

--- src/Verify.MSTest.Tests/Snippets/ParametersTests.cs
+++ src/Verify.MSTest.Tests/Snippets/ParametersTests.cs
@@ -7,13 +7,13 @@ namespace TheTests;
 public class ParametersTests :
     VerifyBase
 {
-    [DataTestMethod]
-    [DataRow("1.1")]
-    public async Task Decimal(decimal arg)
-    {
-        await Verify(arg)
-            .UseParameters(arg);
-    }
+    //[DataTestMethod]
+    //[DataRow("1.1")]
+    //public async Task Decimal(decimal arg)
+    //{
+    //    await Verify(arg)
+    //        .UseParameters(arg);
+    //}
 
     [DataTestMethod]
     [DataRow((float) 1.1)]

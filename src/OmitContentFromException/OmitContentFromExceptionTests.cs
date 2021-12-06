397a1444de09da0e6c5acbec10359c3259cca9bb
Simon Cropp <simon.cropp@gmail.com>
2021-12-06 13:00:39 +1100

    add OmitContentFromException (#446)

---
+++ src/OmitContentFromException/OmitContentFromExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿[UsesVerify]
+public class OmitContentFromExceptionTests
+{
+    static OmitContentFromExceptionTests()
+    {
+        VerifierSettings.OmitContentFromException();
+        VerifierSettings.DisableRequireUniquePrefix();
+    }
+
+    [Fact]
+    public async Task String()
+    {
+        try
+        {
+            await Verifier.Verify("Foo").DisableDiff();
+        }
+        catch (Exception exception)
+        {
+            await Verifier.Verify(exception.Message).ScrubLinesContaining("DiffEngineTray");
+        }
+    }
+}

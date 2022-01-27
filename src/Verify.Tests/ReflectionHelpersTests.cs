f35cb2052da5fcb677b0291ac48c2fdb1b0d4831
Simon Cropp <simon.cropp@gmail.com>
2022-01-28 10:40:05 +1100

    Create ReflectionHelpersTests.cs

---
+++ src/Verify.Tests/ReflectionHelpersTests.cs
@@ -0,0 +1,15 @@
+﻿public class ReflectionHelpersTests
+{
+    [Theory]
+    [InlineData(typeof(string), false)]
+    [InlineData(typeof(ICollection), true)]
+    [InlineData(typeof(IList), true)]
+    [InlineData(typeof(IEnumerable), false)]
+    [InlineData(typeof(ICollection<int>), true)]
+    [InlineData(typeof(IList), true)]
+    [InlineData(typeof(IEnumerable<int>), false)]
+    public void ShouldNotLock(Type type, bool isCollection)
+    {
+        Assert.Equal(isCollection, type.IsCollection());
+    }
+}

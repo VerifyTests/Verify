71878e61319bef926008ab427a026ab846a53f69
Simon Cropp <simon.cropp@gmail.com>
2022-02-12 11:05:09 +1100

    Update EqualityResult.cs

--- src/Verify/Verifier/EqualityResult.cs
+++ src/Verify/Verifier/EqualityResult.cs
@@ -2,10 +2,14 @@
 {
     public Equality Equality { get; }
     public string? Message { get; }
+    public string? ReceivedText { get; }
+    public string? VerifiedText { get; }
 
     public EqualityResult(in Equality equality, in string? message, in string? receivedText, in string? verifiedText)
     {
         Equality = equality;
         Message = message;
+        ReceivedText = receivedText;
+        VerifiedText = verifiedText;
     }
 }

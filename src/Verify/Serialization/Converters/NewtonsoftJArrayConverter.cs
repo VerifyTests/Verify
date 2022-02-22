3be8129d6b9a0308396424ae37a767241dcb5fc3
Simon Cropp <simon.cropp@gmail.com>
2022-02-22 20:20:06 +1100

    ..

---
+++ src/Verify/Serialization/Converters/NewtonsoftJArrayConverter.cs
@@ -0,0 +1,15 @@
+﻿class NewtonsoftJArrayConverter :
+    WriteOnlyJsonConverter
+{
+    public override void Write(VerifyJsonWriter writer, object value)
+    {
+        var method = value.GetType().GetMethod("ToObject", new Type[] {typeof(Type)})!;
+        var list = method.Invoke(value,new object[]{typeof(List<object>)})!;
+        writer.Serialize(list);
+    }
+
+    public sealed override bool CanConvert(Type type)
+    {
+        return type.FullName == "Newtonsoft.Json.Linq.JArray";
+    }
+}
---
+++ src/Verify/Serialization/Converters/NewtonsoftJObjectConverter.cs
@@ -0,0 +1,15 @@
+﻿class NewtonsoftJObjectConverter :
+    WriteOnlyJsonConverter
+{
+    public override void Write(VerifyJsonWriter writer, object value)
+    {
+        var method = value.GetType().GetMethod("ToObject", new Type[] {typeof(Type)})!;
+        var dictionary = method.Invoke(value,new object[]{typeof(Dictionary<string, object>)})!;
+        writer.Serialize(dictionary);
+    }
+
+    public sealed override bool CanConvert(Type type)
+    {
+        return type.FullName == "Newtonsoft.Json.Linq.JObject";
+    }
+}

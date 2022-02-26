fd1aee48f44cea67a4b3c7a836e79577a711a7b4
Simon Cropp <simon.cropp@gmail.com>
2022-02-26 19:47:51 +1100

    re add naming tests

--- src/Naming/Tests.cs
+++ src/Naming/Tests.cs
@@ -1,5 +1,4 @@
-﻿using VerifyTests;
-using VerifyXunit;
+﻿using VerifyXunit;
 using Xunit;
 
 [UsesVerify]
@@ -32,22 +31,15 @@ public class Tests
 
     public Result Run(
         bool run,
-        bool runG,
         bool config,
-        bool configG,
         bool runVersion,
-        bool runVersionG,
         bool arch,
-        bool archG,
         bool os,
-        bool osG,
         bool method,
         bool type,
         bool dir,
         bool fw,
-        bool fwG,
-        bool fwVersion,
-        bool fwVersionG)
+        bool fwVersion)
     {
         var sharedNamer = VerifierSettings.SharedNamer;
         sharedNamer.UniqueForAssemblyConfiguration = false;
@@ -57,7 +49,7 @@ public class Tests
         sharedNamer.UniqueForOSPlatform = false;
         sharedNamer.UniqueForTargetFramework = false;
         sharedNamer.UniqueForTargetFrameworkAndVersion = false;
-        
+
         var settings = new VerifySettings();
 
         if (run)
@@ -65,18 +57,9 @@ public class Tests
             settings.UniqueForRuntime();
         }
 
-        if (runG)
-        {
-            VerifierSettings.UniqueForRuntime();
-        }
         if (fw)
         {
             settings.UniqueForTargetFramework();
-        }
-
-        if (fwG)
-        {
-            VerifierSettings.UniqueForTargetFramework();
         }
 
         if (config)
@@ -84,19 +67,9 @@ public class Tests
             settings.UniqueForAssemblyConfiguration();
         }
 
-        if (configG)
-        {
-            VerifierSettings.UniqueForAssemblyConfiguration();
-        }
-
         if (runVersion)
         {
             settings.UniqueForRuntimeAndVersion();
-        }
-
-        if (runVersionG)
-        {
-            VerifierSettings.UniqueForRuntimeAndVersion();
         }
 
         if (fwVersion)
@@ -104,29 +77,14 @@ public class Tests
             settings.UniqueForTargetFrameworkAndVersion();
         }
 
-        if (fwVersionG)
-        {
-            VerifierSettings.UniqueForTargetFrameworkAndVersion();
-        }
-
         if (arch)
         {
             settings.UniqueForArchitecture();
         }
 
-        if (archG)
-        {
-            VerifierSettings.UniqueForArchitecture();
-        }
-
         if (os)
         {
             settings.UniqueForOSPlatform();
-        }
-
-        if (osG)
-        {
-            VerifierSettings.UniqueForOSPlatform();
         }
 
         if (method)
@@ -147,10 +105,79 @@ public class Tests
         var builder = Builder(settings);
         var fileNames = builder.GetFileNames("txt");
         var fileNamesWithIndex = builder.GetIndexedFileNames("txt", 2);
-        File.WriteAllText(fileNames.Received, "");
-        File.WriteAllText(fileNames.Verified, "");
-        File.WriteAllText(fileNamesWithIndex.Received, "");
-        File.WriteAllText(fileNamesWithIndex.Verified, "");
+        PrefixUnique.Clear();
+        builder = Builder(settings);
+
+        var receivedFiles = builder.ReceivedFiles.OrderBy(x => x);
+        var verifiedFiles = builder.VerifiedFiles.OrderBy(x => x);
+        PrefixUnique.Clear();
+        return new()
+        {
+            FileNames = fileNames,
+            FileNamesWithIndex = fileNamesWithIndex,
+            ReceivedFiles = receivedFiles,
+            VerifiedFiles = verifiedFiles
+        };
+    }
+
+    public Result RunStatic(
+        bool runG,
+        bool configG,
+        bool runVersionG,
+        bool archG,
+        bool osG,
+        bool fwG,
+        bool fwVersionG)
+    {
+        var sharedNamer = VerifierSettings.SharedNamer;
+        sharedNamer.UniqueForAssemblyConfiguration = false;
+        sharedNamer.UniqueForRuntime = false;
+        sharedNamer.UniqueForRuntimeAndVersion = false;
+        sharedNamer.UniqueForArchitecture = false;
+        sharedNamer.UniqueForOSPlatform = false;
+        sharedNamer.UniqueForTargetFramework = false;
+        sharedNamer.UniqueForTargetFrameworkAndVersion = false;
+
+        var settings = new VerifySettings();
+
+        if (runG)
+        {
+            VerifierSettings.UniqueForRuntime();
+        }
+
+        if (fwG)
+        {
+            VerifierSettings.UniqueForTargetFramework();
+        }
+
+        if (configG)
+        {
+            VerifierSettings.UniqueForAssemblyConfiguration();
+        }
+
+        if (runVersionG)
+        {
+            VerifierSettings.UniqueForRuntimeAndVersion();
+        }
+
+        if (fwVersionG)
+        {
+            VerifierSettings.UniqueForTargetFrameworkAndVersion();
+        }
+
+        if (archG)
+        {
+            VerifierSettings.UniqueForArchitecture();
+        }
+
+        if (osG)
+        {
+            VerifierSettings.UniqueForOSPlatform();
+        }
+
+        var builder = Builder(settings);
+        var fileNames = builder.GetFileNames("txt");
+        var fileNamesWithIndex = builder.GetIndexedFileNames("txt", 2);
         PrefixUnique.Clear();
         builder = Builder(settings);
 
@@ -185,67 +212,73 @@ public class Tests
     {
         foreach (var runtime in bools)
         {
-            foreach (var runtimeStatic in bools)
+            foreach (var fw in bools)
             {
-                foreach (var fw in bools)
+                foreach (var config in bools)
                 {
-                    foreach (var fwStatic in bools)
+                    foreach (var runtimeVersion in bools)
                     {
-                        foreach (var config in bools)
+                        foreach (var fwVersion in bools)
                         {
-                            foreach (var configStatic in bools)
+                            foreach (var method in bools)
                             {
-                                foreach (var runtimeVersion in bools)
+                                foreach (var type in bools)
                                 {
-                                    foreach (var runtimeVersionStatic in bools)
+                                    foreach (var dir in bools)
                                     {
-                                        foreach (var fwVersion in bools)
+                                        foreach (var arch in bools)
                                         {
-                                            foreach (var fwVersionStatic in bools)
+                                            foreach (var os in bools)
                                             {
-                                                foreach (var method in bools)
-                                                {
-                                                    foreach (var type in bools)
-                                                    {
-                                                        foreach (var dir in bools)
-                                                        {
-                                                            foreach (var arch in bools)
-                                                            {
-                                                                foreach (var archStatic in bools)
-                                                                {
-                                                                    foreach (var os in bools)
-                                                                    {
-                                                                        foreach (var osStatic in bools)
-                                                                        {
-                                                                            yield return Run(
-                                                                                runtime,
-                                                                                runtimeStatic,
-                                                                                config,
-                                                                                configStatic,
-                                                                                runtimeVersion,
-                                                                                runtimeVersionStatic,
-                                                                                arch,
-                                                                                archStatic,
-                                                                                os,
-                                                                                osStatic,
-                                                                                method,
-                                                                                type,
-                                                                                dir,
-                                                                                fw,
-                                                                                fwStatic,
-                                                                                fwVersion,
-                                                                                fwVersionStatic
-                                                                            );
-                                                                        }
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
+                                                yield return Run(
+                                                    runtime,
+                                                    config,
+                                                    runtimeVersion,
+                                                    arch,
+                                                    os,
+                                                    method,
+                                                    type,
+                                                    dir,
+                                                    fw,
+                                                    fwVersion
+                                                );
                                             }
                                         }
                                     }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public IEnumerable<Result> BuildStaticData()
+    {
+        foreach (var runtimeStatic in bools)
+        {
+            foreach (var fwStatic in bools)
+            {
+                foreach (var configStatic in bools)
+                {
+                    foreach (var runtimeVersionStatic in bools)
+                    {
+                        foreach (var fwVersionStatic in bools)
+                        {
+                            foreach (var archStatic in bools)
+                            {
+                                foreach (var osStatic in bools)
+                                {
+                                    yield return RunStatic(
+                                        runtimeStatic,
+                                        configStatic,
+                                        runtimeVersionStatic,
+                                        archStatic,
+                                        osStatic,
+                                        fwStatic,
+                                        fwVersionStatic
+                                    );
                                 }
                             }
                         }

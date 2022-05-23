284ace5bc58bfc36a84fc6c016879d3a04e863ce
Simon Cropp <simon.cropp@gmail.com>
2022-05-23 15:01:44 +1000

    Update ReflectionHelpers.cs

--- src/Verify/Serialization/ReflectionHelpers.cs
+++ src/Verify/Serialization/ReflectionHelpers.cs
@@ -180,22 +180,6 @@
         return type.GetGenericArguments()[0] == typeof(Stream);
     }
 
-    public static T GetValue<T>(this MemberInfo member, object instance)
-    {
-        // this value could be in a public field or public property
-        if (member is PropertyInfo propertyInfo)
-        {
-            return (T) propertyInfo.GetValue(instance, null)!;
-        }
-
-        if (member is FieldInfo fieldInfo)
-        {
-            return (T) fieldInfo.GetValue(instance)!;
-        }
-
-        throw new($"No supported MemberType: {member.MemberType}");
-    }
-
     public static Type MemberType(this MemberInfo member)
     {
         if (member is PropertyInfo property)

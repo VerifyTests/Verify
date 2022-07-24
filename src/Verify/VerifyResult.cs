070529c5e752f997cffab8c79d0d42f4f42c2b56
Simon Cropp <simon.cropp@gmail.com>
2022-07-24 20:01:32 +1000

    .

---
+++ src/Verify/VerifyResult.cs
@@ -0,0 +1,11 @@
+﻿namespace VerifyTests;
+
+public class VerifyResult
+{
+    public VerifyResult(IReadOnlyList<string> files)
+    {
+        Files = files;
+    }
+
+    public IReadOnlyList<string> Files { get; }
+}

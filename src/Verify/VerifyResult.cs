54527e29087423b897e28ca864d6dafea68c42f0
Simon Cropp <simon.cropp@gmail.com>
2022-07-24 22:01:45 +1000

    Update VerifyResult.cs

--- src/Verify/VerifyResult.cs
+++ src/Verify/VerifyResult.cs
@@ -6,4 +6,23 @@ public class VerifyResult
         Files = files;
 
     public IReadOnlyList<FilePair> Files { get; }
+
+    public string Text
+    {
+        get
+        {
+            var textFiles = Files.Where(_ => _.IsText).ToList();
+            if (textFiles.Count == 0)
+            {
+                throw new("No text files in results");
+            }
+
+            if (textFiles.Count > 1)
+            {
+                throw new("More than one text file in results");
+            }
+
+            return File.ReadAllText(textFiles[0].VerifiedPath);
+        }
+    }
 }

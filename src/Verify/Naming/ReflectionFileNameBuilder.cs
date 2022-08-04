6e13a66801a1cf0fac034863f91294dd3b5b9294
Simon Cropp <simon.cropp@gmail.com>
2022-08-05 09:27:53 +1000

    early exist for settings.fileName is not null

--- src/Verify/Naming/ReflectionFileNameBuilder.cs
+++ src/Verify/Naming/ReflectionFileNameBuilder.cs
@@ -8,24 +8,24 @@
         string uniqueness)
     {
         var pathInfo = VerifierSettings.GetPathInfo(sourceFile, type, method);
+        var directory = settings.Directory ?? pathInfo.Directory;
+
+        if (settings.fileName is not null)
+        {
+            var filename = settings.fileName + uniqueness;
+            return (filename, filename, directory);
+        }
 
         var (prefixWithParameters, prefixWithoutParameters) = GetFileNamePrefix(method, type, settings, pathInfo, uniqueness);
 
         var receivedFilePrefix = prefixWithParameters;
         var verifiedFilePrefix = settings.ignoreParametersForVerified ? prefixWithoutParameters : prefixWithParameters;
 
-        var directory = settings.Directory ?? pathInfo.Directory;
         return (receivedFilePrefix, verifiedFilePrefix, directory);
     }
 
     static (string prefixWithParameters, string prefixWithoutParameters) GetFileNamePrefix(MethodInfo method, Type type, VerifySettings settings, PathInfo pathInfo, string uniqueness)
     {
-        if (settings.fileName is not null)
-        {
-            var filename = settings.fileName + uniqueness;
-            return (filename, filename);
-        }
-
         var typeName = settings.typeName ?? pathInfo.TypeName ?? GetTypeName(type);
         var methodName = settings.methodName ?? pathInfo.MethodName ?? method.Name;
 

3b2fbd3a400977af8ebef378591dbe7d14f4d4ae
Simon Cropp <simon.cropp@gmail.com>
2022-08-06 13:03:50 +1000

    make machine name and user name scrubbing less greedy

--- src/ApplyScrubbersTests/DisableDirScrubbingTests.cs
+++ src/ApplyScrubbersTests/DisableDirScrubbingTests.cs
@@ -1,6 +1,9 @@
 ﻿[UsesVerify]
 public class ApplyScrubbersTests
 {
+    public ApplyScrubbersTests() =>
+        Scrubbers.ResetReplacements("FakeMachineName", "FakeUserName");
+
     [Theory]
     [InlineData("/", "/project", "/,/project")]
     [InlineData("/solution", "/project", "/solution,/project")]
@@ -23,4 +26,74 @@ public class ApplyScrubbersTests
         // Assert
         Assert.Equal("{SolutionDirectory},{ProjectDirectory}", builder.ToString());
     }
+
+    [Fact]
+    public Task ScrubUserName() =>
+        Verify("FakeUserName")
+            .ScrubUserName();
+
+    [Fact]
+    public Task ScrubUserNameValidBefore() =>
+        Verify(" FakeUserName")
+            .ScrubUserName();
+
+    [Fact]
+    public Task ScrubUserNameValidAfter() =>
+        Verify("FakeUserName ")
+            .ScrubUserName();
+
+    [Fact]
+    public Task ScrubUserNameValidWrapped() =>
+        Verify(" FakeUserName ")
+            .ScrubUserName();
+
+    [Fact]
+    public Task ScrubMachineName() =>
+        Verify("FakeMachineName")
+            .ScrubMachineName();
+
+    [Fact]
+    public Task ScrubMachineNameValidBefore() =>
+        Verify(" FakeMachineName")
+            .ScrubMachineName();
+
+    [Fact]
+    public Task ScrubMachineNameValidAfter() =>
+        Verify("FakeMachineName ")
+            .ScrubMachineName();
+
+    [Fact]
+    public Task ScrubMachineNameValidWrapped() =>
+        Verify(" FakeMachineName ")
+            .ScrubMachineName();
+
+    [Fact]
+    public Task ScrubUserNameInValidBefore() =>
+        Verify("AFakeUserName")
+        .ScrubUserName();
+
+    [Fact]
+    public Task ScrubUserNameInValidAfter() =>
+        Verify("FakeUserNameA")
+        .ScrubUserName();
+
+    [Fact]
+    public Task ScrubUserNameInValidWrapped() =>
+        Verify("AFakeUserNameA")
+        .ScrubUserName();
+
+    [Fact]
+    public Task ScrubMachineNameInValidBefore() =>
+        Verify("AFakeMachineName")
+        .ScrubMachineName();
+
+    [Fact]
+    public Task ScrubMachineNameInValidAfter() =>
+        Verify("FakeMachineNameA")
+        .ScrubMachineName();
+
+    [Fact]
+    public Task ScrubMachineNameInValidWrapped() =>
+        Verify("AFakeMachineNameA")
+        .ScrubMachineName();
 }

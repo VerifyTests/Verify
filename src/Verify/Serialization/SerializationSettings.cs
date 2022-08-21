e2c820c90362e91ebb76b92b727a2a17b31e3f5f
Simon Cropp <simon.cropp@gmail.com>
2022-08-21 21:04:29 +1000

    Revert "."
    This reverts commit f8d11db86a8084fd10d594325d431607fa6894e0.

--- src/Verify/Serialization/SerializationSettings.cs
+++ src/Verify/Serialization/SerializationSettings.cs
@@ -4,6 +4,39 @@ using Formatting = Argon.Formatting;
 
 partial class SerializationSettings
 {
+    static ArgonJArrayConverter argonJArrayConverter = new();
+    static ArgonJObjectConverter argonJObjectConverter = new();
+    static NewtonsoftJArrayConverter newtonsoftJArrayConverter = new();
+    static NewtonsoftJObjectConverter newtonsoftJObjectConverter = new();
+    static FileInfoConverter fileInfoConverter = new();
+#if NET6_0_OR_GREATER
+    static TimeConverter timeConverter = new();
+    static DateConverter dateConverter = new();
+#endif
+    static DirectoryInfoConverter directoryInfoConverter = new();
+    static StringEnumConverter stringEnumConverter = new();
+    static DelegateConverter delegateConverter = new();
+    static TargetInvocationExceptionConverter targetInvocationExceptionConverter = new();
+    static ExpressionConverter expressionConverter = new();
+    static TypeJsonConverter typeJsonConverter = new();
+    static MethodInfoConverter methodInfoConverter = new();
+    static FieldInfoConverter fieldInfoConverter = new();
+    static ConstructorInfoConverter constructorInfoConverter = new();
+    static ParameterInfoConverter parameterInfoConverter = new();
+    static VersionConverter versionConverter = new();
+    static EncodingConverter encodingConverter = new();
+    static PropertyInfoConverter propertyInfoConverter = new();
+    static ClaimConverter claimConverter = new();
+    static AggregateExceptionConverter aggregateExceptionConverter = new();
+    static ClaimsPrincipalConverter claimsPrincipalConverter = new();
+    static ClaimsIdentityConverter claimsIdentityConverter = new();
+    static NameValueCollectionConverter nameValueCollectionConverter = new();
+    static StringBuilderConverter stringBuilderConverter = new();
+    static TaskConverter taskConverter = new();
+    static ValueTaskConverter valueTaskConverter = new();
+    static StringWriterConverter stringWriterConverter = new();
+    //static DictionaryConverter dictionaryConverter = new();
+
     JsonSerializerSettings jsonSettings;
 
     public SerializationSettings()
@@ -59,10 +92,43 @@ partial class SerializationSettings
         };
 
         #endregion
+
         settings.SerializationBinder = ShortNameBinder.Instance;
 
         settings.ContractResolver = new CustomContractResolver(this);
-        settings.Converters.AddRange(covnerters);
+        var converters = settings.Converters;
+        converters.Add(aggregateExceptionConverter);
+        converters.Add(stringBuilderConverter);
+        converters.Add(stringWriterConverter);
+#if NET6_0_OR_GREATER
+        converters.Add(dateConverter);
+        converters.Add(timeConverter);
+#endif
+        converters.Add(fileInfoConverter);
+        converters.Add(directoryInfoConverter);
+        converters.Add(stringEnumConverter);
+        converters.Add(expressionConverter);
+        converters.Add(delegateConverter);
+        converters.Add(targetInvocationExceptionConverter);
+        converters.Add(versionConverter);
+        converters.Add(encodingConverter);
+        converters.Add(typeJsonConverter);
+        converters.Add(methodInfoConverter);
+        converters.Add(fieldInfoConverter);
+        converters.Add(constructorInfoConverter);
+        converters.Add(propertyInfoConverter);
+        converters.Add(parameterInfoConverter);
+        converters.Add(claimConverter);
+        converters.Add(claimsIdentityConverter);
+        converters.Add(taskConverter);
+        converters.Add(valueTaskConverter);
+        converters.Add(claimsPrincipalConverter);
+        //converters.Add(dictionaryConverter);
+        converters.Add(argonJArrayConverter);
+        converters.Add(argonJObjectConverter);
+        converters.Add(newtonsoftJArrayConverter);
+        converters.Add(newtonsoftJObjectConverter);
+        converters.Add(nameValueCollectionConverter);
         foreach (var extraSetting in extraSettings)
         {
             extraSetting(settings);

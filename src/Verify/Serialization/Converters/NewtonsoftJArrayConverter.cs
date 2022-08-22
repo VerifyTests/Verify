820ca36fa094df3a5b2c514696c92baabe5543d4
Simon Cropp <simon.cropp@gmail.com>
2022-08-22 21:32:31 +1000

    simplify newtonsoft converters

--- src/Verify/Serialization/Converters/NewtonsoftJArrayConverter.cs
+++ src/Verify/Serialization/Converters/NewtonsoftJArrayConverter.cs
@@ -1,12 +1,33 @@
 ﻿class NewtonsoftJArrayConverter :
     WriteOnlyJsonConverter
 {
-    public override void Write(VerifyJsonWriter writer, object value)
+    static MethodInfo? method;
+
+    static object[] parameters =
     {
-        var method = value.GetType().GetMethod("ToObject", new Type[] {typeof(Type)})!;
-        var list = method.Invoke(value,new object[]{typeof(List<object>)})!;
-        writer.Serialize(list);
-    }
+        typeof(List<object>)
+    };
+
+    static Lazy<Func<object, object>> lazy;
+
+    static NewtonsoftJArrayConverter() =>
+        lazy = new(() => value =>
+        {
+            if (method == null)
+            {
+                var type = Type.GetType("Newtonsoft.Json.Linq.JArray, Newtonsoft.Json")!;
+
+                method = type.GetMethod("ToObject", new[]
+                {
+                    typeof(Type)
+                })!;
+            }
+
+            return method.Invoke(value, parameters)!;
+        });
+
+    public override void Write(VerifyJsonWriter writer, object value) =>
+        writer.Serialize(lazy.Value(value));
 
     public sealed override bool CanConvert(Type type) =>
         type.FullName == "Newtonsoft.Json.Linq.JArray";
--- src/Verify/Serialization/Converters/NewtonsoftJObjectConverter.cs
+++ src/Verify/Serialization/Converters/NewtonsoftJObjectConverter.cs
@@ -1,12 +1,33 @@
 ﻿class NewtonsoftJObjectConverter :
     WriteOnlyJsonConverter
 {
-    public override void Write(VerifyJsonWriter writer, object value)
+    static MethodInfo? method;
+
+    static object[] parameters =
     {
-        var method = value.GetType().GetMethod("ToObject", new Type[] {typeof(Type)})!;
-        var dictionary = method.Invoke(value,new object[]{typeof(Dictionary<string, object>)})!;
-        writer.Serialize(dictionary);
-    }
+        typeof(Dictionary<string, object>)
+    };
+
+    static Lazy<Func<object, object>> lazy;
+
+    static NewtonsoftJObjectConverter() =>
+        lazy = new(() => value =>
+        {
+            if (method == null)
+            {
+                var type = Type.GetType("Newtonsoft.Json.Linq.JObject, Newtonsoft.Json")!;
+
+                method = type.GetMethod("ToObject", new[]
+                {
+                    typeof(Type)
+                })!;
+            }
+
+            return method.Invoke(value, parameters)!;
+        });
+
+    public override void Write(VerifyJsonWriter writer, object value) =>
+        writer.Serialize(lazy.Value(value));
 
     public sealed override bool CanConvert(Type type) =>
         type.FullName == "Newtonsoft.Json.Linq.JObject";

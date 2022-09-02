4253a63ad6357228d454f39f5e5ecf8235b3372d
Simon Cropp <simon.cropp@gmail.com>
2022-09-02 22:33:26 +1000

    Better cleanup of old verified files  (#634)

---
+++ src/Verify.Tests/MatchingFileFinderTests.cs
@@ -0,0 +1,16 @@
+﻿public class MatchingFileFinderTests
+{
+    [Theory]
+    [MemberData(nameof(GetData))]
+    public void Simple(string fileNamePrefix, string suffix, string file, bool result) =>
+        Assert.Equal(result, MatchingFileFinder.ShouldInclude(fileNamePrefix, suffix, file));
+
+    public static IEnumerable<object[]> GetData()
+    {
+        yield return new object[] {"Test.Method", ".verified", @"C:/Path/Test.Method.verified.txt", true};
+        yield return new object[] {"Test.Method", ".verified", @"C:/Path/Test.Method.00name1.verified.txt", true};
+        yield return new object[] {"Test.Method", ".verified", @"C:/Path/Test.Method.00.name1.verified.txt", true};
+        yield return new object[] {"Test.Method1", ".verified", @"C:/Path/Test.Method2.00.name1.verified.txt", false};
+        yield return new object[] {"Test1.Method", ".verified", @"C:/Path/Test2.Method.00.name1.verified.txt", false};
+    }
+}

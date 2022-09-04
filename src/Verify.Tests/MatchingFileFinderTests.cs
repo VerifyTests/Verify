d490467a1df45f8d52bcca6e3d9fdb2c5a846d4c
Simon Cropp <simon.cropp@gmail.com>
2022-09-04 18:08:08 +1000

    Update MatchingFileFinderTests.cs

--- src/Verify.Tests/MatchingFileFinderTests.cs
+++ src/Verify.Tests/MatchingFileFinderTests.cs
@@ -7,10 +7,12 @@
 
     public static IEnumerable<object[]> GetData()
     {
-        yield return new object[] {"Test.Method", ".verified", @"C:/Path/Test.Method.verified.txt", true};
-        yield return new object[] {"Test.Method", ".verified", @"C:/Path/Test.Method.00name1.verified.txt", true};
-        yield return new object[] {"Test.Method", ".verified", @"C:/Path/Test.Method.00.name1.verified.txt", true};
-        yield return new object[] {"Test.Method1", ".verified", @"C:/Path/Test.Method2.00.name1.verified.txt", false};
-        yield return new object[] {"Test1.Method", ".verified", @"C:/Path/Test2.Method.00.name1.verified.txt", false};
+        yield return new object[] {"Test.Method", ".verified", @"C:/Test.Method.verified.txt", true};
+        yield return new object[] {"Test.Method.Net4", ".verified", @"C:/Test.Method.Net4.verified.txt", true};
+        yield return new object[] {"Test.Method", ".verified", @"C:/Test.Method.name1.verified.txt", false};
+        yield return new object[] {"Test.Method", ".verified", @"C:/Test.Method.00name1.verified.txt", true};
+        yield return new object[] {"Test.Method", ".verified", @"C:/Test.Method.00.name1.verified.txt", true};
+        yield return new object[] {"Test.Method1", ".verified", @"C:/Test.Method2.00.name1.verified.txt", false};
+        yield return new object[] {"Test1.Method", ".verified", @"C:/Test2.Method.00.name1.verified.txt", false};
     }
 }

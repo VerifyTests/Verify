fc01c23a468d7c480fbc1da6fbd8dc5824035b0c
Simon Cropp <simon.cropp@gmail.com>
2022-09-07 16:46:38 +1000

    Create Obsolete.cs

---
+++ src/Verify/DerivePaths/Obsolete.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable UnusedParameter.Local
+
+namespace VerifyTests;
+
+public static partial class VerifierSettings
+{
+    [Obsolete("Use Verifier.DerivePathInfo for xunit, expecto and nunit. Use VerifierBase.DerivePathInfo for mstest.")]
+    public static void DerivePathInfo()
+    {
+    }
+}

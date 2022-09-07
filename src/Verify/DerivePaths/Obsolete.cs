81b04e6f6703c3b8e571ca2edd8702b972398c1c
Simon Cropp <simon.cropp@gmail.com>
2022-09-07 17:25:28 +1000

    Update Obsolete.cs

--- src/Verify/DerivePaths/Obsolete.cs
+++ src/Verify/DerivePaths/Obsolete.cs
@@ -5,7 +5,7 @@ namespace VerifyTests;
 public static partial class VerifierSettings
 {
     [Obsolete("Use Verifier.DerivePathInfo for xunit, expecto and nunit. Use VerifierBase.DerivePathInfo for mstest.")]
-    public static void DerivePathInfo()
+    public static void DerivePathInfo(string sourceFile, string projectDirectory, Type type, MethodInfo method)
     {
     }
 }

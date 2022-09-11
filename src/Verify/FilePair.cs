12d78dba5582512e14475cd0679299551d12520c
Simon Cropp <simon.cropp@gmail.com>
2022-09-11 19:29:34 +1000

    Update FilePair.cs

--- src/Verify/FilePair.cs
+++ src/Verify/FilePair.cs
@@ -5,8 +5,6 @@ public readonly struct FilePair
     public string Extension { get; }
     public string ReceivedPath { get; }
     public string VerifiedPath { get; }
-    public string ReceivedName { get; }
-    public string VerifiedName { get; }
     public bool IsText { get; }
 
     public FilePair(string extension, string prefixReceived, string prefixVerified)
@@ -14,8 +12,6 @@ public readonly struct FilePair
         Extension = extension;
         ReceivedPath = $"{prefixReceived}.received.{extension}";
         VerifiedPath = $"{prefixVerified}.verified.{extension}";
-        ReceivedName = Path.GetFileName(ReceivedPath);
-        VerifiedName = Path.GetFileName(VerifiedPath);
         IsText = EmptyFiles.Extensions.IsText(extension);
     }
 }

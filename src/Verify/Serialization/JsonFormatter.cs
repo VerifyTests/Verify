3e2ba88168d4bbb5cc2cf2989b3abd6341a9efd7
Simon Cropp <simon.cropp@gmail.com>
2022-09-21 20:14:22 +1000

    .

--- src/Verify/Serialization/JsonFormatter.cs
+++ src/Verify/Serialization/JsonFormatter.cs
@@ -4,15 +4,13 @@
     {
         if (appends.Any())
         {
-            var dictionary = new OrderedDictionary
-            {
-                {"target", input ?? "null"}
-            };
+            var dictionary = new MultiValueDictionary();
+            dictionary.Add("target", input ?? "null");
 
             input = dictionary;
             foreach (var append in appends)
             {
-                dictionary[append.Name] = append.Data;
+                dictionary.Add(append.Name, append.Data);
             }
         }
 
@@ -21,5 +19,58 @@
         settings.Serializer.Serialize(writer, input);
         builder.FixNewlines();
         return builder;
+    }
+}
+
+class MultiValueDictionary
+{
+    public class Converter : WriteOnlyJsonConverter<MultiValueDictionary>
+    {
+        public override void Write(VerifyJsonWriter writer, MultiValueDictionary value)
+        {
+            writer.WriteStartArray();
+            foreach (var item in value.inner)
+            {
+                if (item is KeyValuePair<string, List<object>> pair)
+                {
+                    writer.WriteStartObject();
+                    writer.WritePropertyName(pair.Key);
+                    if (pair.Value.Count == 1)
+                    {
+                        writer.Serialize(pair.Value.Single());
+                    }
+                    else
+                    {
+                        writer.Serialize(pair.Value);
+                    }
+                    writer.WriteEndObject();
+                }
+                else
+                {
+                    writer.Serialize(item);
+                }
+            }
+
+            writer.WriteEndArray();
+        }
+    }
+
+    List<object> inner = new();
+    Dictionary<string, List<object>> innerDictionary = new();
+
+    public void Add(string name, object value)
+    {
+        if (innerDictionary.TryGetValue(name, out var list))
+        {
+            list.Add(value);
+            return;
+        }
+
+        var objects = new List<object>
+        {
+            value
+        };
+        innerDictionary.Add(name, objects);
+        inner.Add(new KeyValuePair<string, List<object>>(name, objects));
     }
 }

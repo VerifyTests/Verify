3b541c859d4a4186df2271fa8d94e7474182dc87
Simon Cropp <simon.cropp@gmail.com>
2022-09-23 20:36:21 +1000

    .

---
+++ src/Verify/Verifier/InnerVerifier_String.cs
@@ -0,0 +1,44 @@
+﻿partial class InnerVerifier
+{
+    public async Task<VerifyResult> VerifyString(Task<string> task)
+    {
+        var value = await task;
+        return await VerifyString(value);
+    }
+
+    public Task<VerifyResult> VerifyString(string value)
+    {
+        value = OrEmpty(value);
+        return VerifyInner(
+            value,
+            null,
+            Array.Empty<Target>());
+    }
+    public async Task<VerifyResult> VerifyString(Task<string> task, string extension)
+    {
+        var value = await task;
+        return await VerifyString(value, extension);
+    }
+
+    public Task<VerifyResult> VerifyString(string value, string extension)
+    {
+        value = OrEmpty(value);
+        return VerifyInner(
+            null,
+            null,
+            new[]
+            {
+                new Target(extension, value)
+            });
+    }
+
+    static string OrEmpty(string value)
+    {
+        if (value.Length == 0)
+        {
+            return "emptyString";
+        }
+
+        return value;
+    }
+}

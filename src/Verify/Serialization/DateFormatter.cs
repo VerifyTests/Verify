3a0e869f9ecdead9c17b13b4eaf5d382015c8199
Simon Cropp <simon.cropp@gmail.com>
2022-10-20 22:48:44 +1100

    ffs

--- src/Verify/Serialization/DateFormatter.cs
+++ src/Verify/Serialization/DateFormatter.cs
@@ -2,164 +2,81 @@
 {
     public static string ToJsonString(DateTimeOffset value)
     {
-        string stringValue;
         if (value.TimeOfDay == TimeSpan.Zero)
         {
-            stringValue = value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
-        else
+
+        if (value.Second == 0 && value.Millisecond == 0)
         {
-            if (value.Second == 0 && value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
-            }
-            else if (value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                stringValue = value.ToString("yyyy-MM-dd HH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
-            }
+            return value.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
         }
 
-        stringValue += $" {GetDateOffset(value)}";
+        if (value.Millisecond == 0)
+        {
+            return value.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+        }
 
-        return stringValue;
+        return value.ToString("yyyy-MM-dd HH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
     }
 
     public static string ToParameterString(DateTimeOffset value)
     {
-        string stringValue;
         if (value.TimeOfDay == TimeSpan.Zero)
         {
-            stringValue = value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
-        else
+
+        if (value.Second == 0 && value.Millisecond == 0)
         {
-            if (value.Second == 0 && value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture);
-            }
-            else if (value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                stringValue = value.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
-            }
+            return value.ToString("yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture);
         }
 
-        stringValue += GetDateOffset(value);
+        if (value.Millisecond == 0)
+        {
+            return value.ToString("yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
+        }
 
-        return stringValue;
+        return value.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
     }
 
     public static string ToJsonString(DateTime value)
     {
-        string stringValue;
         if (value.TimeOfDay == TimeSpan.Zero)
         {
-            stringValue = value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
-        else
+
+        if (value.Second == 0 && value.Millisecond == 0)
         {
-            if (value.Second == 0 && value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
-            }
-            else if (value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                stringValue = value.ToString("yyyy-MM-dd HH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
-            }
+            return value.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture);
         }
 
-        stringValue += $" {GetOffsetOrKind(value)}";
+        if (value.Millisecond == 0)
+        {
+            return value.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+        }
 
-        return stringValue;
+        return value.ToString("yyyy-MM-dd HH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
     }
 
     public static string ToParameterString(DateTime value)
     {
-        string stringValue;
         if (value.TimeOfDay == TimeSpan.Zero)
         {
-            stringValue = value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
+            return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
         }
-        else
+
+        if (value.Second == 0 && value.Millisecond == 0)
         {
-            if (value.Second == 0 && value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture);
-            }
-            else if (value.Millisecond == 0)
-            {
-                stringValue = value.ToString("yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                stringValue = value.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
-            }
+            return value.ToString("yyyy-MM-ddTHH:mm", CultureInfo.InvariantCulture);
         }
 
-        stringValue += GetOffsetOrKind(value);
-
-        return stringValue;
-    }
-
-    static string GetOffsetOrKind(DateTime value)
-    {
-        if (value.Kind == DateTimeKind.Unspecified)
+        if (value.Millisecond == 0)
         {
-            var offset = TimeZoneInfo.Local.GetUtcOffset(value);
-            return GetOffsetText(offset);
+            return value.ToString("yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
         }
 
-        return value.Kind.ToString();
-    }
-
-    static TimeSpan machineOffset = DateTimeOffset.Now.Offset;
-    static string GetDateOffset(DateTimeOffset value)
-    {
-        var offset = value.Offset;
-        if (offset == TimeSpan.Zero)
-        {
-            return "Utc";
-        }
-
-        if (offset == machineOffset)
-        {
-            return "Local";
-        }
-
-        return GetOffsetText(offset);
-    }
-
-    static string GetOffsetText(TimeSpan offset)
-    {
-        var hours = offset.Hours;
-        var minutes = offset.Minutes;
-
-        if (offset > TimeSpan.Zero)
-        {
-            if (minutes == 0)
-            {
-                return $"+{hours}";
-            }
-
-            return $"+{hours}:{minutes}";
-        }
-
-        if (minutes == 0)
-        {
-            return $"-{hours}";
-        }
-
-        return $"-{hours}:{minutes}";
+        return value.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
     }
 }

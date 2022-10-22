c223f29d2390692c539ca462493409b502052345
Simon Cropp <simon.cropp@gmail.com>
2022-10-23 00:14:10 +1100

    .

--- src/Verify/Serialization/DateFormatter.cs
+++ src/Verify/Serialization/DateFormatter.cs
@@ -134,9 +134,25 @@
         return value.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
     }
 
-    static string GetDateOffset(IFormattable value) =>
-        value.ToString("zzz", CultureInfo.InvariantCulture)
+    static string GetDateOffset(IFormattable value)
+    {
+        var dateOffset = value.ToString("zzz", CultureInfo.InvariantCulture)
             .Replace(":00", "")
             .Replace("+0", "+")
             .Replace("-0", "-");
+        return dateOffset;
+    }
+
+    static string GetDateOffset(DateTime value)
+    {
+        if(value.Kind == DateTimeKind.Utc)
+        {
+            return "+0";
+        }
+        var dateOffset = value.ToString("zzz", CultureInfo.InvariantCulture)
+            .Replace(":00", "")
+            .Replace("+0", "+")
+            .Replace("-0", "-");
+        return dateOffset;
+    }
 }

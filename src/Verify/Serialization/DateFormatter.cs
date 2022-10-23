84e250c63af4e5a656d722627b040572b15c31ee
Simon Cropp <simon.cropp@gmail.com>
2022-10-23 17:46:07 +1100

    more date stuff

--- src/Verify/Serialization/DateFormatter.cs
+++ src/Verify/Serialization/DateFormatter.cs
@@ -2,6 +2,13 @@
 {
     public static string ToJsonString(DateTimeOffset value)
     {
+        var result = GetJsonDatePart(value);
+        result += $" {GetDateOffset(value)}";
+        return result;
+    }
+
+    static string GetJsonDatePart(DateTimeOffset value)
+    {
         if (value.TimeOfDay == TimeSpan.Zero)
         {
             return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
@@ -21,6 +28,14 @@
     }
 
     public static string ToParameterString(DateTimeOffset value)
+    {
+        var result = GetParameterDatePart(value);
+        result += GetDateOffset(value);
+
+        return result;
+    }
+
+    static string GetParameterDatePart(DateTimeOffset value)
     {
         if (value.TimeOfDay == TimeSpan.Zero)
         {
@@ -42,6 +57,18 @@
 
     public static string ToJsonString(DateTime value)
     {
+        var result = GetJsonDatePart(value);
+
+        if (value.Kind != DateTimeKind.Unspecified)
+        {
+            result += $" {value.Kind}";
+        }
+
+        return result;
+    }
+
+    static string GetJsonDatePart(DateTime value)
+    {
         if (value.TimeOfDay == TimeSpan.Zero)
         {
             return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
@@ -62,6 +89,18 @@
 
     public static string ToParameterString(DateTime value)
     {
+        var result = GetParameterDatePart(value);
+
+        if (value.Kind != DateTimeKind.Unspecified)
+        {
+            result += value.Kind;
+        }
+
+        return result;
+    }
+
+    static string GetParameterDatePart(DateTime value)
+    {
         if (value.TimeOfDay == TimeSpan.Zero)
         {
             return value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
@@ -78,5 +117,32 @@
         }
 
         return value.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFF", CultureInfo.InvariantCulture);
+    }
+
+    static string GetDateOffset(DateTimeOffset value)
+    {
+        var offset = value.Offset;
+
+        if (offset > TimeSpan.Zero)
+        {
+            if (offset.Minutes == 0)
+            {
+                return $"+{offset.TotalHours:0}";
+            }
+
+            return $"+{offset.Hours:0}:{offset.Minutes:00}";
+        }
+
+        if (offset < TimeSpan.Zero)
+        {
+            if (offset.Minutes == 0)
+            {
+                return $"-{offset.Hours:0}";
+            }
+
+            return $"-{offset.Hours:0}:{offset.Minutes:00}";
+        }
+
+        return "+0";
     }
 }

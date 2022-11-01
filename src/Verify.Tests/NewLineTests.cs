33e32b256daccd17729788dc43bab93314c6da03
Simon Cropp <simon.cropp@gmail.com>
2022-11-01 20:17:17 +1100

    split tests

---
+++ src/Verify.Tests/NewLineTests.cs
@@ -0,0 +1,105 @@
+// Non-nullable field is uninitialized.
+
+#pragma warning disable CS8618
+
+[UsesVerify]
+public class NewLineTests
+{
+    [Fact]
+    public Task WithNewline() =>
+        Verify(new
+        {
+            Property = "F\roo"
+        });
+
+#if NET6_0
+    [Fact]
+    public async Task StringWithDifferingNewline()
+    {
+        var fullPath = CurrentFile.Relative("Tests.StringWithDifferingNewline.verified.txt");
+        File.Delete(fullPath);
+        File.WriteAllText(fullPath, "a\r\nb");
+        await Verify("a\r\nb");
+        PrefixUnique.Clear();
+        await Verify("a\rb");
+        PrefixUnique.Clear();
+        await Verify("a\nb");
+        PrefixUnique.Clear();
+
+        File.Delete(fullPath);
+        File.WriteAllText(fullPath, "a\nb");
+        await Verify("a\r\nb");
+        PrefixUnique.Clear();
+        await Verify("a\rb");
+        PrefixUnique.Clear();
+        await Verify("a\nb");
+        PrefixUnique.Clear();
+
+        File.Delete(fullPath);
+        File.WriteAllText(fullPath, "a\rb");
+        await Verify("a\r\nb");
+        PrefixUnique.Clear();
+        await Verify("a\rb");
+        PrefixUnique.Clear();
+        await Verify("a\nb");
+    }
+#endif
+
+    [Fact]
+    public Task Newlines() =>
+        Verify("a\r\nb\nc\rd\r\n");
+
+#if NET6_0
+    [Fact]
+    public async Task TrailingNewlinesRaw()
+    {
+        var file = CurrentFile.Relative("Tests.TrailingNewlinesRaw.verified.txt");
+        File.Delete(file);
+        var settings = new VerifySettings();
+        settings.DisableRequireUniquePrefix();
+
+        await File.WriteAllTextAsync(file, "a\r\n");
+        await Verify("a\r\n", settings);
+        await Verify("a\n", settings);
+        await Verify("a", settings);
+
+        await File.WriteAllTextAsync(file, "a\r\n\r\n");
+        await Verify("a\r\n\r\n", settings);
+        await Verify("a\n\n", settings);
+        await Verify("a\n", settings);
+
+        await File.WriteAllTextAsync(file, "a\n");
+        await Verify("a\n", settings);
+        await Verify("a", settings);
+
+        await File.WriteAllTextAsync(file, "a\n\n");
+        await Verify("a\n\n", settings);
+        await Verify("a\n", settings);
+        File.Delete(file);
+    }
+
+    [Fact]
+    public async Task TrailingNewlinesObject()
+    {
+        var file = CurrentFile.Relative("Tests.TrailingNewlinesObject.verified.txt");
+        var settings = new VerifySettings();
+        settings.DisableRequireUniquePrefix();
+        var target = new
+        {
+            s = "a"
+        };
+        File.WriteAllText(file, "{\n  s: a\n}");
+        await Verify(target, settings);
+
+        File.WriteAllText(file, "{\n  s: a\r}");
+        await Verify(target, settings);
+
+        File.WriteAllText(file, "{\n  s: a\n}\n");
+        await Verify(target, settings);
+
+        File.WriteAllText(file, "{\n  s: a\n}\r\n");
+        await Verify(target, settings);
+    }
+
+#endif
+}
--- src/Verify.Tests/Tests.cs
+++ src/Verify.Tests/Tests.cs
@@ -50,13 +50,6 @@ public class Tests
         }
     }
 
-    [Fact]
-    public Task WithNewline() =>
-        Verify(new
-        {
-            Property = "F\roo"
-        });
-
     [ModuleInitializer]
     public static void TreatAsStringInit() =>
         VerifierSettings.TreatAsString<ClassWithToString>(
@@ -197,39 +190,6 @@ public class Tests
         });
     }
 
-#if NET6_0
-    [Fact]
-    public async Task StringWithDifferingNewline()
-    {
-        var fullPath = CurrentFile.Relative("Tests.StringWithDifferingNewline.verified.txt");
-        File.Delete(fullPath);
-        File.WriteAllText(fullPath, "a\r\nb");
-        await Verify("a\r\nb");
-        PrefixUnique.Clear();
-        await Verify("a\rb");
-        PrefixUnique.Clear();
-        await Verify("a\nb");
-        PrefixUnique.Clear();
-
-        File.Delete(fullPath);
-        File.WriteAllText(fullPath, "a\nb");
-        await Verify("a\r\nb");
-        PrefixUnique.Clear();
-        await Verify("a\rb");
-        PrefixUnique.Clear();
-        await Verify("a\nb");
-        PrefixUnique.Clear();
-
-        File.Delete(fullPath);
-        File.WriteAllText(fullPath, "a\rb");
-        await Verify("a\r\nb");
-        PrefixUnique.Clear();
-        await Verify("a\rb");
-        PrefixUnique.Clear();
-        await Verify("a\nb");
-    }
-#endif
-
     [Fact]
     public async Task ShouldNotIgnoreCase()
     {
@@ -239,64 +199,6 @@ public class Tests
         PrefixUnique.Clear();
         await Assert.ThrowsAsync<VerifyException>(() => Verify("a", settings));
     }
-
-    [Fact]
-    public Task Newlines() =>
-        Verify("a\r\nb\nc\rd\r\n");
-
-#if NET6_0
-    [Fact]
-    public async Task TrailingNewlinesRaw()
-    {
-        var file = CurrentFile.Relative("Tests.TrailingNewlinesRaw.verified.txt");
-        File.Delete(file);
-        var settings = new VerifySettings();
-        settings.DisableRequireUniquePrefix();
-
-        await File.WriteAllTextAsync(file, "a\r\n");
-        await Verify("a\r\n", settings);
-        await Verify("a\n", settings);
-        await Verify("a", settings);
-
-        await File.WriteAllTextAsync(file, "a\r\n\r\n");
-        await Verify("a\r\n\r\n", settings);
-        await Verify("a\n\n", settings);
-        await Verify("a\n", settings);
-
-        await File.WriteAllTextAsync(file, "a\n");
-        await Verify("a\n", settings);
-        await Verify("a", settings);
-
-        await File.WriteAllTextAsync(file, "a\n\n");
-        await Verify("a\n\n", settings);
-        await Verify("a\n", settings);
-        File.Delete(file);
-    }
-    
-    [Fact]
-    public async Task TrailingNewlinesObject()
-    {
-        var file = CurrentFile.Relative("Tests.TrailingNewlinesObject.verified.txt");
-        var settings = new VerifySettings();
-        settings.DisableRequireUniquePrefix();
-        var target = new
-        {
-            s = "a"
-        };
-        File.WriteAllText(file, "{\n  s: a\n}");
-        await Verify(target, settings);
-
-        File.WriteAllText(file, "{\n  s: a\r}");
-        await Verify(target, settings);
-
-        File.WriteAllText(file, "{\n  s: a\n}\n");
-        await Verify(target, settings);
-
-        File.WriteAllText(file, "{\n  s: a\n}\r\n");
-        await Verify(target, settings);
-    }
-
-#endif
 
     [Fact]
     public async Task DanglingFiles()

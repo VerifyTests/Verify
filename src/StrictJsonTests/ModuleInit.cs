ff55d84e020fc4270777e775f67f2bdb4802fcd8
Sean Feldman <SeanFeldman@users.noreply.github.com>
2023-01-08 20:16:53 -0500

    Hint at closing the method (#759)

--- src/StrictJsonTests/ModuleInit.cs
+++ src/StrictJsonTests/ModuleInit.cs
@@ -1,14 +1,15 @@
-﻿public static class ModuleInit
+public static class ModuleInit
 {
     #region UseStrictJson
 
     [ModuleInitializer]
-    public static void Init()
-    {
+    public static void Init() =>
         VerifierSettings.UseStrictJson();
 
-        #endregion
+    #endregion
+
+    [ModuleInitializer]
+    public static void InitDerivePathInfo() =>
         DerivePathInfo(
             (_, _, type, method) => new(AttributeReader.GetProjectDirectory(), typeName: type.Name, method.Name));
-    }
 }

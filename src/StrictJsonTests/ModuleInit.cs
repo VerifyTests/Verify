fa96707a92fbdf2e55e7db1f4d3eac26205807b2
Simon Cropp <simon.cropp@gmail.com>
2023-01-09 12:16:31 +1100

    Update ModuleInit.cs

--- src/StrictJsonTests/ModuleInit.cs
+++ src/StrictJsonTests/ModuleInit.cs
@@ -3,16 +3,13 @@ public static class ModuleInit
     #region UseStrictJson
 
     [ModuleInitializer]
-    public static void Init()
-    {
+    public static void Init() =>
         VerifierSettings.UseStrictJson();
-    }
+
     #endregion
-    
+
     [ModuleInitializer]
-    public static void InitDerivePathInfo()
-    {
+    public static void InitDerivePathInfo() =>
         DerivePathInfo(
             (_, _, type, method) => new(AttributeReader.GetProjectDirectory(), typeName: type.Name, method.Name));
-    }
 }

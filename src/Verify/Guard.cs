6bfc959c9694c5a09b19203494a475664ab12fa9
Simon <simon.cropp@gmail.com>
2023-01-17 08:57:52 +1100

    Update Guard.cs

--- src/Verify/Guard.cs
+++ src/Verify/Guard.cs
@@ -145,7 +145,7 @@
         }
     }
 }
-#if (NETFRAMEWORK || NETSTANDARD)
+#if (NETFRAMEWORK || NETSTANDARD || NETCOREAPP)
 namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Parameter)]

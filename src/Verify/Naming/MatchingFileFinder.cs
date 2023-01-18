da8c897324e0dcf79b9dc1a43a6e440a52119484
Simon Cropp <simon.cropp@gmail.com>
2023-01-19 06:15:58 +1100

    MatchingFileFinder perf

--- src/Verify/Naming/MatchingFileFinder.cs
+++ src/Verify/Naming/MatchingFileFinder.cs
@@ -2,31 +2,34 @@
 {
     public static IEnumerable<string> FindReceived(string fileNamePrefix, string directory)
     {
-        var nonIndexedPattern = $"{fileNamePrefix}.received.*";
-        foreach (var file in Directory.EnumerateFiles(directory, nonIndexedPattern))
+        var nonIndexedPattern = $"{fileNamePrefix}.received.";
+        var indexedPattern = $"{fileNamePrefix}#";
+        foreach (var file in Directory.EnumerateFiles(directory,  $"{fileNamePrefix}*.received.*"))
         {
-            yield return file;
-        }
-
-        var indexedPattern = $"{fileNamePrefix}#??.received.*";
-        foreach (var file in Directory.EnumerateFiles(directory, indexedPattern))
-        {
-            yield return file;
+            if (file.StartsWith(nonIndexedPattern))
+            {
+                yield return file;
+            }
+            if (file.StartsWith(indexedPattern))
+            {
+                yield return file;
+            }
         }
     }
-
     public static IEnumerable<string> FindVerified(string fileNamePrefix, string directory)
     {
-        var nonIndexedPattern = $"{fileNamePrefix}.verified.*";
-        foreach (var file in Directory.EnumerateFiles(directory, nonIndexedPattern))
+        var nonIndexedPattern = $"{fileNamePrefix}.verified.";
+        var indexedPattern = $"{fileNamePrefix}#";
+        foreach (var file in Directory.EnumerateFiles(directory,  $"{fileNamePrefix}*.verified.*"))
         {
-            yield return file;
-        }
-
-        var indexedPattern = $"{fileNamePrefix}#??.verified.*";
-        foreach (var file in Directory.EnumerateFiles(directory, indexedPattern))
-        {
-            yield return file;
+            if (file.StartsWith(nonIndexedPattern))
+            {
+                yield return file;
+            }
+            if (file.StartsWith(indexedPattern))
+            {
+                yield return file;
+            }
         }
     }
 }

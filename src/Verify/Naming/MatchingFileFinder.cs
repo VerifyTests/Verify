1b4ea568d9e92e29f1d54c9c64f56c753a18130c
Simon Cropp <simon.cropp@gmail.com>
2023-01-19 08:04:16 +1100

    .

--- src/Verify/Naming/MatchingFileFinder.cs
+++ src/Verify/Naming/MatchingFileFinder.cs
@@ -4,29 +4,47 @@
     {
         var nonIndexedPattern = $"{fileNamePrefix}.received.";
         var indexedPattern = $"{fileNamePrefix}#";
+        var startIndex = directory.Length+1;
         foreach (var file in Directory.EnumerateFiles(directory,  $"{fileNamePrefix}*.received.*"))
         {
-            if (file.StartsWith(nonIndexedPattern))
+            if (file.SubStringEquals(nonIndexedPattern, startIndex))
             {
                 yield return file;
             }
-            if (file.StartsWith(indexedPattern))
+            else if (file.SubStringEquals(indexedPattern, startIndex))
             {
                 yield return file;
             }
         }
     }
+
+    static bool SubStringEquals(this string value, string match, int startIndex)
+    {
+        for (int i = 0; i < match.Length; i++)
+        {
+            var c = value[startIndex + i];
+            var l = match[i];
+            if (c != l)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static IEnumerable<string> FindVerified(string fileNamePrefix, string directory)
     {
         var nonIndexedPattern = $"{fileNamePrefix}.verified.";
         var indexedPattern = $"{fileNamePrefix}#";
+        var startIndex = directory.Length+1;
         foreach (var file in Directory.EnumerateFiles(directory,  $"{fileNamePrefix}*.verified.*"))
         {
-            if (file.StartsWith(nonIndexedPattern))
+            if (file.SubStringEquals(nonIndexedPattern, startIndex))
             {
                 yield return file;
             }
-            if (file.StartsWith(indexedPattern))
+            else if (file.SubStringEquals(indexedPattern, startIndex))
             {
                 yield return file;
             }

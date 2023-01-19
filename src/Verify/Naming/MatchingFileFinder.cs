9c56f67ba463ee9ddec35644c3d8d25eef3767cb
Simon Cropp <simon.cropp@gmail.com>
2023-01-19 11:49:22 +1100

    Matching file finder perf (#768)

--- src/Verify/Naming/MatchingFileFinder.cs
+++ src/Verify/Naming/MatchingFileFinder.cs
@@ -2,31 +2,50 @@
 {
     public static IEnumerable<string> FindReceived(string fileNamePrefix, string directory)
     {
-        var nonIndexedPattern = $"{fileNamePrefix}.received.*";
-        foreach (var file in Directory.EnumerateFiles(directory, nonIndexedPattern))
+        var nonIndexedPattern = $"{fileNamePrefix}.received.";
+        var indexedPattern = $"{fileNamePrefix}#";
+        var startIndex = directory.Length + 1;
+        foreach (var file in Directory.GetFiles(directory, $"{fileNamePrefix}*.received.*"))
         {
-            yield return file;
-        }
-
-        var indexedPattern = $"{fileNamePrefix}#??.received.*";
-        foreach (var file in Directory.EnumerateFiles(directory, indexedPattern))
-        {
-            yield return file;
+            if (file.SubStringEquals(nonIndexedPattern, startIndex))
+            {
+                yield return file;
+            }
+            else if (file.SubStringEquals(indexedPattern, startIndex))
+            {
+                yield return file;
+            }
         }
     }
 
     public static IEnumerable<string> FindVerified(string fileNamePrefix, string directory)
     {
-        var nonIndexedPattern = $"{fileNamePrefix}.verified.*";
-        foreach (var file in Directory.EnumerateFiles(directory, nonIndexedPattern))
+        var nonIndexedPattern = $"{fileNamePrefix}.verified.";
+        var indexedPattern = $"{fileNamePrefix}#";
+        var startIndex = directory.Length + 1;
+        foreach (var file in Directory.GetFiles(directory, $"{fileNamePrefix}*.verified.*"))
         {
-            yield return file;
+            if (file.SubStringEquals(nonIndexedPattern, startIndex))
+            {
+                yield return file;
+            }
+            else if (file.SubStringEquals(indexedPattern, startIndex))
+            {
+                yield return file;
+            }
+        }
+    }
+
+    static bool SubStringEquals(this string value, string match, int startIndex)
+    {
+        for (var index = 0; index < match.Length; index++)
+        {
+            if (value[startIndex + index] != match[index])
+            {
+                return false;
+            }
         }
 
-        var indexedPattern = $"{fileNamePrefix}#??.verified.*";
-        foreach (var file in Directory.EnumerateFiles(directory, indexedPattern))
-        {
-            yield return file;
-        }
+        return true;
     }
 }

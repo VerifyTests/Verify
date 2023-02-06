2cd25f4b6fd4f23f1d57b83889beb09c2edd7fd8
Simon Cropp <simon.cropp@gmail.com>
2023-02-06 14:37:43 +1100

    Add InitializePlugins (#781)

---
+++ src/Verify.Tests/PluginConventionTests.cs
@@ -0,0 +1,53 @@
+﻿
+#pragma warning disable CS0618
+public class PluginConventionTests :
+    XunitContextBase
+{
+    [ModuleInitializer]
+    public static void Init() =>
+        VerifierSettings.InitializePlugins();
+
+    [Fact]
+    public void Find() =>
+        Assert.True(VerifySamplePlugin.Initialized);
+
+    [Fact]
+    public void TryGetType()
+    {
+        Assert.True(VerifierSettings.TryGetType(typeof(VerifySamplePlugin).Assembly.Location, out var type));
+        Assert.Same(typeof(VerifySamplePlugin), type);
+    }
+
+    [Fact]
+    public void InvokeInitialize()
+    {
+        VerifierSettings.InvokeInitialize(typeof(InitializeTarget));
+        Assert.True(InitializeTarget.Initialized);
+    }
+
+    public static class InitializeTarget
+    {
+        public static bool Initialized { get; private set; }
+
+        public static void Initialize() =>
+            Initialized = true;
+    }
+
+    [Fact]
+    public void InvokeInitialized()
+    {
+        Assert.False(VerifierSettings.GetInitialized(typeof(InitializedTarget)));
+        InitializedTarget.Initialized = true;
+        Assert.True(VerifierSettings.GetInitialized(typeof(InitializedTarget)));
+    }
+
+    public static class InitializedTarget
+    {
+        public static bool Initialized { get; set; }
+    }
+
+    public PluginConventionTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
---
+++ src/Verify/VerifierSettings_PluginConvention.cs
@@ -0,0 +1,77 @@
+﻿namespace VerifyTests;
+
+public static partial class VerifierSettings
+{
+    public static void InitializePlugins()
+    {
+        InnerVerifier.ThrowIfVerifyHasBeenRun();
+        var directory = Path.GetDirectoryName(GetLocation())!;
+        foreach (var file in Directory.EnumerateFiles(directory, "Verify.*.dll"))
+        {
+            ProcessFile(file);
+        }
+    }
+
+    static string GetLocation()
+    {
+        var assembly = typeof(VerifierSettings).Assembly;
+        // ReSharper disable once RedundantSuppressNullableWarningExpression
+#pragma warning disable SYSLIB0012
+        return new Uri(assembly.CodeBase!).LocalPath;
+#pragma warning restore SYSLIB0012
+    }
+
+    static void ProcessFile(string file)
+    {
+        if (!TryGetType(file, out var type))
+        {
+            Console.WriteLine($"Did not find type in {file}");
+            return;
+        }
+
+        if (GetInitialized(type))
+        {
+            return;
+        }
+
+        InvokeInitialize(type);
+    }
+
+    internal static bool TryGetType(string file, [NotNullWhen(true)] out Type? type)
+    {
+        var name = Path.GetFileNameWithoutExtension(file);
+        if (!name.StartsWith("Verify."))
+        {
+            type = null;
+            return false;
+        }
+#pragma warning disable CS0618
+        var assembly = Assembly.LoadWithPartialName(name)!;
+#pragma warning restore CS0618
+        var typeName = name.Replace("Verify.", "VerifyTests.Verify");
+        type = assembly.GetType(typeName);
+        return type != null;
+    }
+
+    internal static void InvokeInitialize(Type type)
+    {
+        var method = type.GetMethod("Initialize", BindingFlags.Static | BindingFlags.Public);
+        if (method == null)
+        {
+            throw new($"Expected {type.Name} to have a method `public static void Initialize()`.");
+        }
+
+        method.Invoke(null, null);
+    }
+
+    internal static bool GetInitialized(Type type)
+    {
+        var property = type.GetProperty("Initialized", BindingFlags.Static | BindingFlags.Public | BindingFlags.GetProperty);
+        if (property == null)
+        {
+            throw new($"Expected {type.Name} to have a property `public static bool Initialized {{get;}}` that indicates if Initialize() has been called.");
+        }
+
+        return (bool) property.GetValue(null)!;
+    }
+}

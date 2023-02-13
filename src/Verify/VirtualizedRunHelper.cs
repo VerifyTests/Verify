b01fa0816001c7a9b54bad50f75c4ff320fdb147
Simon <simon.cropp@gmail.com>
2023-02-13 14:50:23 +1100

    Update VirtualizedRunHelper.cs

--- src/Verify/VirtualizedRunHelper.cs
+++ src/Verify/VirtualizedRunHelper.cs
@@ -1,6 +1,7 @@
 class VirtualizedRunHelper
 {
     internal static IEnvironment Env { private get; set; } = PhysicalEnvironment.Instance;
+
     // e.g. WSL or docker run (https://github.com/VerifyTests/Verify#unit-testing-inside-virtualized-environment)
     internal bool AppearsToBeLocalVirtualizedRun { get; private set; }
     internal bool Initialized { get; private set; }
@@ -18,7 +19,8 @@ class VirtualizedRunHelper
         : this(
             AttributeReader.TryGetSolutionDirectory(userAssembly, false, out var sln) ? sln : string.Empty,
             AttributeReader.TryGetProjectDirectory(userAssembly, false, out var proj) ? proj : string.Empty)
-    { }
+    {
+    }
 
     internal VirtualizedRunHelper(string solutionDir, string projectDir)
     {
@@ -98,8 +100,8 @@ class VirtualizedRunHelper
     static bool InnerTryInitializeFromBuildTimePath(
         string originalCodeBaseRoot,
         string buildTimePath,
-        [NotNullWhen(true)]out string? mappedCodeBaseRootAbsolute,
-        [NotNullWhen(true)]out string? originalCodeBaseRootAbsolute)
+        [NotNullWhen(true)] out string? mappedCodeBaseRootAbsolute,
+        [NotNullWhen(true)] out string? originalCodeBaseRootAbsolute)
     {
         // First attempt - by the cross-section of the build-time path and run-time path
         if (TryFindByCrossSectionOfBuildRunPath(originalCodeBaseRoot, out mappedCodeBaseRootAbsolute, out originalCodeBaseRootAbsolute))

5b6c5329ecd54d1daf77611b519111408596e4ff
Simon Cropp <simon.cropp@gmail.com>
2023-03-10 22:19:02 +1100

    Argon 0.5.0

--- src/Verify/Serialization/Converters/EncodingConverter.cs
+++
@@ -1,6 +0,0 @@
-class EncodingConverter :
-    WriteOnlyJsonConverter<Encoding>
-{
-    public override void Write(VerifyJsonWriter writer, Encoding value) =>
-        writer.WriteRawValueIfNoStrict(value.EncodingName);
-}

67362b335c474b8da5181fd1df4079e6f3b11371
Simon Cropp <simon.cropp@gmail.com>
2023-03-13 22:26:14 +1100

    move to polyfill

--- src/Verify.Xunit.Tests/ModuleInitializerAttribute.cs
+++
@@ -1,8 +0,0 @@
-#if(!NET5_0_OR_GREATER)
-namespace System.Runtime.CompilerServices;
-
-[AttributeUsage(AttributeTargets.Method, Inherited = false)]
-sealed class ModuleInitializerAttribute : Attribute
-{
-}
-#endif
--- src/Verify/Guard.cs
+++ src/Verify/Guard.cs
@@ -145,16 +145,3 @@
         }
     }
 }
-#if (NETFRAMEWORK || NETSTANDARD || NETCOREAPP)
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Parameter)]
-    sealed class CallerArgumentExpressionAttribute : Attribute
-    {
-        public CallerArgumentExpressionAttribute(string parameterName) =>
-            ParameterName = parameterName;
-
-        public string ParameterName { get; }
-    }
-}
-#endif

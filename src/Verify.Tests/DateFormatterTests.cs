1e17173fef3005b3893787bd9ad737b87d92935f
Simon <simon.cropp@gmail.com>
2023-03-28 15:45:55 +1100

    more date tests

--- src/Verify.Tests/DateFormatterTests.cs
+++ src/Verify.Tests/DateFormatterTests.cs
@@ -84,4 +84,107 @@ public class DateFormatterTests
         var date = new DateTimeOffset(2000, 10, 1, 0, 0, 0, TimeSpan.Zero);
         return Verify(DateFormatter.ToParameterString(date));
     }
+
+    [Fact]
+    public async Task DateTimeOffsetCombinations()
+    {
+        var jsonValues = new Dictionary<string, string>();
+        var parameterValues = new Dictionary<string, string>();
+
+        foreach (var offset in bools)
+        foreach (var hour in bools)
+        foreach (var minute in bools)
+        foreach (var second in bools)
+        foreach (var tick in bools)
+        {
+            var name = new StringBuilder();
+            var timeSpan = TimeSpan.Zero;
+            if (offset)
+            {
+                name.Append("_offset");
+                timeSpan = new(7, 8, 0);
+            }
+
+            var value = new DateTimeOffset(2020, 1, 1, 0, 0, 0, timeSpan);
+            if (hour)
+            {
+                name.Append("_hour");
+                value = value.AddHours(2);
+            }
+
+            if (minute)
+            {
+                name.Append("_minute");
+                value = value.AddMinutes(3);
+            }
+
+            if (second)
+            {
+                name.Append("_second");
+                value = value.AddMinutes(4);
+            }
+
+            if (tick)
+            {
+                name.Append("_tick");
+                value = value.AddTicks(6);
+            }
+
+            jsonValues.Add(name.ToString(), DateFormatter.ToJsonString(value));
+            parameterValues.Add(name.ToString(), DateFormatter.ToParameterString(value));
+        }
+
+        await Verify(new{jsonValues, parameterValues});
+    }
+
+    [Fact]
+    public async Task DateTimeCombinations()
+    {
+        var jsonValues = new Dictionary<string, string>();
+        var parameterValues = new Dictionary<string, string>();
+
+        foreach (var kind in new[]{DateTimeKind.Local,DateTimeKind.Unspecified,DateTimeKind.Utc})
+        foreach (var hour in bools)
+        foreach (var minute in bools)
+        foreach (var second in bools)
+        foreach (var tick in bools)
+        {
+            var name = new StringBuilder(kind.ToString());
+            var value = new DateTime(2020, 1, 1, 0, 0, 0,kind);
+            if (hour)
+            {
+                name.Append("_hour");
+                value = value.AddHours(2);
+            }
+
+            if (minute)
+            {
+                name.Append("_minute");
+                value = value.AddMinutes(3);
+            }
+
+            if (second)
+            {
+                name.Append("_second");
+                value = value.AddMinutes(4);
+            }
+
+            if (tick)
+            {
+                name.Append("_tick");
+                value = value.AddTicks(6);
+            }
+
+            jsonValues.Add(name.ToString(), DateFormatter.ToJsonString(value));
+            parameterValues.Add(name.ToString(), DateFormatter.ToParameterString(value));
+        }
+
+        await Verify(new{jsonValues, parameterValues});
+    }
+
+    static bool[] bools =
+    {
+        true,
+        false
+    };
 }

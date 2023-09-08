d48004629fa81d05d9b120da9858fa9910544ac4
Simon Cropp <simon.cropp@gmail.com>
2023-09-08 15:46:03 +1000

    Update VerifySettings_ExtensionMappedInstanceScrubbers.cs

--- src/Verify/Serialization/Scrubbers/VerifySettings_ExtensionMappedInstanceScrubbers.cs
+++ src/Verify/Serialization/Scrubbers/VerifySettings_ExtensionMappedInstanceScrubbers.cs
@@ -59,26 +59,26 @@ public partial class VerifySettings
     /// Replace inline <see cref="Guid" />s with a placeholder.
     /// </summary>
     public void ScrubInlineGuids(string extension, ScrubberLocation location = ScrubberLocation.First) =>
-        AddScrubber(extension,GuidScrubber.ReplaceGuids, location);
+        AddScrubber(extension, GuidScrubber.ReplaceGuids, location);
 
     /// <summary>
     /// Remove any lines matching <paramref name="removeLine" /> from the test results.
     /// </summary>
     public void ScrubLines(string extension, Func<string, bool> removeLine, ScrubberLocation location = ScrubberLocation.First) =>
-        AddScrubber(extension,_ => _.FilterLines(removeLine), location);
+        AddScrubber(extension, _ => _.FilterLines(removeLine), location);
 
     /// <summary>
     /// Scrub lines with an optional replace.
     /// <paramref name="replaceLine" /> can return the input to ignore the line, or return a different string to replace it.
     /// </summary>
     public void ScrubLinesWithReplace(string extension, Func<string, string?> replaceLine, ScrubberLocation location = ScrubberLocation.First) =>
-        AddScrubber(extension,_ => _.ReplaceLines(replaceLine), location);
+        AddScrubber(extension, _ => _.ReplaceLines(replaceLine), location);
 
     /// <summary>
     /// Remove any lines containing only whitespace from the test results.
     /// </summary>
     public void ScrubEmptyLines(string extension, ScrubberLocation location = ScrubberLocation.First) =>
-        AddScrubber(extension,_ => _.RemoveEmptyLines(), location);
+        AddScrubber(extension, _ => _.RemoveEmptyLines(), location);
 
     /// <summary>
     /// Remove any lines containing any of <paramref name="stringToMatch" /> from the test results.

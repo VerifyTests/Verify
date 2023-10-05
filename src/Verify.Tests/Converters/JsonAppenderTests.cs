ff2132299791e9907f9c44e19bd4ae7560df8e9e
Simon Cropp <simon.cropp@gmail.com>
2023-10-05 17:10:08 +1100

    docs

--- src/Verify.Tests/Converters/JsonAppenderTests.cs
+++ src/Verify.Tests/Converters/JsonAppenderTests.cs
@@ -80,7 +80,8 @@ public class JsonAppenderTests : IDisposable
             "info",
             new[]
             {
-                new Target("bin",
+                new Target(
+                    "bin",
                     new MemoryStream(new byte[]
                     {
                         1

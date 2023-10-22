3c2a924bad87b4f5ca1034db0b3bd7bcbdb99a7a
Simon Cropp <simon.cropp@gmail.com>
2023-10-22 20:31:48 +1100

    .

--- src/Verify.Tests/RecordingTests.cs
+++ src/Verify.Tests/RecordingTests.cs
@@ -24,10 +24,18 @@ public class RecordingTests
     }
 
     [Fact]
-    public Task Overwrite()
+    public Task Append()
     {
         Recording.Add("name", "value1");
         Recording.Add("name", "value2");
+        return Verify("TheValue");
+    }
+
+    [Fact]
+    public Task Case()
+    {
+        Recording.Add("name", "value1");
+        Recording.Add("Name", "value2");
         return Verify("TheValue");
     }
 }

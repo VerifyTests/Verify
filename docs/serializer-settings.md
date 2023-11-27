62b68a84fda65a109e79f48e0b61b8223f637ff2
Simon Cropp <simon.cropp@gmail.com>
2023-11-27 22:14:53 +1100

    delay

--- docs/serializer-settings.md
+++ docs/serializer-settings.md
@@ -81,7 +81,7 @@ The default encoding for snapshot files uses UTF-8 with byte order marks (BOM) e
 ```cs
 VerifierSettings.UseUtf8NoBom();
 ```
-<sup><a href='/src/StaticSettingsTests/EncodingTests.cs#L14-L16' title='Snippet source file'>snippet source</a> | <a href='#snippet-useutf8nobom' title='Start of snippet'>anchor</a></sup>
+<sup><a href='/src/StaticSettingsTests/EncodingTests.cs#L8-L10' title='Snippet source file'>snippet source</a> | <a href='#snippet-useutf8nobom' title='Start of snippet'>anchor</a></sup>
 <!-- endSnippet -->
 
 ## UseEncoding
@@ -91,10 +91,13 @@ To override the encoding used for snapshot files, replacing the default UTF-8 en
 <!-- snippet: UseEncoding -->
 <a id='snippet-useencoding'></a>
 ```cs
-var encoding = new UnicodeEncoding(bigEndian: false, byteOrderMark: true, throwOnInvalidBytes: true);
+var encoding = new UnicodeEncoding(
+    bigEndian: false,
+    byteOrderMark: true,
+    throwOnInvalidBytes: true);
 VerifierSettings.UseEncoding(encoding);
 ```
-<sup><a href='/src/StaticSettingsTests/EncodingTests.cs#L31-L34' title='Snippet source file'>snippet source</a> | <a href='#snippet-useencoding' title='Start of snippet'>anchor</a></sup>
+<sup><a href='/src/StaticSettingsTests/EncodingTests.cs#L26-L32' title='Snippet source file'>snippet source</a> | <a href='#snippet-useencoding' title='Start of snippet'>anchor</a></sup>
 <!-- endSnippet -->
 
 ## Default settings
--- src/StaticSettingsTests/EncodingTests.cs
+++ src/StaticSettingsTests/EncodingTests.cs
@@ -5,16 +5,16 @@ public class EncodingTests :
     [Fact]
     public async Task UseUtf8NoBom()
     {
+        #region UseUtf8NoBom
         VerifierSettings.UseUtf8NoBom();
+        #endregion
 
         var file = CurrentFile.Relative($"EncodingTests.{nameof(UseUtf8NoBom)}.verified.txt");
         File.Delete(file);
         var str = "value";
         var encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
-        #region UseUtf8NoBom
-        VerifierSettings.UseUtf8NoBom();
-        #endregion
         await Verify(str).AutoVerify();
+        await Task.Delay(1000);
         var fileBytes = await File.ReadAllBytesAsync(file);
         var expectedBytes = encoding.GetBytes(str);
         Assert.Equal(expectedBytes, fileBytes);
@@ -23,16 +23,19 @@ public class EncodingTests :
     [Fact]
     public async Task Utf16()
     {
-        VerifierSettings.UseUtf8NoBom();
+        #region UseEncoding
+        var encoding = new UnicodeEncoding(
+            bigEndian: false,
+            byteOrderMark: true,
+            throwOnInvalidBytes: true);
+        VerifierSettings.UseEncoding(encoding);
+        #endregion
 
         var file = CurrentFile.Relative($"EncodingTests.{nameof(Utf16)}.verified.txt");
         File.Delete(file);
         var str = "value";
-        #region UseEncoding
-        var encoding = new UnicodeEncoding(bigEndian: false, byteOrderMark: true, throwOnInvalidBytes: true);
-        VerifierSettings.UseEncoding(encoding);
-        #endregion
         await Verify(str).AutoVerify();
+        await Task.Delay(1000);
         var fileBytes = await File.ReadAllBytesAsync(file);
         var expectedBytes = encoding.GetPreamble().Concat(encoding.GetBytes(str)).ToArray();
         Assert.Equal(expectedBytes, fileBytes);

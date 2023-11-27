5dbe55bcf75ac3611d0bbf775ef9bb26cc654e8c
Joel Verhagen <joel.verhagen@gmail.com>
2023-11-26 22:55:26 -0500

    Remove runtime and version from test file, broke on CI and we only have on TFM anyway

--- src/StaticSettingsTests/Tests.cs
+++ src/StaticSettingsTests/Tests.cs
@@ -17,16 +17,14 @@ public class Tests
     [Fact]
     public async Task NonStandardEncoding_DisableUtf8Bom()
     {
-        var file = CurrentFile.Relative($"Tests.{nameof(NonStandardEncoding_DisableUtf8Bom)}.{Namer.RuntimeAndVersion}.verified.txt");
+        var file = CurrentFile.Relative($"Tests.{nameof(NonStandardEncoding_DisableUtf8Bom)}.verified.txt");
         File.Delete(file);
         var str = "value";
         var encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
         #region UseUtf8NoBom
         VerifierSettings.UseUtf8NoBom();
         #endregion
-        await Verify(str)
-            .UniqueForRuntimeAndVersion()
-            .AutoVerify();
+        await Verify(str).AutoVerify();
         var fileBytes = File.ReadAllBytes(file);
         var expectedBytes = encoding.GetBytes(str);
         Assert.Equal(expectedBytes, fileBytes);
@@ -35,16 +33,14 @@ public class Tests
     [Fact]
     public async Task NonStandardEncoding_Utf16()
     {
-        var file = CurrentFile.Relative($"Tests.{nameof(NonStandardEncoding_Utf16)}.{Namer.RuntimeAndVersion}.verified.txt");
+        var file = CurrentFile.Relative($"Tests.{nameof(NonStandardEncoding_Utf16)}.verified.txt");
         File.Delete(file);
         var str = "value";
         #region UseEncoding
         var encoding = new UnicodeEncoding(bigEndian: false, byteOrderMark: true, throwOnInvalidBytes: true);
         VerifierSettings.UseEncoding(encoding);
         #endregion
-        await Verify(str)
-            .UniqueForRuntimeAndVersion()
-            .AutoVerify();
+        await Verify(str).AutoVerify();
         var fileBytes = File.ReadAllBytes(file);
         var expectedBytes = encoding.GetPreamble().Concat(encoding.GetBytes(str)).ToArray();
         Assert.Equal(expectedBytes, fileBytes);

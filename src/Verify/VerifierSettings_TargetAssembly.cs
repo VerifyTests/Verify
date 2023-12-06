6635eca0f348e6d4a685a864e08caeb0c816fcb3
Simon Cropp <simon.cropp@gmail.com>
2023-12-07 10:08:02 +1100

    refs

--- src/Verify/VerifierSettings_TargetAssembly.cs
+++ src/Verify/VerifierSettings_TargetAssembly.cs
@@ -3,8 +3,10 @@
 public static partial class VerifierSettings
 {
     static Assembly? assembly;
+
     [Experimental("VerifyTestsProjectDir")]
     public static string ProjectDir { get; private set; } = null!;
+
     internal static string? SolutionDir { get; private set; }
     internal static bool TargetsMultipleFramework { get; private set; } = true;
 

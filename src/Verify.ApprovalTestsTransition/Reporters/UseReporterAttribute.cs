5184586cea3e15b0da18c605ccc223acd347557d
Simon Cropp <simon.cropp@gmail.com>
2024-03-04 13:49:08 +1100

    cleanup

--- src/Verify.ApprovalTestsTransition/Reporters/UseReporterAttribute.cs
+++ src/Verify.ApprovalTestsTransition/Reporters/UseReporterAttribute.cs
@@ -4,7 +4,7 @@ namespace ApprovalTests.Reporters;
 
 [Obsolete("Verify does not use reporters. For Clipboard settings see https://github.com/VerifyTests/Verify/blob/main/docs/clipboard.md. " + DiffReporter.Error, true)]
 [AttributeUsage(AttributeTargets.All)]
-public class UseReporterAttribute : Attribute
+public sealed class UseReporterAttribute : Attribute
 {
     public UseReporterAttribute(params Type[] reporters)
     {

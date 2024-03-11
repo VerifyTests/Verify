541b44abfc19bc0c38bb8f6f5d2b9e9561b32c45
Simon Cropp <simon.cropp@gmail.com>
2024-03-11 20:15:50 +1100

    Update AutoVerify.cs

--- src/StaticSettingsTests/AutoVerify.cs
+++ src/StaticSettingsTests/AutoVerify.cs
@@ -9,6 +9,51 @@
         var fullPath = Path.GetFullPath(path);
         File.Delete(fullPath);
         await Verify("Foo");
+        Assert.True(File.Exists(fullPath));
+        File.Delete(fullPath);
+    }
+
+    [Fact]
+    public async Task DelegateTrue()
+    {
+        var funcCalled = false;
+        VerifierSettings.AutoVerify(
+            (typeName, methodName) =>
+            {
+                Assert.NotNull(typeName);
+                Assert.NotNull(methodName);
+                funcCalled = true;
+                return true;
+            });
+        var path = CurrentFile.Relative("AutoVerify.DelegateTrue.verified.txt");
+        var fullPath = Path.GetFullPath(path);
+        File.Delete(fullPath);
+        await Verify("Foo");
+        Assert.True(File.Exists(fullPath));
+        Assert.True(funcCalled);
+        File.Delete(fullPath);
+    }
+
+    [Fact]
+    public async Task DelegateFalse()
+    {
+        var funcCalled = false;
+        VerifierSettings.AutoVerify(
+            (typeName, methodName) =>
+            {
+                Assert.NotNull(typeName);
+                Assert.NotNull(methodName);
+                funcCalled = true;
+                return false;
+            });
+        var path = CurrentFile.Relative("AutoVerify.DelegateFalse.verified.txt");
+        var fullPath = Path.GetFullPath(path);
+        File.Delete(fullPath);
+        await Assert.ThrowsAsync<VerifyException>(
+            () => Verify("Foo")
+                .DisableDiff());
+        Assert.False(File.Exists(fullPath));
+        Assert.True(funcCalled);
         File.Delete(fullPath);
     }
 }

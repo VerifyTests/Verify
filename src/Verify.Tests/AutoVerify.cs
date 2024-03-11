f3e7cd13816831668dccc997dcb5076d03deb8ad
Simon Cropp <simon.cropp@gmail.com>
2024-03-11 22:01:53 +1100

    .

---
+++ src/Verify.Tests/AutoVerify.cs
@@ -0,0 +1,110 @@
+﻿public class AutoVerify
+{
+    [Fact]
+    public async Task Simple()
+    {
+        var path = CurrentFile.Relative("AutoVerify.Simple.verified.txt");
+        var fullPath = Path.GetFullPath(path);
+        File.Delete(fullPath);
+        await Verify("Foo")
+            .AutoVerify();
+        Assert.True(File.Exists(fullPath));
+        File.Delete(fullPath);
+    }
+
+    [Fact]
+    public async Task DelegateTrue()
+    {
+        var funcCalled = false;
+        var path = CurrentFile.Relative("AutoVerify.DelegateTrue.verified.txt");
+        var fullPath = Path.GetFullPath(path);
+        File.Delete(fullPath);
+        await Verify("Foo")
+            .AutoVerify(
+                (typeName, methodName, file) =>
+                {
+                    Assert.NotNull(typeName);
+                    Assert.NotNull(methodName);
+                    Assert.NotNull(file);
+                    funcCalled = true;
+                    return true;
+                });
+        Assert.True(File.Exists(fullPath));
+        Assert.True(funcCalled);
+        File.Delete(fullPath);
+    }
+
+    [Fact]
+    public async Task DelegateTrueCustomNames()
+    {
+        var funcCalled = false;
+        var path = CurrentFile.Relative("diffTypeNameTrue.diffMethodName.verified.txt");
+        var fullPath = Path.GetFullPath(path);
+        File.Delete(fullPath);
+        await Verify("Foo")
+            .UseTypeName("diffTypeNameTrue")
+            .UseMethodName("diffMethodName")
+            .AutoVerify(
+                (typeName, methodName, file) =>
+                {
+                    Assert.Equal("diffTypeNameTrue", typeName);
+                    Assert.Equal("diffMethodName", methodName);
+                    Assert.NotNull(file);
+                    funcCalled = true;
+                    return true;
+                });
+        Assert.True(File.Exists(fullPath));
+        Assert.True(funcCalled);
+        File.Delete(fullPath);
+    }
+
+    [Fact]
+    public async Task DelegateFalse()
+    {
+        var funcCalled = false;
+        var path = CurrentFile.Relative("AutoVerify.DelegateFalse.verified.txt");
+        var fullPath = Path.GetFullPath(path);
+        File.Delete(fullPath);
+        await Assert.ThrowsAsync<VerifyException>(
+            () => Verify("Foo")
+                .DisableDiff()
+                .AutoVerify(
+                    (typeName, methodName, file) =>
+                    {
+                        Assert.NotNull(typeName);
+                        Assert.NotNull(methodName);
+                        Assert.NotNull(file);
+                        funcCalled = true;
+                        return false;
+                    }));
+        Assert.False(File.Exists(fullPath));
+        Assert.True(funcCalled);
+        File.Delete(fullPath);
+    }
+
+    [Fact]
+    public async Task DelegateFalseCustomNames()
+    {
+        var funcCalled = false;
+        var path = CurrentFile.Relative("diffTypeNameFalse.diffMethodName.verified.txt");
+        var fullPath = Path.GetFullPath(path);
+        File.Delete(fullPath);
+        await Assert.ThrowsAsync<VerifyException>(
+            () => Verify("Foo")
+                .DisableDiff()
+                .UseTypeName("diffTypeNameFalse")
+                .UseMethodName("diffMethodName")
+                .AutoVerify(
+                    (typeName, methodName, file) =>
+                    {
+                        Assert.Equal("diffTypeNameFalse", typeName);
+                        Assert.Equal("diffMethodName", methodName);
+                        Assert.NotNull(file);
+                        funcCalled = true;
+                        return false;
+                    }));
+        Assert.False(File.Exists(fullPath));
+        Assert.True(funcCalled);
+        File.Delete(fullPath);
+    }
+}

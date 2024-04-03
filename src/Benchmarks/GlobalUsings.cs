68ab2ec9d8568d9b4c7f6680c19d9901a5f02fd9
Simon Cropp <simon.cropp@gmail.com>
2024-04-03 18:46:21 +1100

    ScrubStackTraceBenchmarks

--- src/Benchmarks/GlobalUsings.cs
+++ src/Benchmarks/GlobalUsings.cs
@@ -1,2 +1,3 @@
 ﻿global using BenchmarkDotNet.Attributes;
 global using BenchmarkDotNet.Running;
+global using VerifyTests;
---
+++ src/Benchmarks/ScrubStackTraceBenchmarks.cs
@@ -0,0 +1,15 @@
+﻿[MemoryDiagnoser(false)]
+public class ScrubStackTraceBenchmarks
+{
+    [Benchmark]
+    public void ScrubStackTrace() =>
+        Scrubbers.ScrubStackTrace(
+            """
+            Elmah.TestException: This is a test exception that can be safely ignored.
+                at Elmah.ErrorLogPageFactory.FindHandler(String name) in C:\ELMAH\src\Elmah\ErrorLogPageFactory.cs:line 126
+                at Elmah.ErrorLogPageFactory.GetHandler(HttpContext context, String requestType, String url, String pathTranslated) in C:\ELMAH\src\Elmah\ErrorLogPageFactory.cs:line 66
+                at System.Web.HttpApplication.MapHttpHandler(HttpContext context, String requestType, VirtualPath path, String pathTranslated, Boolean useAppConfig)
+                at System.Web.HttpApplication.MapHandlerExecutionStep.Execute()
+                at System.Web.HttpApplication.ExecuteStep(IExecutionStep step, Boolean& completedSynchronously)
+            """);
+}

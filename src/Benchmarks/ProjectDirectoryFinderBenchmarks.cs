1fe6e14a4f76d9a76311304ef73ffeb063c6c0a1
Simon Cropp <simon.cropp@gmail.com>
2024-04-04 19:18:35 +1100

    Create ProjectDirectoryFinderBenchmarks.cs

---
+++ src/Benchmarks/ProjectDirectoryFinderBenchmarks.cs
@@ -0,0 +1,14 @@
+﻿[MemoryDiagnoser(false)]
+public class ProjectDirectoryFinderBenchmarks
+{
+    static string directory = Path.Combine(
+        AttributeReader.GetSolutionDirectory(),
+        @"Verify.Tests\VerifyDirectoryTests.WithDirectory");
+
+    [Benchmark]
+    public string? ScrubStackTrace()
+    {
+        ProjectDirectoryFinder.TryFind(directory, out var path);
+        return path;
+    }
+}

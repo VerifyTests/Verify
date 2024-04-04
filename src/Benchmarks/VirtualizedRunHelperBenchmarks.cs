d3f6ab30c16e3fd8e52758528db7874f37037ab1
Simon Cropp <simon.cropp@gmail.com>
2024-04-04 21:10:09 +1100

    Create VirtualizedRunHelperBenchmarks.cs

---
+++ src/Benchmarks/VirtualizedRunHelperBenchmarks.cs
@@ -0,0 +1,21 @@
+﻿[MemoryDiagnoser(false)]
+public class VirtualizedRunHelperBenchmarks
+{
+    static string solutionDirectory;
+    static string projectDirectory;
+    static string currentDirectory;
+
+    static VirtualizedRunHelperBenchmarks()
+    {
+        currentDirectory = Environment.CurrentDirectory;
+        solutionDirectory = SolutionDirectoryFinder.Find(currentDirectory);
+        projectDirectory = Path.Combine(solutionDirectory, "Verify.Tests");
+    }
+
+    [Benchmark]
+    public string GetMappedBuildPath()
+    {
+        var helper = new VirtualizedRunHelper(solutionDirectory, projectDirectory);
+        return helper.GetMappedBuildPath(currentDirectory);
+    }
+}

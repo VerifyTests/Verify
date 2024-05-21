54a09387496a1253bd758eed5df3ebb382ee3ede
Matt Kotsenas <matt.kotsenas@gmail.com>
2024-05-20 23:34:59 -0700

    Add more unit tests

--- src/Verify.MSTest.SourceGenerator.Tests/InheritanceTests.cs
+++ src/Verify.MSTest.SourceGenerator.Tests/InheritanceTests.cs
@@ -1,6 +1,44 @@
 public class InheritanceTests(ITestOutputHelper output) : TestBase(output)
 {
     [Fact]
+    public Task HasAttributeOnBaseClass()
+    {
+        var source = """
+            using VerifyMSTest;
+
+            [UsesVerify]
+            public partial class Base
+            {
+            }
+
+            public class Derived : Base
+            {
+            }
+            """;
+
+        return VerifyGenerator(TestDriver.Run(source));
+    }
+
+    [Fact]
+    public Task HasAttributeOnDerivedClass()
+    {
+        var source = """
+            using VerifyMSTest;
+
+            public class Base
+            {
+            }
+
+            [UsesVerify]
+            public partial class Derived : Base
+            {
+            }
+            """;
+
+        return VerifyGenerator(TestDriver.Run(source));
+    }
+
+    [Fact]
     public Task HasAttributeOnBaseAndDerivedClasses()
     {
         var source = """
--- src/Verify.MSTest.SourceGenerator.Tests/NoMatchTests.cs
+++ src/Verify.MSTest.SourceGenerator.Tests/NoMatchTests.cs
@@ -11,4 +11,20 @@ public class NoMatchTests(ITestOutputHelper output) : TestBase(output)
 
         return VerifyGenerator(TestDriver.Run(source));
     }
+
+    [Fact]
+    public Task AttributeFromWrongNamespace()
+    {
+        var source = """
+            [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+            public sealed class UsesVerifyAttribute : Attribute;
+
+            [UsesVerify]
+            public class Foo
+            {
+            }
+            """;
+
+        return VerifyGenerator(TestDriver.Run(source));
+    }
 }

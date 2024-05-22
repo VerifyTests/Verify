c5a758c138626ceb12171c37acbbc4ece09c4b60
Simon Cropp <simon.cropp@gmail.com>
2024-05-23 09:51:23 +1000

    refs and cleanup

--- src/Verify.MSTest.SourceGenerator/IncrementalValuesProviderExtensions.cs
+++ src/Verify.MSTest.SourceGenerator/IncrementalValuesProviderExtensions.cs
@@ -1,7 +1,8 @@
 static class IncrementalValuesProviderExtensions
 {
     public static IncrementalValuesProvider<TSource> WhereNotNull<TSource>(this IncrementalValuesProvider<TSource?> source) where TSource : struct =>
-        source.Where(x => x.HasValue).Select((item, _) => item!.Value);
+        source.Where(_ => _.HasValue)
+            .Select((item, _) => item!.Value);
 
     public static IncrementalValuesProvider<TSource> WhereNotNull<TSource>(this IncrementalValuesProvider<TSource?> source) =>
         source.Where(x => x is not null).Select((item, _) => item!);

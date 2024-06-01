39c9a65ffc397c0ed183def9fe9afacde8b6ac44
Simon Cropp <simon.cropp@gmail.com>
2024-06-01 12:27:49 +1000

    remove shoudly

--- src/Verify.MSTest.SourceGenerator.Tests/GlobalUsings.cs
+++ src/Verify.MSTest.SourceGenerator.Tests/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using System.Collections.Immutable;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
-global using Shouldly;
 global using VerifyMSTest.SourceGenerator;
 global using Xunit.Abstractions;
--- src/Verify.MSTest.SourceGenerator.Tests/TestBase.cs
+++ src/Verify.MSTest.SourceGenerator.Tests/TestBase.cs
@@ -5,15 +5,20 @@ public abstract class TestBase(ITestOutputHelper output)
     private protected TestDriver TestDriver { get; } = new([new UsesVerifyGenerator().AsSourceGenerator()]);
     protected ITestOutputHelper Output { get; } = output;
 
-    private protected async Task VerifyGenerator(GeneratorDriverResults results, IEnumerable<string>? expectedDiagnostics = null)
+    private protected async Task VerifyGenerator(GeneratorDriverResults results, IReadOnlyList<string>? expectedDiagnostics = null)
     {
         var first = results.FirstRun;
         Output.WriteLine($"First run of generators took: {first.TimingInfo.ElapsedTime}");
         var cached = results.CachedRun;
         Output.WriteLine($"Cached re-run of generators took: {cached.TimingInfo.ElapsedTime}");
 
-        expectedDiagnostics ??= [];
-        results.outputCompilation.GetDiagnostics().ShouldAllBe(_ => expectedDiagnostics.Contains(_.Id));
+        if (expectedDiagnostics != null)
+        {
+            foreach (var diagnostic in results.outputCompilation.GetDiagnostics())
+            {
+                Assert.Contains(diagnostic.Id, expectedDiagnostics);
+            }
+        }
 
         await Verify(first.RunResult.SelectGeneratedSources());
 
@@ -22,22 +27,22 @@ public abstract class TestBase(ITestOutputHelper output)
         var trackedSteps1 = first.RunResult.GetTrackedSteps(trackingNames);
         var trackedSteps2 = cached.RunResult.GetTrackedSteps(trackingNames);
 
-        trackedSteps2.Keys.ShouldBe(trackedSteps1.Keys);
+        Assert.Equal(trackedSteps2.Keys, trackedSteps1.Keys);
         foreach (var (key, steps1) in trackedSteps1)
         {
             var steps2 = trackedSteps2[key];
 
-            steps2.Length.ShouldBe(steps1.Length);
+            Assert.Equal(steps2.Length, steps1.Length);
             for (var i = 0; i < steps1.Length; i++)
             {
                 var outputs1 = steps1[i].Outputs;
                 var outputs2 = steps2[i].Outputs;
 
-                outputs1.Select(_ => _.Value)
-                    .ShouldBe(outputs2.Select(_ => _.Value));
-                outputs2.Select(_ => _.Reason)
-                    .ShouldAllBe(_ => _ == IncrementalStepRunReason.Cached ||
-                                      _ == IncrementalStepRunReason.Unchanged);
+                Assert.Equal(outputs1.Select(_ => _.Value), outputs2.Select(_ => _.Value));
+                foreach (var output in outputs2)
+                {
+                    Assert.True(output.Reason is IncrementalStepRunReason.Cached or IncrementalStepRunReason.Unchanged);
+                }
             }
         }
     }

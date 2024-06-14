88abb8b75d13fc1b49b24c7aed8c395cd7579bed
Simon Cropp <simon.cropp@gmail.com>
2024-06-14 19:12:49 +1000

    Create BaseClassTests.cs

---
+++ src/Verify.MSTest.Tests/BaseClassTests.cs
@@ -0,0 +1,11 @@
+﻿[TestClass]
+public class BaseClassTests : VerifyBase
+{
+    [TestMethod]
+    public async Task UseFileName()
+    {
+        var fullPath = Path.GetFullPath("sample.png");
+        await VerifyFile(fullPath)
+            .UseFileName("fileName");
+    }
+}

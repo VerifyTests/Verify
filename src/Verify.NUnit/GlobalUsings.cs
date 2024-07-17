fbbbb6e7f7ad2bb7ba7d85c4a4000d298b622f72
Simon Cropp <simon.cropp@gmail.com>
2024-07-17 21:06:13 +1000

    Update GlobalUsings.cs

--- src/Verify.NUnit/GlobalUsings.cs
+++ src/Verify.NUnit/GlobalUsings.cs
@@ -5,3 +5,5 @@ global using NUnit.Framework.Internal;
 global using NUnit.Framework.Interfaces;
 global using VerifyTests;
 global using Argon;
+
+global using TestAdapter = NUnit.Framework.TestContext.TestAdapter;

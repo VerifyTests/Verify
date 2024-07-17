2e9e67701dd047b13596762ecd5045d9622645a4
Simon Cropp <simon.cropp@gmail.com>
2024-07-17 21:07:00 +1000

    Update GlobalUsings.cs

--- src/Verify.NUnit/GlobalUsings.cs
+++ src/Verify.NUnit/GlobalUsings.cs
@@ -1,7 +1,5 @@
 ﻿global using System.Diagnostics.Contracts;
-global using NUnit;
 global using NUnit.Framework;
 global using NUnit.Framework.Internal;
 global using NUnit.Framework.Interfaces;
 global using VerifyTests;
-global using Argon;

618875756ea89407fd350a28b3a9206e5a8bd967
Simon Cropp <simon.cropp@gmail.com>
2024-07-19 12:05:22 +1000

    .

--- src/Verify.Tests/FileNameCleanerBenchmarksTests.cs
+++
@@ -1,6 +0,0 @@
-﻿public class FileNameCleanerBenchmarksTests
-{
-    [Fact]
-    public Task ReplaceInvalidFileNameChars() =>
-        Verify("Ant apple | The Bear Fox > Theater".ReplaceInvalidFileNameChars());
-}

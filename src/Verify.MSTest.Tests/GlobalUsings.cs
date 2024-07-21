0697db4f50f58da34e5cd3efbdc33ae359630617
Simon Cropp <simon.cropp@gmail.com>
2024-07-21 13:30:06 +1000

    refs or cleanup

--- src/Verify.MSTest.Tests/GlobalUsings.cs
+++ src/Verify.MSTest.Tests/GlobalUsings.cs
@@ -1,3 +1,1 @@
-﻿// Global using directives
-
-global using System.Diagnostics;
+﻿global using System.Diagnostics;
--- src/Verify.NUnit.Tests/GlobalUsings.cs
+++ src/Verify.NUnit.Tests/GlobalUsings.cs
@@ -1,4 +1,2 @@
-﻿// Global using directives
-
-global using NUnit.Framework.Interfaces;
+﻿global using NUnit.Framework.Interfaces;
 global using NUnit.Framework.Internal;

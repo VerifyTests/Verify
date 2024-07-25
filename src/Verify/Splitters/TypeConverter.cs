fefc3241d9e54f4389b56a68f872a2cf39c49ebc
Simon Cropp <simon.cropp@gmail.com>
2024-07-25 22:00:15 +1000

    Update TypeConverter.cs

--- src/Verify/Splitters/TypeConverter.cs
+++ src/Verify/Splitters/TypeConverter.cs
@@ -1,13 +1,7 @@
-﻿class TypeConverter
+﻿class TypeConverter(
+    AsyncConversion conversion,
+    CanConvert canConvert)
 {
-    public AsyncConversion Conversion { get; }
-    public CanConvert CanConvert { get; }
-
-    public TypeConverter(
-        AsyncConversion conversion,
-        CanConvert canConvert)
-    {
-        Conversion = conversion;
-        CanConvert = canConvert;
-    }
+    public AsyncConversion Conversion { get; } = conversion;
+    public CanConvert CanConvert { get; } = canConvert;
 }

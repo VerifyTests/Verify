aebefe7a18136717e1f2c2d7b8ccfa47a89fb35d
Simon Cropp <simon.cropp@gmail.com>
2024-07-28 22:57:04 +1000

    Argon 0.19.0

--- src/Verify.Expecto.Tests/Tests.cs
+++ src/Verify.Expecto.Tests/Tests.cs
@@ -3,18 +3,16 @@
 public class Tests
 {
     // ReSharper disable once UnusedMember.Local
-    static void DerivePathInfo()
-    {
-        #region DerivePathInfoExpecto
+    static void DerivePathInfo() =>
+    #region DerivePathInfoExpecto
 
         Verifier.DerivePathInfo(
             (sourceFile, projectDirectory, type, method) => new(
                 directory: Path.Combine(projectDirectory, "Snapshots"),
                 typeName: type,
                 methodName: method));
+    #endregion
 
-        #endregion
-    }
 
     [Tests]
     public static Test myTest = Runner.TestCase(

8a89dbd47272ffd7d78167cbeaa35b2218b3e156
Simon Cropp <simon.cropp@gmail.com>
2024-07-28 10:17:43 +1000

    docs

--- src/Verify.MSTest.SourceGenerator.Tests/CodeAnalysisVersionTests.cs
+++ src/Verify.MSTest.SourceGenerator.Tests/CodeAnalysisVersionTests.cs
@@ -3,6 +3,7 @@ using CSharpExtensions = Microsoft.CodeAnalysis.CSharp.CSharpExtensions;
 public class CodeAnalysisVersionTests(ITestOutputHelper output) : TestBase(output)
 {
     // prevent a CS9057 https://github.com/VerifyTests/Verify/issues/1255
+    // version compat: https://learn.microsoft.com/en-us/visualstudio/extensibility/roslyn-version-support?view=vs-2022
     [Fact]
     public void AssertVersion()
     {

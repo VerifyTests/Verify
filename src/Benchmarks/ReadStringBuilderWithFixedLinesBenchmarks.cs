816639ebb4d11044a6b48dcdc15e3afc49f3744e
Simon Cropp <simon.cropp@gmail.com>
2024-08-19 18:31:39 +1000

    Create ReadStringBuilderWithFixedLinesBenchmarks.cs

---
+++ src/Benchmarks/ReadStringBuilderWithFixedLinesBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿[MemoryDiagnoser(false)]
+public class ReadStringBuilderWithFixedLinesBenchmarks
+{
+    readonly IEnumerable<string> files;
+
+    public ReadStringBuilderWithFixedLinesBenchmarks()
+    {
+        var solutionDirectory = SolutionDirectoryFinder.Find(Environment.CurrentDirectory);;
+        files = Directory.EnumerateFiles(
+                solutionDirectory,
+                "*.verified.txt",
+                SearchOption.AllDirectories)
+            .ToList();
+    }
+
+    [Benchmark]
+    public async Task ReadStringBuilderWithFixedLines()
+    {
+        foreach (var file in files)
+        {
+            using var reader = IoHelpers.OpenRead(file);
+            await IoHelpers.ReadStringBuilderWithFixedLines(reader);
+        }
+    }
+}

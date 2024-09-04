84ea2df151376d59adeb8929b2f34533ac9b2d42
Simon Cropp <simon.cropp@gmail.com>
2024-09-04 21:59:43 +1000

    .

--- src/Verify.NUnit/Verifier.cs
+++ src/Verify.NUnit/Verifier.cs
@@ -37,7 +37,7 @@ public static partial class Verifier
         IReadOnlyList<string>? parameterNames;
         if (settings.HasParameters)
         {
-            parameterNames = GetParameterNames(adapter);
+            parameterNames = adapter.GetParameterNames();
         }
         else
         {
@@ -75,7 +75,7 @@ public static partial class Verifier
             return (methodParameterNames, adapter.Arguments);
         }
 
-        var names = GetConstructorParameterNames(method.TypeInfo.Type, argumentsLength);
+        var names = method.TypeInfo.Type.GetConstructorParameterNames(argumentsLength);
         if (methodParameterNames == null)
         {
             return (names.ToList(), parent.Arguments);
@@ -84,53 +84,6 @@ public static partial class Verifier
         return (
             [.. names, .. methodParameterNames],
             [.. parent.Arguments, .. adapter.Arguments]);
-    }
-
-    static IReadOnlyList<string>? GetParameterNames(TestAdapter adapter)
-    {
-        var method = adapter.Method!;
-
-        var methodParameterNames = method.MethodInfo.ParameterNames();
-
-        if (!adapter.TryGetParent(out var parent))
-        {
-            return methodParameterNames;
-        }
-
-        var names = GetConstructorParameterNames(method.TypeInfo.Type, parent.Arguments.Length);
-        if (methodParameterNames == null)
-        {
-            return names.ToList();
-        }
-
-        return [.. names, .. methodParameterNames];
-    }
-
-    static IEnumerable<string> GetConstructorParameterNames(Type type, int argumentsLength)
-    {
-        IEnumerable<string>? names = null;
-        foreach (var constructor in type.GetConstructors(BindingFlags.Instance | BindingFlags.Public))
-        {
-            var parameters = constructor.GetParameters();
-            if (parameters.Length != argumentsLength)
-            {
-                continue;
-            }
-
-            if (names != null)
-            {
-                throw new($"Found multiple constructors with {argumentsLength} parameters. Unable to derive names of parameters. Instead use UseParameters to pass in explicit parameter.");
-            }
-
-            names = parameters.Select(_ => _.Name!);
-        }
-
-        if (names == null)
-        {
-            throw new($"Could not find constructor with {argumentsLength} parameters.");
-        }
-
-        return names;
     }
 
     static SettingsTask Verify(

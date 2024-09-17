9db285799a866d72f07cb2829e16cc6e40add38a
Simon Cropp <simon.cropp@gmail.com>
2024-09-17 21:48:42 +1000

    Update TestFixtureSourceAndTestCaseUsage.cs

--- src/Verify.TUnit.Tests/TestFixtureSourceAndTestCaseUsage.cs
+++ src/Verify.TUnit.Tests/TestFixtureSourceAndTestCaseUsage.cs
@@ -1,24 +1,17 @@
-//TODO:
-// [Arguments(nameof(FixtureArgs))]
-// public class TestFixtureSourceAndTestCaseUsage(string arg1, int arg2)
-// {
-//     [Test]
-//     [Arguments("FromTestCase2")]
-//     public Task Test(string arg3) =>
-//         Verify(
-//             new
-//             {
-//                 arg1,
-//                 arg2,
-//                 arg3
-//             });
-//
-//     static object[] FixtureArgs =
-//     [
-//         new object[]
-//         {
-//             "Value2",
-//             2
-//         }
-//     ];
-// }
+[MethodDataSource(nameof(FixtureArgs), UnfoldTuple = true)]
+public class TestFixtureSourceAndTestCaseUsage(string arg1, int arg2)
+{
+    [Test]
+    [Arguments("FromTestCase2")]
+    public Task Test(string arg3) =>
+        Verify(
+            new
+            {
+                arg1,
+                arg2,
+                arg3
+            });
+
+    public static (string, int) FixtureArgs() =>
+        ("Value", 2);
+}

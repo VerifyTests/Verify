0e826dc2cc6f759a120e9a4d5a4b7ec6deb2a5d4
Simon Cropp <simon.cropp@gmail.com>
2024-10-21 22:12:17 +1100

    .

--- src/Verify.Xunit.Tests/VerifyCombinationsSample.cs
+++ src/Verify.Xunit.Tests/VerifyCombinationsSample.cs
@@ -8,4 +8,14 @@
             _ => _.ToLower(),
             list);
     }
+
+    [Fact]
+    public Task Two()
+    {
+        List<string> a = ["A", "b", "C"];
+        List<int> b = [1, 2, 3];
+        return VerifyCombinations(
+            (a, b) => a.ToLower() + b,
+            a, b);
+    }
 }
--- src/Verify.Xunit/Verifier_Combination.cs
+++ src/Verify.Xunit/Verifier_Combination.cs
@@ -1,19 +1,35 @@
-﻿using StringBuilder = System.Text.StringBuilder;
-
-namespace VerifyXunit;
+﻿namespace VerifyXunit;
 
 public static partial class Verifier
 {
     [Pure]
     public static SettingsTask VerifyCombinations<A>(
         Func<A, object> processCall,
-        IEnumerable<A> list,
+        IEnumerable<A> a,
         VerifySettings? settings = null,
         [CallerFilePath] string sourceFile = "")
     {
         var target = GetCombinationString(
             _ => processCall.DynamicInvoke(_.ToArray()),
-            null, [list.Cast<object?>().ToList()]);
+            null, [a.Cast<object?>().ToList()]);
+        return Verify(settings, sourceFile, _ => _.Verify(target));
+    }
+
+    [Pure]
+    public static SettingsTask VerifyCombinations<A,B>(
+        Func<A, B, object> processCall,
+        IEnumerable<A> a,
+        IEnumerable<B> b,
+        VerifySettings? settings = null,
+        [CallerFilePath] string sourceFile = "")
+    {
+        var target = GetCombinationString(
+            _ => processCall.DynamicInvoke(_.ToArray()),
+            null,
+            [
+                a.Cast<object?>().ToList(),
+                b.Cast<object?>().ToList()
+            ]);
         return Verify(settings, sourceFile, _ => _.Verify(target));
     }
 
@@ -24,10 +40,7 @@ public static partial class Verifier
     {
         var builder = new StringBuilder();
         var result = new List<object?>();
-        GenerateCombinations(
-            lists,
-            0,
-            result,
+        var combinationGenerator = new CombinationGenerator(lists,
             combo =>
             {
                 builder.Append('[');
@@ -70,22 +83,47 @@ public static partial class Verifier
 
                 builder.AppendLineN(resultFormatter(result));
             });
+        combinationGenerator.Run();
         return builder;
     }
+}
 
-    static void GenerateCombinations(List<List<object?>> lists, int depth, List<object?> currentCombination, Action<List<object?>> processCombination)
+class CombinationGenerator
+{
+    readonly List<List<object?>> lists;
+    readonly Action<List<object?>> action;
+
+    public CombinationGenerator(List<List<object?>> lists, Action<List<object?>> action)
     {
-        if (depth == lists.Count)
+        this.lists = lists;
+        this.action = action;
+    }
+    public void Run()
+    {
+        var indices = new int[lists.Count];
+
+        while (true)
         {
-            processCombination(currentCombination);
-            return;
+            var combination = new List<object?>();
+            for (var i = 0; i < lists.Count; i++)
+            {
+                combination.Add(lists[i][indices[i]]);
+            }
+
+            action(combination);
+
+            var incrementIndex = lists.Count - 1;
+            while (incrementIndex >= 0 && ++indices[incrementIndex] >= lists[incrementIndex].Count)
+            {
+                indices[incrementIndex] = 0;
+                incrementIndex--;
+            }
+
+            if (incrementIndex < 0)
+            {
+                break;
+            }
         }
 
-        foreach (var item in lists[depth])
-        {
-            currentCombination.Add(item);
-            GenerateCombinations(lists, depth + 1, currentCombination, processCombination);
-            currentCombination.RemoveAt(currentCombination.Count - 1);
-        }
     }
 }

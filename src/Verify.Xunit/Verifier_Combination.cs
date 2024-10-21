656bbd49ff4a699cf1ffda79ee59c35b62bad64e
Simon Cropp <simon.cropp@gmail.com>
2024-10-21 22:49:55 +1100

    .

--- src/Verify.Xunit/Verifier_Combination.cs
+++ src/Verify.Xunit/Verifier_Combination.cs
@@ -109,36 +109,3 @@ public static partial class Verifier
         return builder;
     }
 }
-
-class CombinationGenerator(List<List<object?>> lists, Action<object?[]> action)
-{
-    object?[] parameters = new object[lists.Count];
-
-    public void Run()
-    {
-        var indices = new int[lists.Count];
-
-        while (true)
-        {
-            for (var i = 0; i < lists.Count; i++)
-            {
-                var list = lists[i];
-                parameters[i] = list[indices[i]];
-            }
-
-            action(parameters);
-
-            var incrementIndex = lists.Count - 1;
-            while (incrementIndex >= 0 && ++indices[incrementIndex] >= lists[incrementIndex].Count)
-            {
-                indices[incrementIndex] = 0;
-                incrementIndex--;
-            }
-
-            if (incrementIndex < 0)
-            {
-                break;
-            }
-        }
-    }
-}
---
+++ src/Verify/Combinations/CombinationGenerator.cs
@@ -0,0 +1,33 @@
+﻿namespace VerifyXunit;
+
+class CombinationGenerator(List<List<object?>> lists, Action<object?[]> action)
+{
+    object?[] parameters = new object[lists.Count];
+    int[] indices = new int[lists.Count];
+
+    public void Run()
+    {
+        while (true)
+        {
+            for (var i = 0; i < lists.Count; i++)
+            {
+                var list = lists[i];
+                parameters[i] = list[indices[i]];
+            }
+
+            action(parameters);
+
+            var incrementIndex = lists.Count - 1;
+            while (incrementIndex >= 0 && ++indices[incrementIndex] >= lists[incrementIndex].Count)
+            {
+                indices[incrementIndex] = 0;
+                incrementIndex--;
+            }
+
+            if (incrementIndex < 0)
+            {
+                break;
+            }
+        }
+    }
+}

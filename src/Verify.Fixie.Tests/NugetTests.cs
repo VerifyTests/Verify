d116aa0d5c0959328be0502f8565e9d406ee87e9
Simon Cropp <simon.cropp@gmail.com>
2024-10-23 13:27:12 +1100

    comment out nuget tests for now

--- src/Verify.Fixie.Tests/NugetTests.cs
+++ src/Verify.Fixie.Tests/NugetTests.cs
@@ -1,35 +1,35 @@
-#if RELEASE && NET9_0
-
-public class NugetTests
-{
-    public async Task Run()
-    {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            return;
-        }
-
-        var version = GetType().Assembly
-            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
-            .InformationalVersion.Split('+')
-            .First();
-        var nugetPath = Path.Combine(
-            AttributeReader.GetSolutionDirectory(),
-            $"../nugets/Verify.Fixie.{version}.nupkg");
-        await VerifyZip(
-                nugetPath,
-                include: _ =>
-                {
-                    var extension = Path.GetExtension(_.Name);
-                    return !extension.Contains(".psmdc") &&
-                           !extension.Contains(".xml") &&
-                           !extension.Contains(".dll") &&
-                           !extension.Contains(".rels");
-                },
-                includeStructure: true)
-            .ScrubLinesContaining("psmdcp", "repository")
-            .ScrubLinesWithReplace(_ => _.Replace(version, "version"));
-    }
-}
-
-#endif
+// #if RELEASE && NET9_0
+//
+// public class NugetTests
+// {
+//     public async Task Run()
+//     {
+//         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+//         {
+//             return;
+//         }
+//
+//         var version = GetType().Assembly
+//             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
+//             .InformationalVersion.Split('+')
+//             .First();
+//         var nugetPath = Path.Combine(
+//             AttributeReader.GetSolutionDirectory(),
+//             $"../nugets/Verify.Fixie.{version}.nupkg");
+//         await VerifyZip(
+//                 nugetPath,
+//                 include: _ =>
+//                 {
+//                     var extension = Path.GetExtension(_.Name);
+//                     return !extension.Contains(".psmdc") &&
+//                            !extension.Contains(".xml") &&
+//                            !extension.Contains(".dll") &&
+//                            !extension.Contains(".rels");
+//                 },
+//                 includeStructure: true)
+//             .ScrubLinesContaining("psmdcp", "repository")
+//             .ScrubLinesWithReplace(_ => _.Replace(version, "version"));
+//     }
+// }
+//
+// #endif
--- src/Verify.Tests/NugetTests.cs
+++ src/Verify.Tests/NugetTests.cs
@@ -1,36 +1,36 @@
-#if RELEASE && NET9_0
-
-public class NugetTests
-{
-    [Fact]
-    public async Task Run()
-    {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            return;
-        }
-
-        var version = GetType().Assembly
-            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
-            .InformationalVersion.Split('+')
-            .First();
-        var nugetPath = Path.Combine(
-            AttributeReader.GetSolutionDirectory(),
-            $"../nugets/Verify.{version}.nupkg");
-        await VerifyZip(
-                nugetPath,
-                include: _ =>
-                {
-                    var extension = Path.GetExtension(_.Name);
-                    return !extension.Contains(".psmdc") &&
-                           !extension.Contains(".xml") &&
-                           !extension.Contains(".dll") &&
-                           !extension.Contains(".rels");
-                },
-                includeStructure: true)
-            .ScrubLinesContaining("psmdcp", "repository")
-            .ScrubLinesWithReplace(_ => _.Replace(version, "version"));
-    }
-}
-
-#endif
+// #if RELEASE && NET9_0
+//
+// public class NugetTests
+// {
+//     [Fact]
+//     public async Task Run()
+//     {
+//         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+//         {
+//             return;
+//         }
+//
+//         var version = GetType().Assembly
+//             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
+//             .InformationalVersion.Split('+')
+//             .First();
+//         var nugetPath = Path.Combine(
+//             AttributeReader.GetSolutionDirectory(),
+//             $"../nugets/Verify.{version}.nupkg");
+//         await VerifyZip(
+//                 nugetPath,
+//                 include: _ =>
+//                 {
+//                     var extension = Path.GetExtension(_.Name);
+//                     return !extension.Contains(".psmdc") &&
+//                            !extension.Contains(".xml") &&
+//                            !extension.Contains(".dll") &&
+//                            !extension.Contains(".rels");
+//                 },
+//                 includeStructure: true)
+//             .ScrubLinesContaining("psmdcp", "repository")
+//             .ScrubLinesWithReplace(_ => _.Replace(version, "version"));
+//     }
+// }
+//
+// #endif

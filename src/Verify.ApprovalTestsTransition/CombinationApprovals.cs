21b0c71d82478523c1ee8d0b71b50f813765884c
Simon Cropp <simon.cropp@gmail.com>
2024-10-25 21:53:16 +1100

    obsolete CombinationApprovals

---
+++ src/Verify.ApprovalTestsTransition/CombinationApprovals.cs
@@ -0,0 +1,4 @@
+﻿namespace ApprovalTests.Combinations;
+
+[Obsolete("Use Verifier.VerifyCombinations", true)]
+public static class CombinationApprovals;
--- src/Verify.Fixie/GlobalUsings.cs
+++ src/Verify.Fixie/GlobalUsings.cs
@@ -2,6 +2,5 @@
 global using System.IO.Compression;
 global using VerifyTests;
 global using Polyfills;
-global using EmptyFiles;
 global using Fixie;
 global using CharSpan = System.ReadOnlySpan<char>;

be126c56ea794eb236d32173d65e8b648a48e355
Simon Cropp <simon.cropp@gmail.com>
2024-10-29 21:46:44 +1100

    Update VerifyCombinationsSample.cs

--- src/Verify.Tests/VerifyCombinationsSample.cs
+++ src/Verify.Tests/VerifyCombinationsSample.cs
@@ -2,6 +2,52 @@
 #if NET8_0_OR_GREATER
 public class VerifyCombinationsSample
 {
+    public static async Task<string> BuildAddressTask(int streetNumber, string street, string city)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(street);
+        ArgumentException.ThrowIfNullOrWhiteSpace(city);
+        ArgumentOutOfRangeException.ThrowIfLessThan(streetNumber, 1);
+        await Task.Delay(1);
+        return $"{streetNumber} {street}, {city}";
+    }
+
+    [Fact]
+    public Task BuildAddressTaskTest()
+    {
+        int[] streetNumbers = [1, 10];
+        string[] streets = ["Smith St", "Wallace St"];
+        string[] cities = ["Sydney", "Chicago"];
+        return Combination()
+            .Verify(
+                BuildAddressTask,
+                streetNumbers,
+                streets,
+                cities);
+    }
+
+    public static async ValueTask<string> BuildAddressValueTask(int streetNumber, string street, string city)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(street);
+        ArgumentException.ThrowIfNullOrWhiteSpace(city);
+        ArgumentOutOfRangeException.ThrowIfLessThan(streetNumber, 1);
+        await Task.Delay(1);
+        return $"{streetNumber} {street}, {city}";
+    }
+
+    [Fact]
+    public Task BuildAddressValueTaskTest()
+    {
+        int[] streetNumbers = [1, 10];
+        string[] streets = ["Smith St", "Wallace St"];
+        string[] cities = ["Sydney", "Chicago"];
+        return Combination()
+            .Verify(
+                BuildAddressValueTask,
+                streetNumbers,
+                streets,
+                cities);
+    }
+
     #region CombinationTargetMethod
 
     public static string BuildAddress(int streetNumber, string street, string city)
@@ -59,11 +105,13 @@ public class VerifyCombinationsSample
         int[] streetNumbers = [-1, 0, 10];
         string[] streets = ["", " ", "Valid St"];
         string[] cities = [null!, "Valid City"];
-        return Combination(captureExceptions: true).Verify(
-            BuildAddress,
-            streetNumbers,
-            streets,
-            cities);
+        return Combination(captureExceptions: true)
+            .Verify(
+                BuildAddress,
+                streetNumbers,
+                streets,
+                cities
+            );
     }
 
     #endregion

8400f921a003f049f3a92e6c969a82790eb98ba9
Simon Cropp <simon.cropp@gmail.com>
2024-10-30 08:40:56 +1100

    Update CombinationRunner.cs

--- src/Verify/Combinations/CombinationRunner.cs
+++ src/Verify/Combinations/CombinationRunner.cs
@@ -2,21 +2,21 @@
 {
     Type[] keyTypes;
     int[] indices;
-    List<List<object?>> lists;
+    object?[][] lists;
     bool captureExceptions;
 
     public CombinationRunner(bool? captureExceptions, List<IEnumerable<object?>> lists, Type[] keyTypes)
     {
         this.keyTypes = keyTypes;
         this.captureExceptions = captureExceptions ?? VerifyCombinationSettings.CaptureExceptionsEnabled;
-        this.lists = lists.Select(_ => _.ToList()).ToList();
+        this.lists = lists.Select(_ => _.ToArray()).ToArray();
         indices = new int[lists.Count];
     }
 
     object?[] BuildParameters()
     {
-        var parameters = new object?[lists.Count];
-        for (var i = 0; i < lists.Count; i++)
+        var parameters = new object?[lists.Length];
+        for (var i = 0; i < lists.Length; i++)
         {
             var list = lists[i];
             parameters[i] = list[indices[i]];
@@ -27,9 +27,9 @@
 
     bool Increment()
     {
-        var incrementIndex = lists.Count - 1;
+        var incrementIndex = lists.Length - 1;
         while (incrementIndex >= 0 &&
-               ++indices[incrementIndex] >= lists[incrementIndex].Count)
+               ++indices[incrementIndex] >= lists[incrementIndex].Length)
         {
             indices[incrementIndex] = 0;
             incrementIndex--;

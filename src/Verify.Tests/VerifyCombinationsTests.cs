efd1c139050547b779a8355f231dc56ec34df448
Simon Cropp <simon.cropp@gmail.com>
2024-10-29 22:37:34 +1100

    .

--- src/Verify.Tests/VerifyCombinationsTests.cs
+++ src/Verify.Tests/VerifyCombinationsTests.cs
@@ -2,73 +2,64 @@
 #if NET8_0_OR_GREATER
 public class VerifyCombinationsTests
 {
-    public static async Task<string> TaskMethod(int streetNumber, string street, string city)
+    public static async Task<string> TaskMethod(int param1, string param2)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(street);
-        ArgumentException.ThrowIfNullOrWhiteSpace(city);
-        ArgumentOutOfRangeException.ThrowIfLessThan(streetNumber, 1);
+        ArgumentException.ThrowIfNullOrWhiteSpace(param2);
+        ArgumentOutOfRangeException.ThrowIfLessThan(param1, 1);
         await Task.Delay(1);
-        return $"{streetNumber} {street}, {city}";
+        return $"{param1} {param2}";
     }
 
     [Fact]
     public Task TaskTest()
     {
-        int[] streetNumbers = [1, 10];
-        string[] streets = ["Smith St", "Wallace St"];
-        string[] cities = ["Sydney", "Chicago"];
+        int[] params1 = [1, 10];
+        string[] params2 = ["Smith St", "Wallace St"];
         return Combination()
             .Verify(
                 TaskMethod,
-                streetNumbers,
-                streets,
-                cities);
+                params1,
+                params2);
     }
 
-    public static async ValueTask<string> ValueTaskMethod(int streetNumber, string street, string city)
+    public static async ValueTask<string> ValueTaskMethod(int param1, string param2)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(street);
-        ArgumentException.ThrowIfNullOrWhiteSpace(city);
-        ArgumentOutOfRangeException.ThrowIfLessThan(streetNumber, 1);
+        ArgumentException.ThrowIfNullOrWhiteSpace(param2);
+        ArgumentOutOfRangeException.ThrowIfLessThan(param1, 1);
         await Task.Delay(1);
-        return $"{streetNumber} {street}, {city}";
+        return $"{param1} {param2}";
     }
 
     [Fact]
     public Task ValueTaskTest()
     {
-        int[] streetNumbers = [1, 10];
-        string[] streets = ["Smith St", "Wallace St"];
-        string[] cities = ["Sydney", "Chicago"];
+        int[] param1 = [1, 10];
+        string[] param2 = ["Smith St", "Wallace St"];
         return Combination()
             .Verify(
                 ValueTaskMethod,
-                streetNumbers,
-                streets,
-                cities);
+                param1,
+                param2);
     }
 
-    public static string SimpleReturnMethod(int streetNumber, string street, string city)
+    public static string SimpleReturnMethod(int param1, string param2)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(street);
-        ArgumentException.ThrowIfNullOrWhiteSpace(city);
-        ArgumentOutOfRangeException.ThrowIfLessThan(streetNumber, 1);
+        ArgumentException.ThrowIfNullOrWhiteSpace(param2);
+        ArgumentOutOfRangeException.ThrowIfLessThan(param1, 1);
 
-        return $"{streetNumber} {street}, {city}";
+        return $"{param1} {param2}";
     }
 
     [Fact]
     public Task SimpleReturnTest()
     {
-        int[] streetNumbers = [1, 10];
-        string[] streets = ["Smith St", "Wallace St"];
-        string[] cities = ["Sydney", "Chicago"];
+        int[] param1 = [1, 10];
+        string[] param2 = ["Smith St", "Wallace St"];
         return Combination()
             .Verify(
                 SimpleReturnMethod,
-                streetNumbers,
-                streets,
-                cities);
+                param1,
+                param2);
     }
 }
 #endif

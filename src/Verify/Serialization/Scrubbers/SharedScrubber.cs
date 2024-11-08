d86bbbccb07ff4b30ba17565b005b9ec2da8bfb1
Simon Cropp <simon.cropp@gmail.com>
2024-11-08 15:03:11 +1100

    cleanup

--- src/Verify/Serialization/Scrubbers/SharedScrubber.cs
+++ src/Verify/Serialization/Scrubbers/SharedScrubber.cs
@@ -1,5 +1,26 @@
 ﻿partial class SerializationSettings
 {
+    internal bool TryParseConvert(Counter counter, CharSpan value, [NotNullWhen(true)] out string? result)
+    {
+        if (TryParseConvertGuid(counter, value, out result) ||
+            TryParseConvertDateTimeOffset(counter, value, out result) ||
+            TryParseConvertDateTime(counter, value, out result))
+        {
+            return true;
+        }
+
+#if NET6_0_OR_GREATER
+        if (TryParseConvertDate(counter, value, out result) ||
+            TryParseConvertTime(counter, value, out result))
+        {
+            return true;
+        }
+#endif
+
+        result = null;
+        return false;
+    }
+
     internal bool TryConvertString(Counter counter, CharSpan value, [NotNullWhen(true)] out string? result)
     {
         if (TryParseConvertGuid(counter, value, out result) ||
--- src/Verify/Serialization/Scrubbers/SharedScrubber_Guids.cs
+++ src/Verify/Serialization/Scrubbers/SharedScrubber_Guids.cs
@@ -36,26 +36,4 @@
         result = null;
         return false;
     }
-
-    internal bool TryParseConvert(Counter counter, CharSpan value, [NotNullWhen(true)] out string? result)
-    {
-        if (TryParseConvertGuid(counter, value, out result) ||
-            TryParseConvertDateTimeOffset(counter, value, out result) ||
-            TryParseConvertDateTime(counter, value, out result))
-        {
-            return true;
-        }
-
-#if NET6_0_OR_GREATER
-
-        if (TryParseConvertDate(counter, value, out result) ||
-            TryParseConvertTime(counter, value, out result))
-        {
-            return true;
-        }
-#endif
-
-        result = null;
-        return false;
-    }
 }

2e83808b1ee943be8648e170985509e608e47688
Simon Cropp <simon.cropp@gmail.com>
2024-11-10 19:44:12 +1100

    split CustomContractResolver

---
+++ src/Verify/Serialization/CustomContractResolver_Dictionary.cs
@@ -0,0 +1,114 @@
+﻿partial class CustomContractResolver
+{
+    protected override JsonDictionaryContract CreateDictionaryContract(Type objectType)
+    {
+        var contract = base.CreateDictionaryContract(objectType);
+        contract.DictionaryKeyResolver = (_, name, original) => ResolveDictionaryKey(contract, name, original);
+        if (settings.SortDictionaries)
+        {
+            contract.OrderByKey = true;
+        }
+
+        contract.InterceptSerializeItem = HandleDictionaryItem;
+
+        return contract;
+    }
+
+    KeyValueInterceptResult HandleDictionaryItem(JsonWriter writer, object key, object? value)
+    {
+        if (key is string stringKey &&
+            settings.TryGetScrubOrIgnoreByName(stringKey, out var scrubOrIgnore))
+        {
+            return ToInterceptKeyValueResult(scrubOrIgnore.Value);
+        }
+
+        if (value is not null &&
+            settings.TryGetScrubOrIgnoreByInstance(value, out scrubOrIgnore))
+        {
+            return ToInterceptKeyValueResult(scrubOrIgnore.Value);
+        }
+
+        return KeyValueInterceptResult.Default;
+    }
+
+    static KeyValueInterceptResult ToInterceptKeyValueResult(ScrubOrIgnore scrubOrIgnore)
+    {
+        if (scrubOrIgnore == ScrubOrIgnore.Ignore)
+        {
+            return KeyValueInterceptResult.Ignore;
+        }
+
+        return KeyValueInterceptResult.ReplaceValue("{Scrubbed}");
+    }
+
+    string ResolveDictionaryKey(JsonDictionaryContract contract, string name, object original)
+    {
+        var counter = Counter.Current;
+        var keyType = contract.DictionaryKeyType;
+
+#if NET6_0_OR_GREATER
+
+        if (original is Date date)
+        {
+            if (settings.TryConvert(counter, date, out var result))
+            {
+                return result;
+            }
+        }
+
+        if (original is Time time)
+        {
+            if (settings.TryConvert(counter, time, out var result))
+            {
+                return result;
+            }
+        }
+
+#endif
+
+        if (original is Guid guid)
+        {
+            if (settings.TryConvert(counter, guid, out var result))
+            {
+                return result;
+            }
+        }
+
+        if (original is string stringValue)
+        {
+            if (settings.TryParseConvert(counter, stringValue.AsSpan(), out var result))
+            {
+                return result;
+            }
+        }
+
+        if (original is DateTime dateTime)
+        {
+            if (settings.TryConvert(counter, dateTime, out var result))
+            {
+                return result;
+            }
+        }
+
+        if (original is DateTimeOffset dateTimeOffset)
+        {
+            if (settings.TryConvert(counter, dateTimeOffset, out var result))
+            {
+                return result;
+            }
+        }
+
+        if (keyType == typeof(Type))
+        {
+            var type = Type.GetType(name);
+            if (type is null)
+            {
+                throw new($"Could not load type `{name}`.");
+            }
+
+            return type.SimpleName();
+        }
+
+        return name;
+    }
+}

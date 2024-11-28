4253b7bec3a046e91a4afba84ec476e38027fcf3
Simon Cropp <simon.cropp@gmail.com>
2024-11-28 17:53:15 +1100

    .

---
+++ src/Verify.TUnit.Tests/ClassConstructor/ClassConstructor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TUnit.Core.Interfaces;
+
+public class ClassConstructor : IClassConstructor
+{
+    static readonly IServiceProvider serviceProvider = CreateServiceProvider();
+
+    AsyncServiceScope scope;
+
+    public T Create<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(ClassConstructorMetadata classConstructorMetadata)
+        where T : class
+    {
+        scope = serviceProvider.CreateAsyncScope();
+        return ActivatorUtilities.GetServiceOrCreateInstance<T>(scope.ServiceProvider);
+    }
+
+    static IServiceProvider CreateServiceProvider() =>
+        new ServiceCollection()
+            .AddSingleton<string>("SingletonValue")
+            .BuildServiceProvider();
+}
---
+++ src/Verify.TUnit.Tests/ClassConstructor/ClassConstructorMixedWithArgumentTests.cs
@@ -0,0 +1,11 @@
+﻿[ClassConstructor<ClassConstructor>]
+[Arguments("2")]
+public class ClassConstructorMixedWithArgumentTests(string service)
+{
+    [Test]
+    public async Task Test()
+    {
+        await Assert.That(service).IsNotNull();
+        await Verify(service);
+    }
+}

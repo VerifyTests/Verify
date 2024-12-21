a376b3f45b0a2877db98ae290e2cfafe2b000f78
Mithilesh Zavar <mithileshz@hotmail.co.uk>
2024-12-21 12:58:51 +0000

    Add test and potential fix?

--- src/StaticSettingsTests/DateTimeScrubbingTests.cs
+++ src/StaticSettingsTests/DateTimeScrubbingTests.cs
@@ -1,4 +1,6 @@
-﻿public class DateTimeScrubbingTests :
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class DateTimeScrubbingTests :
     BaseTest
 {
     public DateTimeScrubbingTests() =>
@@ -9,5 +11,14 @@
     {
         var dateTime = DateTime.Now;
         return Verify($"a {dateTime:F} b");
+    }
+
+    [Fact]
+    public Task Test_Long()
+    {
+        var settings = new VerifySettings();
+        settings.ScrubInlineDateTimes("G");
+
+        return Verify("12/11/2024 10:36:43 AM", settings);
     }
 }

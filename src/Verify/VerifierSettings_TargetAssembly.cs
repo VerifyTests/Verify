76879085a264a3f60ba4873ca26a74a2094d0b12
Simon Cropp <simon.cropp@gmail.com>
2025-03-05 15:40:50 +1100

    Update VerifierSettings_TargetAssembly.cs

--- src/Verify/VerifierSettings_TargetAssembly.cs
+++ src/Verify/VerifierSettings_TargetAssembly.cs
@@ -20,6 +20,7 @@ public static partial class VerifierSettings
             {
                 throw new InvalidOperationException("Assembly must be set before calling this method.");
             }
+
             return assembly;
         }
     }

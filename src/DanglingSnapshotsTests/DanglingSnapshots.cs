4d5fc515d4f192b35f461651e11abb1a761a4e8f
Simon Cropp <simon.cropp@gmail.com>
2025-03-05 16:03:01 +1100

    .

--- src/DanglingSnapshotsTests/DanglingSnapshots.cs
+++ src/DanglingSnapshotsTests/DanglingSnapshots.cs
@@ -2,9 +2,6 @@
 static class DanglingSnapshots
 {
     [OneTimeTearDown]
-    public static void Run()
-    {
+    public static void Run() =>
         DanglingSnapshotsCheck.Run();
-        Assert.Fail("dsfsdfsdf");
-    }
 }

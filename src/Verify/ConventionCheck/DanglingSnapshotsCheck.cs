a34aad2a0002772b49a9f8d9efa2ddfb76e152e0
Simon Cropp <simon.cropp@gmail.com>
2025-03-05 14:44:37 +1100

    Update DanglingSnapshotsCheck.cs

--- src/Verify/ConventionCheck/DanglingSnapshotsCheck.cs
+++ src/Verify/ConventionCheck/DanglingSnapshotsCheck.cs
@@ -1,36 +1,22 @@
-﻿// Async method lacks 'await' operators and will run synchronously
-#pragma warning disable CS1998
+﻿#pragma warning disable CS1998
 
 namespace VerifyTests;
 
-[Experimental("InnerVerifyChecks")]
-public static class InnerVerifyChecks
+[Experimental("DanglingSnapshotsCheck")]
+public static class DanglingSnapshotsCheck
 {
-    static string projectDirectory;
-
-    public static Task Run(Assembly assembly)
-    {
-        projectDirectory = AttributeReader.GetProjectDirectory(assembly);
-        if (!AttributeReader.TryGetSolutionDirectory(assembly, out var solutionDirectory))
-        {
-            solutionDirectory = Directory.GetParent(projectDirectory)!.FullName;
-        }
-
-        return Run(solutionDirectory);
-    }
-
     static ConcurrentBag<string>? trackedVerifiedFiles;
 
     internal static void TrackVerifiedFile(string path) => trackedVerifiedFiles?.Add(path);
 
-    public static void Complete()
+    public static void Run(string projectDirectory)
     {
         if (!BuildServerDetector.Detected)
         {
             return;
         }
 
-        foreach (var file in GetVerifiedFiles(projectDirectory))
+        foreach (var file in Directory.EnumerateFiles(projectDirectory, "*.verified.*", SearchOption.AllDirectories))
         {
             if (!trackedVerifiedFiles!.Contains(file))
             {
@@ -46,215 +32,5 @@ public static class InnerVerifyChecks
         {
             trackedVerifiedFiles = [];
         }
-    }
-
-    internal static async Task Run(string directory)
-    {
-        AppDomain.CurrentDomain.ProcessExit += (sender, args) =>
-        {
-            throw new Exception("sdfsdf");
-        };
-        var extensions = GetExtensions(directory);
-        await CheckGitIgnore(directory);
-        await CheckIncorrectlyImportedSnapshots(directory);
-        if (extensions.Count == 0)
-        {
-            return;
-        }
-
-        await CheckEditorConfig(directory, extensions);
-        await CheckGitAttributes(directory, extensions);
-    }
-
-    internal static List<string> GetExtensions(string directory) =>
-        // ReSharper disable once RedundantSuppressNullableWarningExpression
-        GetVerifiedFiles(directory)
-            .Select(_ => Path.GetExtension(_)![1..])
-            .Distinct()
-            .Where(FileExtensions.IsTextExtension)
-            .OrderBy(_ => _)
-            .ToList();
-
-    static IEnumerable<string> GetVerifiedFiles(string directory) =>
-        Directory.EnumerateFiles(directory, "*.verified.*", SearchOption.AllDirectories);
-
-    internal static async Task CheckIncorrectlyImportedSnapshots(string solutionDirectory)
-    {
-        var builder = new StringBuilder();
-        foreach (var project in Directory.EnumerateFiles(solutionDirectory, "*.csproj", SearchOption.AllDirectories))
-        {
-            foreach (var line in await FilePolyfill.ReadAllLinesAsync(project))
-            {
-                //<None Update="SampleVbTest.RunResults#helloWorld.verified.cs">
-                if (!line.Contains("<None Update=\"") ||
-                    !line.Contains(".verified."))
-                {
-                    continue;
-                }
-
-                builder.AppendLine(
-                    $"""
-                     Project: {GetPath(project)}
-                     Line: {line.TrimStart()}
-                     """);
-            }
-        }
-
-        if (builder.Length == 0)
-        {
-            return;
-        }
-
-        builder.Insert(
-            0,
-            """
-            Found incorrectly imported verified file.
-            This occurs when a test file is copied in the IDE and the IDE incorrectly duplicates the dynamically imported verified file nestings.
-
-            """);
-        throw new VerifyCheckException(builder.ToString());
-    }
-
-    internal static async Task CheckEditorConfig(string solutionDirectory, List<string> extensions)
-    {
-        var path = Path.Combine(solutionDirectory, ".editorconfig");
-        if (!File.Exists(path))
-        {
-            return;
-        }
-
-        path = Path.GetFullPath(path);
-        var lines = await FilePolyfill.ReadAllLinesAsync(path);
-
-        if (HasAllExtensions(extensions, lines))
-        {
-            return;
-        }
-
-        throw new VerifyCheckException(
-            $"""
-             Expected .editorconfig to contain settings for Verify for all text files
-             Path: {GetPath(path)}
-             Recommended settings:
-
-             # Verify
-             {$"[*.{{received,verified}}.{{{StringPolyfill.Join(',', extensions)}}}]"}
-             charset = "utf-8-bom"
-             end_of_line = lf
-             indent_size = unset
-             indent_style = unset
-             insert_final_newline = false
-             tab_width = unset
-             trim_trailing_whitespace = false
-             """);
-    }
-
-    static bool HasAllExtensions(List<string> extensions, string[] lines)
-    {
-        var line = lines.SingleOrDefault(_ => _.StartsWith("[*.{received,verified}."));
-        if (line == null)
-        {
-            return false;
-        }
-
-        var suffix = line[24..^2].Split(',');
-        foreach (var extension in extensions)
-        {
-            if (!suffix.Contains(extension))
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    internal static async Task CheckGitAttributes(string solutionDirectory, List<string> extensions)
-    {
-        var path = Path.Combine(solutionDirectory, ".gitattributes");
-        if (!File.Exists(path))
-        {
-            path = Path.Combine(solutionDirectory, "../.gitattributes");
-        }
-
-        if (!File.Exists(path))
-        {
-            return;
-        }
-
-        path = Path.GetFullPath(path);
-        var text = await FilePolyfill.ReadAllLinesAsync(path);
-
-        List<string> missing = [];
-        List<string> expected = [];
-        foreach (var extension in extensions)
-        {
-            var line = $"*.verified.{extension} text eol=lf working-tree-encoding=UTF-8";
-            expected.Add(line);
-            if (text.Contains(line))
-            {
-                continue;
-            }
-
-            missing.Add(line);
-        }
-
-        if (missing.Count == 0)
-        {
-            return;
-        }
-
-        var builder = new StringBuilder(
-            $"""
-             Expected .gitattributes to contain settings for Verify.
-             Path: {GetPath(path)}
-             Recommended settings:
-
-             # Verify
-
-             """);
-        foreach (var line in expected)
-        {
-            builder.AppendLine(line);
-        }
-
-        throw new VerifyCheckException(builder.ToString());
-    }
-
-    static string GetPath(string path) =>
-        $"file:///{path.Replace('\\', '/')}";
-
-    internal static async Task CheckGitIgnore(string solutionDirectory)
-    {
-        var path = Path.Combine(solutionDirectory, ".gitIgnore");
-        if (!File.Exists(path))
-        {
-            path = Path.Combine(solutionDirectory, "../.gitIgnore");
-        }
-
-        if (!File.Exists(path))
-        {
-            return;
-        }
-
-        path = Path.GetFullPath(path);
-        var text = await FilePolyfill.ReadAllTextAsync(path);
-        if (text.Contains("*.received.*") ||
-            text.Contains("*.received/") ||
-            text.Contains("# Verify"))
-        {
-            return;
-        }
-
-        throw new VerifyCheckException(
-            $"""
-             Expected .gitIgnore to contain settings for Verify.
-             Path: {GetPath(path)}
-             Recommended settings:
-
-             # Verify
-             *.received.*
-             *.received/
-             """);
     }
 }

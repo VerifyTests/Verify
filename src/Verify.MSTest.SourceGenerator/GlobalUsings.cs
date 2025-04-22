714e90cfd2a61e16c75f52a4e8160ff561977543
Simon Cropp <simon.cropp@gmail.com>
2025-04-22 16:41:28 +1000

    Update GlobalUsings.cs

--- src/Verify.MSTest.SourceGenerator/GlobalUsings.cs
+++ src/Verify.MSTest.SourceGenerator/GlobalUsings.cs
@@ -1,5 +1,4 @@
 ﻿global using System.Collections.Immutable;
-global using System.ComponentModel;
 global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;

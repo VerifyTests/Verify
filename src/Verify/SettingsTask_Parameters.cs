e49c249d615abccf91e466c78c1dcb08044d1966
Simon Cropp <simon.cropp@gmail.com>
2025-05-08 22:07:15 +1000

    Update SettingsTask_Parameters.cs

--- src/Verify/SettingsTask_Parameters.cs
+++ src/Verify/SettingsTask_Parameters.cs
@@ -2,11 +2,7 @@ namespace VerifyTests;
 
 public partial class SettingsTask
 {
-    /// <summary>
-    /// Ignore parameters in 'verified' filename resulting in the same verified file for multiple testcases.
-    /// Note that UseParameters has still been called for test frameworks that don't support automatic parameter detection and the 'received' files still contains the parameters.
-    /// </summary>
-    /// <param name="parameterNames">The names of the parameters to be ignored. When passing an empty list all parameters will be ignored.</param>
+    /// <inheritdoc cref="VerifySettings.IgnoreParameters(string[])"/>
     [Pure]
     public SettingsTask IgnoreParameters(params string[] parameterNames)
     {
@@ -14,11 +10,7 @@ public partial class SettingsTask
         return this;
     }
 
-    /// <summary>
-    /// Ignore all parameters in 'verified' filename resulting in the same verified file for each testcase.
-    /// Note that the 'received' files still contain the parameters.
-    /// </summary>
-    /// <param name="parameters">The parameters as you would have passed them to the UseParameters function.</param>
+    /// <inheritdoc cref="VerifySettings.IgnoreParametersForVerified(object?[])"/>
     [Pure]
     public SettingsTask IgnoreParametersForVerified(params object?[] parameters)
     {
@@ -50,11 +42,7 @@ public partial class SettingsTask
         return this;
     }
 
-    /// <summary>
-    /// Use a custom text for the `Parameters` part of the file name.
-    /// Not compatible with <see cref="UseParameters" />.
-    /// Where the file format is `{CurrentDirectory}/{TestClassName}.{TestMethodName}_{Parameters}_{UniqueFor1}_{UniqueFor2}_{UniqueForX}.verified.{extension}`.
-    /// </summary>
+    /// <inheritdoc cref="VerifySettings.UseTextForParameters(string)"/>
     [Pure]
     public SettingsTask UseTextForParameters(string parametersText)
     {

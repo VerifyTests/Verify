06fd5c1b0de19c484f16c4dac6bc2f08e8cd35fa
Simon Cropp <simon.cropp@gmail.com>
2025-05-23 12:00:41 +1000

    refs or cleanup

--- src/Verify.TUnit.Tests/ClassConstructor/ClassConstructor.cs
+++ src/Verify.TUnit.Tests/ClassConstructor/ClassConstructor.cs
@@ -5,15 +5,14 @@
 
     AsyncServiceScope scope;
 
-    public T Create<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(ClassConstructorMetadata classConstructorMetadata)
-        where T : class
-    {
-        scope = serviceProvider.CreateAsyncScope();
-        return ActivatorUtilities.GetServiceOrCreateInstance<T>(scope.ServiceProvider);
-    }
-
     static ServiceProvider CreateServiceProvider() =>
         new ServiceCollection()
             .AddSingleton<string>("SingletonValue")
             .BuildServiceProvider();
+
+    public object Create([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] Type type, ClassConstructorMetadata classConstructorMetadata)
+    {
+        scope = serviceProvider.CreateAsyncScope();
+        return ActivatorUtilities.GetServiceOrCreateInstance(scope.ServiceProvider, type);
+    }
 }

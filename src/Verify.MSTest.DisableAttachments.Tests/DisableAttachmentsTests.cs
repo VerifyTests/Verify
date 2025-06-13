ee6e39302cf2748547f52afd7008ac9a29b79826
Simon Cropp <simon.cropp@gmail.com>
2025-06-13 21:12:11 +1000

    Update DisableAttachmentsTests.cs

--- src/Verify.MSTest.DisableAttachments.Tests/DisableAttachmentsTests.cs
+++ src/Verify.MSTest.DisableAttachments.Tests/DisableAttachmentsTests.cs
@@ -3,20 +3,13 @@ public class DisableAttachmentsTests : VerifyBase
 {
     [ResultFilesCallback]
     [TestMethod]
-    public async Task HasNoAttachments()
+    public Task HasNoAttachments()
     {
         ResultFilesCallback.Callback = list => Assert.AreEqual(0, list.Count);
         VerifierSettings.DisableAttachments();
         var settings = new VerifySettings();
         settings.DisableDiff();
-        await Assert.ThrowsExceptionAsync<VerifyException>(
+        return Assert.ThrowsExceptionAsync<VerifyException>(
             () => Verify("Bar", settings));
-    }
-
-    [TestCleanup]
-    public void ResetStaticSettings()
-    {
-        VerifierSettings.Reset();
-        CombinationSettings.Reset();
     }
 }

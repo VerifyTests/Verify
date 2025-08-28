192f8f5a1a8c274bffe47bd64d9c4843565702af
Simon Cropp <simon.cropp@gmail.com>
2025-08-28 22:31:02 +1000

    Create TestingConverters.cs

---
+++ src/Verify.Tests/Serialization/TestingConverters.cs
@@ -0,0 +1,47 @@
+#if NET9_0_OR_GREATER
+public class TestingConverters
+{
+    record Target(string? Property);
+
+    [Fact]
+    public Task Test()
+    {
+        var converter = new Converter();
+        var builder = new StringBuilder();
+
+        var settings = new VerifySettings();
+        Counter.Start(
+            dateCounting: false,
+            scrubDateTimes: false,
+            scrubGuids: false,
+            namedDates: [],
+            namedTimes: [],
+            namedDateTimes: [],
+            namedGuids: [],
+            namedDateTimeOffsets: []
+        );
+
+        var target = new Target("Value");
+
+        using (var writer = new VerifyJsonWriter(builder, settings, Counter.Current))
+        {
+            converter.Write(writer, target);
+        }
+
+        return Verify(builder);
+    }
+
+    class Converter :
+        WriteOnlyJsonConverter<Target>
+    {
+        public override void Write(VerifyJsonWriter writer, Target target)
+        {
+            writer.WriteStartObject();
+            writer.WriteMember(target, target.Property, "Property");
+            writer.WritePropertyName("Custom");
+            writer.Serialize(new Dictionary<string,string>(){{"d","D"}});
+            writer.WriteEnd();
+        }
+    }
+}
+#endif

a6b1a68dc8084c903fcf3417cbd5f505e24ed188
Simon Cropp <simon.cropp@gmail.com>
2025-09-13 19:44:51 +1000

    Update SerializationTests.cs

--- src/Verify.Tests/Serialization/SerializationTests.cs
+++ src/Verify.Tests/Serialization/SerializationTests.cs
@@ -750,33 +750,6 @@ public class SerializationTests
     }
 
     [Fact]
-    public Task CounterTryConvert()
-    {
-        var settings = new VerifySettings();
-        settings.AddScrubber((builder, counter) =>
-        {
-            var values = builder.ToString().Split();
-            builder.Clear();
-            foreach (var value in values)
-            {
-                var converted = counter.TryConvert(value, out var result);
-                if (converted)
-                {
-                    builder.Append(result);
-                }
-                else
-                {
-                    builder.Append(value);
-                }
-
-                builder.Append(' ');
-            }
-        });
-
-        return Verify("The user with id c9cb98bf-3def-415e-a009-7d58055f5ffc was created on 2022-10-12", settings);
-    }
-
-    [Fact]
     public Task NameValueCollection() =>
         Verify(
             new

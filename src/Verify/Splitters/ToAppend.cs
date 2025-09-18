119d133e7be30342a6d742bc40acbf947881effb
Simon Cropp <simon.cropp@gmail.com>
2025-09-18 14:29:38 +1000

    Update ToAppend.cs

--- src/Verify/Splitters/ToAppend.cs
+++ src/Verify/Splitters/ToAppend.cs
@@ -7,7 +7,7 @@ public readonly struct ToAppend
 
     public ToAppend(string name, object data)
     {
-        Guard.NotNull(name);
+        Guard.NotNullOrWhiteSpace(name);
         Name = name;
         Data = data;
     }

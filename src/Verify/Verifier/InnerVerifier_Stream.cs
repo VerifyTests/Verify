661670769dfcad60175a52f1a4de531c5f0ec1f5
Simon Cropp <simon.cropp@gmail.com>
2025-09-18 22:10:47 +1000

    .

--- src/Verify/Verifier/InnerVerifier_Stream.cs
+++ src/Verify/Verifier/InnerVerifier_Stream.cs
@@ -139,7 +139,8 @@ partial class InnerVerifier
         {
             var target = queue.Dequeue();
 
-            if (!VerifierSettings.TryGetStreamConverter(target.Extension, out var conversion))
+            if (extension == target.Extension ||
+                !VerifierSettings.TryGetStreamConverter(target.Extension, out var conversion))
             {
                 targets.Add(target);
                 continue;
@@ -151,6 +152,7 @@ partial class InnerVerifier
             {
                 cleanup += result.Cleanup;
             }
+
             if (result.Info != null)
             {
                 infos.Add(result.Info);

4f5322a02be1c2c0f48a4284913adba8daff57c6
Simon Cropp <simon.cropp@gmail.com>
2025-10-28 12:09:14 +1100

    Update TempDirectoryTests.cs

--- src/Verify.Tests/TempDirectoryTests.cs
+++ src/Verify.Tests/TempDirectoryTests.cs
@@ -49,7 +49,7 @@
         var filesViaPath = Directory.EnumerateFiles(path);
         Trace.WriteLine(filesViaPath.Count());
 
-        // implicit conversion to DirectoryInfo prop
+        // Info property retursn a DirectoryInfo for the directory
         var fileViaInfoProp = temp.Info.EnumerateFiles();
         Trace.WriteLine(fileViaInfoProp.Count());
 

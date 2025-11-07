26cf004299d7029c5875219af5f716d23317c086
Simon Cropp <simon.cropp@gmail.com>
2025-11-07 18:59:47 +1100

    .

--- src/Verify/Serialization/Scrubbers/DirectoryReplacements.cs
+++ src/Verify/Serialization/Scrubbers/DirectoryReplacements.cs
@@ -4,9 +4,6 @@ static class DirectoryReplacements
 {
     static List<KeyValuePair<string, string>> replacements = [];
 
-    static string ReplaceAltDirChar(this string directory) =>
-        directory.Replace(IoHelpers.DirectorySeparator, IoHelpers.AltDirectorySeparator);
-
     public static void Replace(StringBuilder builder) =>
         builder.ReplaceDirectoryPaths(replacements);
 
@@ -14,45 +11,27 @@ static class DirectoryReplacements
     {
         var values = new Dictionary<string, string>();
         var baseDir = CleanPath(AppDomain.CurrentDomain.BaseDirectory!);
-        var altBaseDir = baseDir.ReplaceAltDirChar();
-        values[baseDir + IoHelpers.DirectorySeparator] = "{CurrentDirectory}";
         values[baseDir] = "{CurrentDirectory}";
-        values[altBaseDir + IoHelpers.AltDirectorySeparator] = "{CurrentDirectory}";
-        values[altBaseDir] = "{CurrentDirectory}";
 
         var currentDir = CleanPath(Environment.CurrentDirectory);
-        var altCurrentDir = currentDir.ReplaceAltDirChar();
-        values[currentDir + IoHelpers.DirectorySeparator] = "{CurrentDirectory}";
         values[currentDir] = "{CurrentDirectory}";
-        values[altCurrentDir + IoHelpers.AltDirectorySeparator] = "{CurrentDirectory}";
-        values[altCurrentDir] = "{CurrentDirectory}";
 #if !NET6_0_OR_GREATER
         if (CodeBaseLocation.CurrentDirectory is not null)
         {
             var codeBaseLocation = CleanPath(CodeBaseLocation.CurrentDirectory);
-            var altCodeBaseLocation = codeBaseLocation.ReplaceAltDirChar();
-            values[codeBaseLocation + IoHelpers.DirectorySeparator] = "{CurrentDirectory}";
             values[codeBaseLocation] = "{CurrentDirectory}";
-            values[altCodeBaseLocation + IoHelpers.AltDirectorySeparator] = "{CurrentDirectory}";
-            values[altCodeBaseLocation] = "{CurrentDirectory}";
         }
 #endif
 
         var tempPath = CleanPath(Path.GetTempPath());
-        var altTempPath = tempPath.ReplaceAltDirChar();
-        values[tempPath + IoHelpers.DirectorySeparator] = "{TempPath}";
         values[tempPath] = "{TempPath}";
-        values[altTempPath + IoHelpers.AltDirectorySeparator] = "{TempPath}";
-        values[altTempPath] = "{TempPath}";
 
         if (VerifierSettings.scrubUserProfile)
         {
             var profileDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             if (!string.IsNullOrWhiteSpace(profileDir))
             {
-                var altProfileDir = profileDir.ReplaceAltDirChar();
                 values[profileDir] = "{UserProfile}";
-                values[altProfileDir] = "{UserProfile}";
             }
         }
 
@@ -70,40 +49,18 @@ static class DirectoryReplacements
             return;
         }
 
-        var altProjectDir = projectDir.ReplaceAltDirChar();
-        var altProjectDirTrimmed = altProjectDir.TrimEnd('/', '\\');
-        var projectDirTrimmed = projectDir.TrimEnd('/', '\\');
-
         if (!VerifierSettings.scrubSolutionDir ||
             solutionDir is null)
         {
             replacements[projectDir] = "{ProjectDirectory}";
-            replacements[projectDirTrimmed] = "{ProjectDirectory}";
-            replacements[altProjectDir] = "{ProjectDirectory}";
-            replacements[altProjectDirTrimmed] = "{ProjectDirectory}";
 
             return;
         }
 
-        var altSolutionDir = solutionDir.ReplaceAltDirChar();
-
         replacements[projectDir] = "{ProjectDirectory}";
-        replacements[projectDirTrimmed] = "{ProjectDirectory}";
-        replacements[altProjectDir] = "{ProjectDirectory}";
-        replacements[altProjectDirTrimmed] = "{ProjectDirectory}";
-
-        replacements[solutionDir] = "{SolutionDirectory}";
         if (solutionDir.Length > 1)
         {
-            var solutionDirectoryTrimmed = solutionDir.TrimEnd('/', '\\');
-            replacements[solutionDirectoryTrimmed] = "{SolutionDirectory}";
-        }
-
-        replacements[altSolutionDir] = "{SolutionDirectory}";
-        if (solutionDir.Length > 1)
-        {
-            var altSolutionDirTrimmed = altSolutionDir.TrimEnd('/', '\\');
-            replacements[altSolutionDirTrimmed] = "{SolutionDirectory}";
+            replacements[solutionDir] = "{SolutionDirectory}";
         }
     }
 
--- src/Verify/Serialization/Scrubbers/StringBuilderExtensions.cs
+++ src/Verify/Serialization/Scrubbers/StringBuilderExtensions.cs
@@ -4,7 +4,7 @@
         this StringBuilder builder,
         List<KeyValuePair<string, string>> paths)
     {
-        if (builder.Length == 0)
+        if (builder.Length == 0 || paths.Count == 0)
         {
             return;
         }
@@ -37,13 +37,11 @@
         foreach (var replacement in replacements)
         {
             var replacementEnd = replacement.Position + replacement.Length;
-            if (replacementEnd > lastEnd)
+            if (replacementEnd <= lastEnd)
             {
-                continue;
+                filtered.Add(replacement);
+                lastEnd = replacement.Position;
             }
-
-            filtered.Add(replacement);
-            lastEnd = replacement.Position;
         }
 
         // Apply replacements in reverse order
@@ -54,7 +52,7 @@
         }
     }
 
-    static void FindMatches(
+    private static void FindMatches(
         StringBuilder builder,
         string searchKey,
         string replaceValue,
@@ -71,7 +69,7 @@
             while (chunkOffset <= chunkSpan.Length - keySpan.Length)
             {
                 var remaining = chunkSpan[chunkOffset..];
-                var matchIndex = remaining.IndexOf(keySpan, StringComparison.Ordinal);
+                var matchIndex = FindPotentialMatch(remaining, keySpan);
 
                 if (matchIndex == -1)
                 {
@@ -84,9 +82,24 @@
                 if (IsCompleteMatch(builder, absolutePosition, keySpan) &&
                     HasValidBoundaries(builder, absolutePosition, keySpan.Length))
                 {
+                    // Check for trailing directory separator and include it greedily
+                    var matchLength = keySpan.Length;
+                    var endPosition = absolutePosition + matchLength;
+
+                    if (endPosition < builder.Length)
+                    {
+                        var nextChar = builder[endPosition];
+                        if (nextChar == '/' || nextChar == '\\')
+                        {
+                            matchLength++;
+                            // Characters following a directory separator are always valid
+                            // so no additional boundary check needed
+                        }
+                    }
+
                     replacements.Add(new Replacement(
                         absolutePosition,
-                        keySpan.Length,
+                        matchLength,
                         replaceValue));
                 }
 
@@ -97,10 +110,44 @@
         }
     }
 
-    static bool IsCompleteMatch(
+    private static int FindPotentialMatch(ReadOnlySpan<char> span, ReadOnlySpan<char> searchKey)
+    {
+        if (searchKey.Length == 0)
+        {
+            return -1;
+        }
+
+        var firstChar = searchKey[0];
+        var firstCharAlt = GetDirectorySeparatorAlternative(firstChar);
+
+        for (var i = 0; i <= span.Length - searchKey.Length; i++)
+        {
+            if (span[i] == firstChar || span[i] == firstCharAlt)
+            {
+                var matches = true;
+                for (var j = 1; j < searchKey.Length; j++)
+                {
+                    if (!AreCharsEqual(span[i + j], searchKey[j]))
+                    {
+                        matches = false;
+                        break;
+                    }
+                }
+
+                if (matches)
+                {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private static bool IsCompleteMatch(
         StringBuilder builder,
         int position,
-        CharSpan searchKey)
+        ReadOnlySpan<char> searchKey)
     {
         // Verify the match using direct indexing
         // (handles case where match might span chunk boundaries)
@@ -111,13 +158,44 @@
 
         for (var i = 0; i < searchKey.Length; i++)
         {
-            if (builder[position + i] != searchKey[i])
+            if (!AreCharsEqual(builder[position + i], searchKey[i]))
             {
                 return false;
             }
         }
 
         return true;
+    }
+
+    private static bool AreCharsEqual(char c1, char c2)
+    {
+        if (c1 == c2)
+        {
+            return true;
+        }
+
+        // Treat / and \ as equivalent
+        if ((c1 == '/' || c1 == '\\') && (c2 == '/' || c2 == '\\'))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static char GetDirectorySeparatorAlternative(char c)
+    {
+        if (c == '/')
+        {
+            return '\\';
+        }
+
+        if (c == '\\')
+        {
+            return '/';
+        }
+
+        return c;
     }
 
     private static bool HasValidBoundaries(
@@ -136,10 +214,20 @@
         }
 
         // Check trailing character - must not be letter or digit
+        // UNLESS it's a directory separator (which will be included greedily)
         var endPosition = position + length;
         if (endPosition < builder.Length)
         {
             var nextChar = builder[endPosition];
+
+            // Directory separators are valid boundaries (and will be included greedily)
+            // Any character following a separator is implicitly valid
+            if (nextChar is '/' or '\\')
+            {
+                return true;
+            }
+
+            // Otherwise, must not be alphanumeric
             if (char.IsLetterOrDigit(nextChar))
             {
                 return false;

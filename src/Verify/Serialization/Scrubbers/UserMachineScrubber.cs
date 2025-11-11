4a2b930dc3d42bc5c0a1a7e8a923ba745a42c2bb
Simon Cropp <simon.cropp@gmail.com>
2025-11-11 14:46:40 +1100

    fix chunk spanning user and machine names (#1628)

--- src/Verify/Serialization/Scrubbers/UserMachineScrubber.cs
+++ src/Verify/Serialization/Scrubbers/UserMachineScrubber.cs
@@ -1,4 +1,4 @@
-﻿static class UserMachineScrubber
+﻿static partial class UserMachineScrubber
 {
     static string machineName;
     static string userName;
@@ -13,73 +13,9 @@
         UserMachineScrubber.userName = userName;
     }
 
-    static bool IsValidWrapper(char ch) =>
-        ch is
-            ' ' or
-            '\t' or
-            '\n' or
-            '\r';
-
     public static void Machine(StringBuilder builder) =>
         PerformReplacements(builder, machineName, "TheMachineName");
 
     public static void User(StringBuilder builder) =>
         PerformReplacements(builder, userName, "TheUserName");
-
-    static void PerformReplacements(StringBuilder builder, string find, string replace)
-    {
-        if (builder.Length < find.Length)
-        {
-            return;
-        }
-
-        var matches = FindMatches(builder, find);
-
-        // Sort by position descending
-        var orderByDescending = matches.OrderByDescending(_ => _);
-
-        // Apply matches
-        foreach (var match in orderByDescending)
-        {
-            builder.Overwrite(replace, match, find.Length);
-        }
-    }
-
-    static IEnumerable<int> FindMatches(StringBuilder builder, string find)
-    {
-        var absolutePosition = 0;
-
-        foreach (var chunk in builder.GetChunks())
-        {
-            if (chunk.Length < find.Length)
-            {
-                absolutePosition += chunk.Length;
-                continue;
-            }
-
-            var chunkIndex = 0;
-            while (true)
-            {
-                var value = chunk.Span;
-                chunkIndex = value[chunkIndex..].IndexOf(find);
-                if (chunkIndex == -1)
-                {
-                    break;
-                }
-
-                var end = chunkIndex + find.Length;
-                if ((chunkIndex != 0 && !IsValidWrapper(value[chunkIndex - 1])) ||
-                    (end != value.Length && !IsValidWrapper(value[end])))
-                {
-                    chunkIndex++;
-                    continue;
-                }
-
-                yield return absolutePosition + chunkIndex;
-                chunkIndex += find.Length;
-            }
-
-            absolutePosition += chunk.Length;
-        }
-    }
 }

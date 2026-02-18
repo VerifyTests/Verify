e7e23c9bcfc29ce1a328df2e3806f963a323286d
Odonno <bottiau.david@laposte.net>
2026-02-18 12:31:41 +0100

    wip

---
+++ src/Verify.AotTests/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    [Test]
+    public Task Person()
+    {
+        var person = ClassBeingTested.FindPerson();
+        return Verify(person);
+        // var person = new Person
+        // {
+        //     FirstName = "FirstName",
+        //     LastName = "LastName"
+        // };
+        //
+        // var settings = new VerifySettings();
+        // await Verify(person, settings);
+        // var responseBody = "{\"hello\":\"world\"}";
+        // return Verify(responseBody);
+    }
+}
+
+// public class Person
+// {
+//     public string FirstName { get; set; }
+//     public string LastName { get; set; }
+// }
